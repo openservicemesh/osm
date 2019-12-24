48787a5010f39a34fa3fba6b0674dfe4ce32e262
Delyan Raychev <delyan.raychev@microsoft.com>
2019-12-23 16:52:06 -0800

    demo: Correct TrafficSplit

--- demo/deploy-traffic-split.sh
+++ demo/deploy-traffic-split.sh
@@ -14,18 +14,18 @@ kubectl apply -f - <<EOF
 apiVersion: split.smi-spec.io/v1alpha2
 kind: TrafficSplit
 metadata:
-  name: bookstore.azuremesh
+  name: bookstore.mesh
   namespace: "$K8S_NAMESPACE"
 spec:
-  service: bookstore.azuremesh
+  service: bookstore.mesh
   backends:
 
   - service: bookstore-1
-    weight: 100
+    weight: 80
 
   - service: bookstore-2
-    weight: 5
+    weight: 15
 
   - service: "/subscriptions/$AZURE_SUBSCRIPTION/resourceGroups/$AZURE_RESOURCE_GROUP/providers/Microsoft.Compute/virtualMachines/$VM_NAME"
-    weight: 100
+    weight: 5
 EOF
