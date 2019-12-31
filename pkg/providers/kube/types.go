f5e225520dd096f6416433ddc05eb7a26b6bdf66
Delyan Raychev <delyan.raychev@microsoft.com>
2019-12-31 13:08:45 -0500

    eds: Use the AzureResource Informer

--- pkg/providers/kube/types.go
+++ pkg/providers/kube/types.go
@@ -6,20 +6,24 @@ import (
 	"k8s.io/client-go/tools/cache"
 )
 
+type friendlyName string
+
 // InformerCollection is a struct of the Kubernetes informers used in SMC
 type InformerCollection struct {
-	Endpoints    cache.SharedIndexInformer
-	Services     cache.SharedIndexInformer
-	Pods         cache.SharedIndexInformer
-	TrafficSplit cache.SharedIndexInformer
+	Endpoints     cache.SharedIndexInformer
+	Services      cache.SharedIndexInformer
+	Pods          cache.SharedIndexInformer
+	TrafficSplit  cache.SharedIndexInformer
+	AzureResource cache.SharedIndexInformer
 }
 
 // CacheCollection is a struct of the Kubernetes caches used in SMC
 type CacheCollection struct {
-	Endpoints    cache.Store
-	Services     cache.Store
-	Pods         cache.Store
-	TrafficSplit cache.Store
+	Endpoints     cache.Store
+	Services      cache.Store
+	Pods          cache.Store
+	TrafficSplit  cache.Store
+	AzureResource cache.Store
 }
 
 // KubernetesProvider is a struct of the Kubernetes config and components used in SMC
