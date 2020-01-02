019c72a75bdde12b28959daab716f2edb98355a5
Delyan Raychev <delyan.raychev@microsoft.com>
2020-01-02 15:17:12 -0500

    azure: Adding tests

---
+++ pkg/providers/azure/provider_test.go
@@ -0,0 +1,22 @@
+package azure
+
+import (
+	"github.com/deislabs/smc/pkg/mesh"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Azure Compute Provider", func() {
+	Describe("Testing Azure Compute Provider", func() {
+		Context("Testing parseAzureID", func() {
+			uri := mesh.AzureID("/resource/subscriptions/e3f0/resourceGroups/mesh-rg/providers/Microsoft.Compute/virtualMachineScaleSets/baz")
+			It("returns default value in absence of an env var", func() {
+				rg, kind, name, err := parseAzureID(uri)
+				Expect(err).ShouldNot(HaveOccurred())
+				Expect(rg).To(Equal(resourceGroup("mesh-rg")))
+				Expect(kind).To(Equal(computeKind("Microsoft.Compute/virtualMachineScaleSets")))
+				Expect(name).To(Equal(computeName(("baz"))))
+			})
+		})
+	})
+})
