fab06b102dba008a4436a1a9bc159bb2865e75c0
Delyan Raychev <delyan.raychev@microsoft.com>
2020-01-03 15:02:47 -0500

    Moving run-*.sh to ./scripts

---
+++ scripts/run-eds.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+rm -rf ./bin/eds
+
+CGO_ENABLED=0 go build -v -o ./bin/eds ./cmd/eds
+
+## GRPC_TRACE=all GRPC_VERBOSITY=DEBUG GODEBUG='http2debug=2,gctrace=1,netdns=go+1'
+
+if [ ! -f ".env" ]; then
+    echo "Create .env (see .env.example)"
+    exit 1
+fi
+
+source .env
+
+./bin/eds \
+    --kubeconfig="$HOME/.kube/config" \
+    --azureAuthFile="$HOME/.azure/azureAuth.json" \
+    --subscriptionID="$AZURE_SUBSCRIPTION" \
+    --namespace="$K8S_NAMESPACE" \
+    --verbosity=7
