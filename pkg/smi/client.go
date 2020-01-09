de8696177ff8bdc4f820260f06fad6accd73248f
Delyan Raychev <delyan.raychev@microsoft.com>
2020-01-09 15:24:16 -0800

    Adding a dedicated SMI Spec client

---
+++ pkg/smi/client.go
@@ -0,0 +1,101 @@
+package smi
+
+import (
+	"time"
+
+	"github.com/deislabs/smi-sdk-go/pkg/gen/client/split/clientset/versioned"
+	smiExternalVersions "github.com/deislabs/smi-sdk-go/pkg/gen/client/split/informers/externalversions"
+	"github.com/eapache/channels"
+	"github.com/golang/glog"
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/cache"
+)
+
+var resyncPeriod = 1 * time.Second
+
+// Run executes informer collection.
+func (c *Client) Run(stopCh <-chan struct{}) error {
+	glog.V(1).Infoln("Kubernetes Compute Provider started")
+	var hasSynced []cache.InformerSynced
+
+	if c.informers == nil {
+		return errInitInformers
+	}
+
+	sharedInformers := map[friendlyName]cache.SharedInformer{
+		"TrafficSplit": c.informers.TrafficSplit,
+		"Services":     c.informers.Services,
+	}
+
+	var names []friendlyName
+	for name, informer := range sharedInformers {
+		// Depending on the use-case, some Informers from the collection may not have been initialized.
+		if informer == nil {
+			continue
+		}
+		names = append(names, name)
+		glog.Info("Starting informer: ", name)
+		go informer.Run(stopCh)
+		hasSynced = append(hasSynced, informer.HasSynced)
+	}
+
+	glog.V(1).Infof("Waiting informers cache sync: %+v", names)
+	if !cache.WaitForCacheSync(stopCh, hasSynced...) {
+		return errSyncingCaches
+	}
+
+	// Closing the cacheSynced channel signals to the rest of the system that... caches have been synced.
+	close(c.cacheSynced)
+
+	glog.V(1).Infof("Cache sync finished for %+v", names)
+	return nil
+}
+
+// GetID returns a string descriptor / identifier of the compute provider.
+// Required by interface: EndpointsProvider
+func (c *Client) GetID() string {
+	return c.providerIdent
+}
+
+// newClient creates a provider based on a Kubernetes client instance.
+func newSMIClient(kubeClient *kubernetes.Clientset, smiClient *versioned.Clientset, namespaces []string, announceChan *channels.RingChannel, providerIdent string) *Client {
+	var options []informers.SharedInformerOption
+	var smiOptions []smiExternalVersions.SharedInformerOption
+	for _, namespace := range namespaces {
+		options = append(options, informers.WithNamespace(namespace))
+		smiOptions = append(smiOptions, smiExternalVersions.WithNamespace(namespace))
+	}
+
+	informerFactory := informers.NewSharedInformerFactoryWithOptions(kubeClient, resyncPeriod, options...)
+	smiInformerFactory := smiExternalVersions.NewSharedInformerFactoryWithOptions(smiClient, resyncPeriod, smiOptions...)
+	informerCollection := InformerCollection{
+		Services:     informerFactory.Core().V1().Services().Informer(),
+		TrafficSplit: smiInformerFactory.Split().V1alpha2().TrafficSplits().Informer(),
+	}
+
+	cacheCollection := CacheCollection{
+		Services:     informerCollection.Services.GetStore(),
+		TrafficSplit: informerCollection.TrafficSplit.GetStore(),
+	}
+
+	client := Client{
+		providerIdent: providerIdent,
+		informers:     &informerCollection,
+		caches:        &cacheCollection,
+		announceChan:  announceChan,
+		cacheSynced:   make(chan interface{}),
+	}
+
+	h := handlers{client}
+
+	resourceHandler := cache.ResourceEventHandlerFuncs{
+		AddFunc:    h.addFunc,
+		UpdateFunc: h.updateFunc,
+		DeleteFunc: h.deleteFunc,
+	}
+
+	informerCollection.TrafficSplit.AddEventHandler(resourceHandler)
+
+	return &client
+}
