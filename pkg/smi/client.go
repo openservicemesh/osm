190799053df0497c76019b089acfecf6f309ce2a
Delyan Raychev <delyan.raychev@microsoft.com>
2020-01-24 12:39:03 -0800

    Refactor

--- pkg/smi/client.go
+++ pkg/smi/client.go
@@ -5,24 +5,40 @@ import (
 	"time"
 
 	"github.com/deislabs/smi-sdk-go/pkg/apis/split/v1alpha2"
-	"github.com/deislabs/smi-sdk-go/pkg/gen/client/split/clientset/versioned"
-	smiExternalVersions "github.com/deislabs/smi-sdk-go/pkg/gen/client/split/informers/externalversions"
 	"github.com/eapache/channels"
-	"github.com/golang/glog"
 	v1 "k8s.io/api/core/v1"
-	"k8s.io/client-go/informers"
-	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/cache"
 
 	"github.com/deislabs/smc/pkg/endpoint"
-	"github.com/deislabs/smc/pkg/mesh"
+	"github.com/deislabs/smi-sdk-go/pkg/gen/client/split/clientset/versioned"
+	smiExternalVersions "github.com/deislabs/smi-sdk-go/pkg/gen/client/split/informers/externalversions"
+	"github.com/golang/glog"
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/cache"
 )
 
 var resyncPeriod = 1 * time.Second
 
-// Run executes informer collection.
-func (c *Client) Run(stopCh <-chan struct{}) error {
+// We have a few different k8s clients. This identifies these in logs.
+const kubernetesClientName = "MeshTopology"
+
+// NewSpecificationClient implements mesh.Specificator and creates the Kubernetes client, which retrieves SMI specific CRDs.
+func NewSpecificationClient(kubeConfig *rest.Config, namespaces []string, announcement chan struct{}, stop chan struct{}) Specificator {
+	kubeClient := kubernetes.NewForConfigOrDie(kubeConfig)
+	smiClientset := versioned.NewForConfigOrDie(kubeConfig)
+
+	announcements := channels.NewRingChannel(1204)
+	client := newSMIClient(kubeClient, smiClientset, namespaces, announcements, kubernetesClientName)
+	err := client.run(stop)
+	if err != nil {
+		glog.Fatalf("Could not start %s client: %s", kubernetesClientName, err)
+	}
+	return client
+}
+
+// run executes informer collection.
+func (c *Client) run(stopCh <-chan struct{}) error {
 	glog.V(1).Infoln("SMI Client started")
 	var hasSynced []cache.InformerSynced
 
@@ -108,22 +124,7 @@ func newSMIClient(kubeClient *kubernetes.Clientset, smiClient *versioned.Clients
 	return &client
 }
 
-// We have a few different k8s clients. This identifies these in logs.
-const kubernetesClientName = "MeshTopology"
-
-// NewMeshTopologyClient creates the Kubernetes client, which retrieves SMI specific CRDs.
-func NewMeshTopologyClient(kubeConfig *rest.Config, namespaces []string, announcements *channels.RingChannel, stopChan chan struct{}) mesh.Topology {
-	kubeClient := kubernetes.NewForConfigOrDie(kubeConfig)
-	smiClientset := versioned.NewForConfigOrDie(kubeConfig)
-	client := newSMIClient(kubeClient, smiClientset, namespaces, announcements, kubernetesClientName)
-	err := client.Run(stopChan)
-	if err != nil {
-		glog.Fatalf("Could not start %s client: %s", kubernetesClientName, err)
-	}
-	return client
-}
-
-// ListTrafficSplits implements mesh.Topology by returning the list of traffic splits.
+// ListTrafficSplits implements mesh.Specificator by returning the list of traffic splits.
 func (c *Client) ListTrafficSplits() []*v1alpha2.TrafficSplit {
 	var trafficSplits []*v1alpha2.TrafficSplit
 	for _, splitIface := range c.caches.TrafficSplit.List() {
@@ -133,7 +134,7 @@ func (c *Client) ListTrafficSplits() []*v1alpha2.TrafficSplit {
 	return trafficSplits
 }
 
-// ListServices implements mesh.Topology by returning the services observed from the given compute provider
+// ListServices implements mesh.Specificator by returning the services observed from the given compute provider
 func (c *Client) ListServices() []endpoint.ServiceName {
 	// TODO(draychev): split the namespace and the service kubernetesClientName -- for non-kubernetes services we won't have namespace
 	var services []endpoint.ServiceName
