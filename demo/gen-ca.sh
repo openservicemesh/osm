e6ffb7b1906e04d95235fa77a7a29dbbd7e5e7b0
Michelle Noorali <michellemolu@gmail.com>
2020-02-12 21:04:31 -0500

    ref(demo): remove duplicate cert gen step

--- demo/gen-ca.sh
+++ demo/gen-ca.sh
@@ -8,14 +8,6 @@ CRT="$DIR/root-cert.pem"
 
 mkdir -p "$DIR"
 
-openssl req -x509 -sha256 -nodes -days 365 \
-        -newkey rsa:2048 \
-        -subj "/CN=$(uuidgen).azure.mesh/O=Az Mesh/C=US" \
-        -keyout "$KEY"  \
-        -out "$CRT"
-
-exit 0
-
 ./bin/cert \
     --caPEMFileOut="$CRT" \
     --caKeyPEMFileOut="$KEY" \
