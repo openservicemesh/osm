68444009c550423591379f47d723f42045071c2b
Delyan Raychev <delyan.raychev@microsoft.com>
2020-02-14 16:16:38 -0800

    ads: Simplify response makers (#182)

--- pkg/envoy/ads/server.go
+++ pkg/envoy/ads/server.go
@@ -3,9 +3,11 @@ package ads
 import (
 	"context"
 
+	envoy_api_v2 "github.com/envoyproxy/go-control-plane/envoy/api/v2"
 	xds "github.com/envoyproxy/go-control-plane/envoy/service/discovery/v2"
 
 	"github.com/deislabs/smc/pkg/catalog"
+	"github.com/deislabs/smc/pkg/envoy"
 	"github.com/deislabs/smc/pkg/envoy/cds"
 	"github.com/deislabs/smc/pkg/envoy/eds"
 	"github.com/deislabs/smc/pkg/envoy/lds"
@@ -20,7 +22,7 @@ const (
 
 // NewADSServer creates a new CDS server
 func NewADSServer(ctx context.Context, meshCatalog catalog.MeshCataloger, meshSpec smi.MeshSpec) *Server {
-	return &Server{
+	s := Server{
 		catalog: meshCatalog,
 
 		cdsServer: cds.NewCDSServer(meshCatalog),
@@ -29,6 +31,14 @@ func NewADSServer(ctx context.Context, meshCatalog catalog.MeshCataloger, meshSp
 		ldsServer: lds.NewLDSServer(meshCatalog),
 		sdsServer: sds.NewSDSServer(meshCatalog),
 	}
+	s.xdsHandlers = map[envoy.TypeURI]func(envoy.Proxyer) (*envoy_api_v2.DiscoveryResponse, error){
+		envoy.TypeEDS: s.edsServer.NewEndpointDiscoveryResponse,
+		envoy.TypeCDS: s.cdsServer.NewClusterDiscoveryResponse,
+		envoy.TypeRDS: s.rdsServer.NewRouteDiscoveryResponse,
+		envoy.TypeLDS: s.ldsServer.NewListenerDiscoveryResponse,
+		envoy.TypeSDS: s.sdsServer.NewSecretDiscoveryResponse,
+	}
+	return &s
 }
 
 // DeltaAggregatedResources implements xds.AggregatedDiscoveryServiceServer
