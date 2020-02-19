b991bc021c0749da410b9e2261c58e6c144c4070
Delyan Raychev <delyan.raychev@microsoft.com>
2020-02-19 14:11:18 -0800

    ads: Use AggregatedConfigSource where ConfigSource is needed

--- pkg/envoy/types.go
+++ pkg/envoy/types.go
@@ -8,4 +8,13 @@ const (
 	TypeLDS TypeURI = "type.googleapis.com/envoy.api.v2.Listener"
 	TypeRDS TypeURI = "type.googleapis.com/envoy.api.v2.RouteConfiguration"
 	TypeEDS TypeURI = "type.googleapis.com/envoy.api.v2.ClusterLoadAssignment"
+
+	TransportSocketTLS = "envoy.transport_sockets.tls"
+	CertificateName    = "server_cert"
+
+	accessLogPath = "/dev/stdout"
+
+	// cipher suites
+	aes    = "ECDHE-ECDSA-AES128-GCM-SHA256"
+	chacha = "ECDHE-ECDSA-CHACHA20-POLY1305"
 )
