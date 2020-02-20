5669b7ed0bdf22d97faa82f36faf8d9f3448b19e
Delyan Raychev <delyan.raychev@microsoft.com>
2020-02-20 15:18:32 -0800

    demo: Bookbuyer to wait up to N seconds for a 200 OK (#206)
    * demo: Increase bookbuyer counter
    
    * demo: Run the demo for WAIT_FOR_OK_SECONDS before failing

--- demo/cmd/bookbuyer/bookbuyer.go
+++ demo/cmd/bookbuyer/bookbuyer.go
@@ -3,37 +3,60 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"strconv"
 	"strings"
 	"time"
 )
 
 const (
+	waitForEnvVar = "WAIT_FOR_OK_SECONDS"
+
 	counter = "http://bookstore.mesh/counter"
 	incremt = "http://bookstore.mesh/incrementcounter"
 )
 
 func main() {
+	waitForOK := getWaitForOK()
+	started := time.Now()
+	finishBy := started.Add(time.Duration(waitForOK) * time.Second)
 	iteration := 0
 	for {
+		iteration++
 		fmt.Printf("---[ %d ]-----------------------------------------\n", iteration)
+		var responses []int
 		for _, url := range []string{counter, incremt} {
-			fetch(url)
+			response := fetch(url)
+			fmt.Println("")
+			responses = append(responses, response)
 		}
-		fmt.Println("")
+		if waitForOK != 0 {
+			if responses[0] == 200 {
+				fmt.Printf("Success")
+				os.Exit(0)
+			} else if time.Now().After(finishBy) {
+				fmt.Printf("It has been %v since we started the test. Response code from %s is %d. This test has failed.",
+					time.Now().Sub(started), counter, responses[0])
+				os.Exit(1)
+			}
+		}
+		fmt.Print("\n\n")
 		time.Sleep(3 * time.Second)
 	}
 }
 
-func fetch(url string) {
+func fetch(url string) (responseCode int) {
 	fmt.Printf("Fetching %s\n", url)
 	if resp, err := http.Get(url); err != nil {
 		fmt.Printf("Error fetching %s: %s\n", url, err)
 	} else {
+		responseCode = resp.StatusCode
 		for _, hdr := range []string{"Identity", "Counter", "Server", "Date"} {
 			fmt.Printf("%s: %s\n", hdr, getHeader(resp.Header, hdr))
 		}
-		fmt.Printf("Status: %d %s\n", resp.StatusCode, resp.Status)
+		fmt.Printf("Status: %s\n", resp.Status)
 	}
+	return responseCode
 }
 
 func getHeader(headers map[string][]string, header string) string {
@@ -42,4 +65,13 @@ func getHeader(headers map[string][]string, header string) string {
 		val = []string{"n/a"}
 	}
 	return strings.Join(val, ", ")
+}
+
+func getWaitForOK() int64 {
+	waitForOKString := os.Getenv(waitForEnvVar)
+	waitForOK, err := strconv.ParseInt(waitForOKString, 10, 64)
+	if err != nil {
+		waitForOK = 0
+	}
+	return waitForOK
 }
