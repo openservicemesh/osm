5ffe0c64fb6cc0987c294b908308af91991f211b
Delyan Raychev <delyan.raychev@microsoft.com>
2020-02-19 16:45:08 -0800

    Move pre-push-hook to ./scripts

--- scripts/install-git-pre-push-hook.sh
+++ scripts/install-git-pre-push-hook.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mkdir -p .git/hooks
-pushd .git/hooks && ln -f -s ../../pre-push-hook ./pre-push && popd
+pushd .git/hooks && ln -f -s ../../scripts/pre-push-hook ./pre-push && popd
 chmod +x .git/hooks/pre-push
