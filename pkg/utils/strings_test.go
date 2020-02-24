900b4c8196ccd9c39cb8e55a8a7aae7058ddae35
Sneha Chhabria <snchh@microsoft.com>
2020-02-24 14:01:53 -0800

    fixing get service name from proxy

--- pkg/utils/strings_test.go
+++ pkg/utils/strings_test.go
@@ -7,12 +7,24 @@ import (
 
 var _ = Describe("Testing utils helpers", func() {
 	Context("Test GetLastNOfDotted", func() {
-		It("Should return the last slice of a string split on a slash.", func() {
+		It("Should return the last slice of a string split on a dot.", func() {
 			Expect(GetLastNOfDotted("a.b.c", 2)).To(Equal("b.c"))
 		})
 
-		It("Should return the full string when there are no slashes.", func() {
-			Expect(GetLastChunkOfSlashed("abc")).To(Equal("abc"))
+		It("Should return the full string when there are no dots.", func() {
+			Expect(GetLastNOfDotted("abc", 0)).To(Equal("abc"))
+		})
+	})
+})
+
+var _ = Describe("Testing utils helpers", func() {
+	Context("Test GetFirstOfDotted", func() {
+		It("Should return the first slice of a string split on a dot.", func() {
+			Expect(GetFirstOfDotted("a.b.c")).To(Equal("a"))
+		})
+
+		It("Should return the full string when there are no dots.", func() {
+			Expect(GetFirstOfDotted("abc")).To(Equal("abc"))
 		})
 	})
 })
