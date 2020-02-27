b6e47009e0725a6f80e617212a8922c3993b2719
Delyan Raychev <delyan.raychev@microsoft.com>
2020-02-26 18:31:48 -0800

    demo: Move AzureResource.sh into a template / bash script

--- demo/deploy-AzureResource.sh
+++ demo/deploy-AzureResource.sh
@@ -1,17 +1,28 @@
+#!/bin/bash
+
+set -aueo pipefail
+
+# shellcheck disable=SC1091
+source .env
+
+SVC="bookstore-vm"
+
+echo -e "Deploy $SVC demo service"
+cat <<EOF | kubectl apply -f -
 apiVersion: v1
 kind: Service
 metadata:
-  name: bookstore-vm
-  namespace: smc
+  name: $SVC
+  namespace: $K8S_NAMESPACE
   labels:
-    app: bookstore-vm
+    app: $SVC
 spec:
   ports:
   - port: 80
     targetPort: 80
     name: app-port
   selector:
-    app: bookstore-vm
+    app: $SVC
   type: NodePort
 ---
 
@@ -19,8 +30,9 @@ apiVersion: smc.osm.k8s.io/v1
 kind: AzureResource
 metadata:
   name: bookstore
-  namespace: smc
+  namespace: $K8S_NAMESPACE
   labels:
-    app: bookstore-vm
+    app: $SVC
 spec:
   resourceid: /subscriptions/your-subscription-id/resourceGroups/your-resource-group-name/providers/Microsoft.Compute/virtualMachines/vm-name
+EOF
