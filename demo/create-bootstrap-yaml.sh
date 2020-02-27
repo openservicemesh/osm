ff590cb20faa048a0cdf25f09f34bb8ec1881fe8
Delyan Raychev <delyan.raychev@microsoft.com>
2020-02-27 13:08:27 -0800

    Create the bootstrap.yaml and push to k8s

--- demo/create-bootstrap-yaml.sh
+++ demo/create-bootstrap-yaml.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
-XDS="ads.${K8S_NAMESPACE}.svc.cluster.local"
+set -aueo pipefail
 
-cat <<EOF > ./demo/config/bootstrap.yaml
+# shellcheck disable=SC1091
+source .env
+
+
+CONFIG_DIR="./config/${K8S_NAMESPACE}"
+
+mkdir -p "$CONFIG_DIR"
+
+FILE="$CONFIG_DIR/bootstrap.yaml"
+
+echo "Creating $FILE"
+
+cat <<EOF > "$FILE"
 admin:
   access_log_path: "/dev/stdout"
   address:
-    socket_address:
-      address: 0.0.0.0
-      port_value: 15000
+    socket_address: {address: 0.0.0.0, port_value: 15000}
+
 dynamic_resources:
   ads_config:
     api_type: GRPC
@@ -20,8 +31,10 @@ dynamic_resources:
     ads: {}
   lds_config:
     ads: {}
+
 static_resources:
   clusters:
+
   - name: ads
     connect_timeout: 0.25s
     type: LOGICAL_DNS
@@ -42,6 +55,15 @@ static_resources:
         - endpoint:
             address:
               socket_address:
-                address: $XDS
+                address: ads.${K8S_NAMESPACE}.svc.cluster.local
                 port_value: 15128
 EOF
+
+rm -rf "${CONFIG_DIR}/*~"
+rm -rf "${CONFIG_DIR}/*\\#*"
+
+kubectl delete configmap envoyproxy-config -n "$K8S_NAMESPACE" || true
+kubectl create \
+        configmap envoyproxy-config \
+        --from-file="${CONFIG_DIR}" \
+        -n "$K8S_NAMESPACE" || true
