677244c5800fb9c8242e18e0be43e94c809f7453
Michelle Noorali <michellemolu@gmail.com>
2020-02-28 16:18:42 -0500

    ref(demo): move build/push img to file for reuse

--- demo/build-push-images.sh
+++ demo/build-push-images.sh
@@ -5,13 +5,9 @@ set -aueo pipefail
 # shellcheck disable=SC1091
 source .env
 
-make docker-push-cds
-make docker-push-lds
-make docker-push-eds
-make docker-push-sds
-make docker-push-rds
+make docker-push-ads
 
 make docker-push-init
-docker-push-envoyproxy
+make docker-push-envoyproxy
 make docker-push-bookbuyer
 make docker-push-bookstore
