ae61c508c561e0f6f25aacc1795ab8aae4d5f544
Delyan Raychev <delyan.raychev@microsoft.com>
2020-03-02 14:16:22 -0800

    Adding ginkgo test runners for most packages

---
+++ pkg/log/suite_test.go
@@ -0,0 +1,13 @@
+package log
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestEndpoints(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Test Suite")
+}
