b0bc4ddf491c58457c6ccef3837b9b170b9544f3
Delyan Raychev <delyan.raychev@microsoft.com>
2020-03-02 11:00:31 -0800

    lds: Remove duplicated code generating HTTP Connection Manager (#244)
    * lds: Remove duplicated code generating HTTP Connection Manager
    
    * ci: Bump WAIT_FOR_OK_SECONDS from 55 to 120 seconds

--- pkg/envoy/lds/connection_manager.go
+++ pkg/envoy/lds/connection_manager.go
@@ -2,18 +2,16 @@ package lds
 
 import (
 	envoy_hcm "github.com/envoyproxy/go-control-plane/envoy/config/filter/network/http_connection_manager/v2"
-	wellknown "github.com/envoyproxy/go-control-plane/pkg/wellknown"
+	"github.com/envoyproxy/go-control-plane/pkg/wellknown"
 
 	"github.com/deislabs/smc/pkg/envoy"
-	"github.com/deislabs/smc/pkg/envoy/route"
 )
 
 const (
 	statPrefix = "http"
 )
 
-// getRdsHttpClientConnectionFilter gets the required route configuration for the envoy on the source service
-func getRdsHTTPClientConnectionFilter() *envoy_hcm.HttpConnectionManager {
+func getHTTPConnectionManager(routeName string) *envoy_hcm.HttpConnectionManager {
 	return &envoy_hcm.HttpConnectionManager{
 		StatPrefix: statPrefix,
 		CodecType:  envoy_hcm.HttpConnectionManager_AUTO,
@@ -23,25 +21,7 @@ func getRdsHTTPClientConnectionFilter() *envoy_hcm.HttpConnectionManager {
 		RouteSpecifier: &envoy_hcm.HttpConnectionManager_Rds{
 			Rds: &envoy_hcm.Rds{
 				ConfigSource:    envoy.GetADSConfigSource(),
-				RouteConfigName: route.SourceRouteConfig,
-			},
-		},
-		AccessLog: envoy.GetAccessLog(),
-	}
-}
-
-// getRdsHttpClientConnectionFilter gets the required route configuration for the envoy on the destination service
-func getRdsHTTPServerConnectionFilter() *envoy_hcm.HttpConnectionManager {
-	return &envoy_hcm.HttpConnectionManager{
-		StatPrefix: statPrefix,
-		CodecType:  envoy_hcm.HttpConnectionManager_AUTO,
-		HttpFilters: []*envoy_hcm.HttpFilter{{
-			Name: wellknown.Router,
-		}},
-		RouteSpecifier: &envoy_hcm.HttpConnectionManager_Rds{
-			Rds: &envoy_hcm.Rds{
-				ConfigSource:    envoy.GetADSConfigSource(),
-				RouteConfigName: route.DestinationRouteConfig,
+				RouteConfigName: routeName,
 			},
 		},
 		AccessLog: envoy.GetAccessLog(),
