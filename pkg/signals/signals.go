d7eda0d4d8fb250fcc2be6110aa01f70672569d1
Shashank Ram <shashank08@gmail.com>
2020-03-05 14:19:59 -0800

    signals: Use signals package for exit handlers (#261)

---
+++ pkg/signals/signals.go
@@ -0,0 +1,24 @@
+package signals
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+var exitSignals = []os.Signal{os.Interrupt, syscall.SIGTERM} // SIGTERM is POSIX specific
+
+// RegisterExitHandlers returns a handle channel to wait on exit signals
+func RegisterExitHandlers() chan struct{} { // TODO: needs to return a recv channel
+	stop := make(chan struct{})
+	s := make(chan os.Signal, 2)
+	signal.Notify(s, exitSignals...)
+
+	go func() {
+		// Wait for a singal from the OS before signalling others on 'stop' channel
+		<-s
+		close(stop)
+	}()
+
+	return stop
+}
