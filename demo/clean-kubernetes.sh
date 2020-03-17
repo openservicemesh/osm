c1cb833504a04045130c5e01898c525a843d6f8f
Shashank Ram <shashank08@gmail.com>
2020-03-17 10:55:50 -0700

    Use a meaningful label to annotate namespaces for sidecar injection (#317)

--- demo/clean-kubernetes.sh
+++ demo/clean-kubernetes.sh
@@ -8,6 +8,6 @@ source .env
 kubectl delete mutatingwebhookconfiguration ads --ignore-not-found=true
 kubectl delete namespace "$K8S_NAMESPACE" || true
 kubectl create namespace "$K8S_NAMESPACE" || true
-kubectl label  namespaces "$K8S_NAMESPACE" osm="$K8S_NAMESPACE"
+kubectl label  namespaces "$K8S_NAMESPACE" osm-inject="$K8S_NAMESPACE"
 kubectl delete clusterrole osm-xds || true
 kubectl delete clusterrolebinding osm-xds || true
