3e75a2d68aded0215c48a4b8f7d36d9beafd5034
Shashank Ram <shashank.ram@microsoft.com>
2020-03-18 16:51:57 -0700

    Update port-forward* scripts with demo namespaces

--- scripts/port-forward-bookthief.sh
+++ scripts/port-forward-bookthief.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+# shellcheck disable=SC1091
+source .env
 
-POD="$(kubectl get pods --selector app=bookthief -nsmc --no-headers | grep 'Running' | awk '{print $1}')"
+POD="$(kubectl get pods --selector app=bookthief -n "$BOOKTHIEF_NAMESPACE" --no-headers | grep 'Running' | awk '{print $1}')"
 
-kubectl port-forward "$POD" -n smc 15000:15000
+kubectl port-forward "$POD" -n "$BOOKTHIEF_NAMESPACE" 15000:15000
 
