91e4b6ef1b2cb4f750dc02d8dfa8cd29396f7096
Delyan Raychev <delyan.raychev@microsoft.com>
2020-03-25 05:19:43 -0700

    Simplify bookstore

--- demo/cmd/bookstore/bookstore.go
+++ demo/cmd/bookstore/bookstore.go
@@ -23,7 +23,6 @@ var identity = flag.String("ident", "unidentified", "the identity of the contain
 var port = flag.Int("port", 80, "port on which this app is listening for incoming HTTP")
 var path = flag.String("path", ".", "path to the HTML template")
 var booksBought int
-var tmpl *template.Template
 
 func getIdentity() string {
 	ident := os.Getenv("IDENTITY")
@@ -35,33 +34,16 @@ func getIdentity() string {
 	return ident
 }
 
-// getCurrentBooksBought gets the value of the counter
-func getCurrentBooksBought(w http.ResponseWriter, r *http.Request) {
-
+func setHeaders(w http.ResponseWriter) {
 	w.Header().Set(common.BooksBoughtHeader, fmt.Sprintf("%d", booksBought))
-
 	w.Header().Set(common.IdentityHeader, getIdentity())
-	err := tmpl.Execute(w, map[string]string{
-		common.IdentityHeader:    getIdentity(),
-		common.BooksBoughtHeader: fmt.Sprintf("%d", booksBought),
-	})
-	if err != nil {
-		glog.Fatal("Could not render template", err)
-	}
-	fmt.Printf("%s;  URL: %q;  Count: %d\n", getIdentity(), html.EscapeString(r.URL.Path), booksBought)
 }
 
-// updateBooksBoughtValue updates the booksBought value to the one specified by the user
-func updateBooksBoughtValue(w http.ResponseWriter, r *http.Request) {
-
-	var updatedBooksBought int
-	err := json.NewDecoder(r.Body).Decode(&updatedBooksBought)
+func renderTemplate(w http.ResponseWriter) {
+	tmpl, err := template.ParseFiles(fmt.Sprintf("%s/bookstore.html.template", *path))
 	if err != nil {
-		glog.Fatal("Could not decode request body", err)
+		log.Fatal(err)
 	}
-	booksBought = updatedBooksBought
-	w.Header().Set(common.BooksBoughtHeader, fmt.Sprintf("%d", booksBought))
-	w.Header().Set(common.IdentityHeader, getIdentity())
 	err = tmpl.Execute(w, map[string]string{
 		common.IdentityHeader:    getIdentity(),
 		common.BooksBoughtHeader: fmt.Sprintf("%d", booksBought),
@@ -69,22 +51,33 @@ func updateBooksBoughtValue(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		glog.Fatal("Could not render template", err)
 	}
+}
+
+func getBooksBought(w http.ResponseWriter, r *http.Request) {
+	setHeaders(w)
+	renderTemplate(w)
+	fmt.Printf("%s;  URL: %q;  Count: %d\n", getIdentity(), html.EscapeString(r.URL.Path), booksBought)
+}
+
+// updateBooksBought updates the booksBought value to the one specified by the user
+func updateBooksBought(w http.ResponseWriter, r *http.Request) {
+	var updatedBooksBought int
+	err := json.NewDecoder(r.Body).Decode(&updatedBooksBought)
+	if err != nil {
+		glog.Fatal("Could not decode request body", err)
+	}
+	booksBought = updatedBooksBought
+	setHeaders(w)
+	renderTemplate(w)
 	fmt.Printf("%s;  URL: %q;  %s: %d\n", getIdentity(), html.EscapeString(r.URL.Path), common.BooksBoughtHeader, booksBought)
 }
 
-// incrementBooksBought increments the value of the booksBought
-func incrementBooksBought(w http.ResponseWriter, r *http.Request) {
+// buyBook increments the value of the booksBought
+func buyBook(w http.ResponseWriter, r *http.Request) {
 
 	booksBought++
-	w.Header().Set(common.BooksBoughtHeader, fmt.Sprintf("%d", booksBought))
-	w.Header().Set(common.IdentityHeader, getIdentity())
-	err := tmpl.Execute(w, map[string]string{
-		common.IdentityHeader:    getIdentity(),
-		common.BooksBoughtHeader: fmt.Sprintf("%d", booksBought),
-	})
-	if err != nil {
-		glog.Fatal("Could not render template", err)
-	}
+	setHeaders(w)
+	renderTemplate(w)
 	fmt.Printf("%s;  URL: %q;  Count: %d\n", getIdentity(), html.EscapeString(r.URL.Path), booksBought)
 	// Loop through headers
 	for name, headers := range r.Header {
@@ -97,20 +90,15 @@ func incrementBooksBought(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	flag.Parse()
-	var err error
-	tmpl, err = template.ParseFiles(fmt.Sprintf("%s/bookstore.html.template", *path))
-	if err != nil {
-		log.Fatal(err)
-	}
 	booksBought = 1
 
 	//initializing router
 	router := mux.NewRouter()
 
 	//endpoints
-	router.HandleFunc("/books-bought", getCurrentBooksBought).Methods("GET")
-	router.HandleFunc("/books-bought", updateBooksBoughtValue).Methods("POST")
-	router.HandleFunc("/buy-a-book", incrementBooksBought).Methods("GET")
+	router.HandleFunc("/books-bought", getBooksBought).Methods("GET")
+	router.HandleFunc("/books-bought", updateBooksBought).Methods("POST")
+	router.HandleFunc("/buy-a-book", buyBook).Methods("GET")
 	http.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {})
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), router))
 }
