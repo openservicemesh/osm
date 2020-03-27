3f80d43e8fd359cb860a7b337ffb2bd6b69bebf6
Delyan Raychev <delyan.raychev@microsoft.com>
2020-03-27 10:56:42 -0700

    kubernetes: Carving out Kubernetes event handlers in their own package so we can reuse them

--- pkg/kubernetes/event_handlers.go
+++ pkg/kubernetes/event_handlers.go
@@ -8,7 +8,6 @@ import (
 	"k8s.io/client-go/tools/cache"
 )
 
-// GetKubernetesEventHandlers creates Kubernetes events handlers.
 func GetKubernetesEventHandlers(informerName string, providerName string, announcements chan interface{}) cache.ResourceEventHandlerFuncs {
 	return cache.ResourceEventHandlerFuncs{
 		AddFunc:    Add(informerName, providerName, announcements),
@@ -17,7 +16,6 @@ func GetKubernetesEventHandlers(informerName string, providerName string, announ
 	}
 }
 
-// Add a new item to Kubernetes caches from an incoming Kubernetes event.
 func Add(informerName string, providerName string, announce chan interface{}) func(obj interface{}) {
 	return func(obj interface{}) {
 		glog.V(level.Trace).Infof("[%s][%s] Add event: %+v", providerName, informerName, obj)
@@ -30,7 +28,6 @@ func Add(informerName string, providerName string, announce chan interface{}) fu
 	}
 }
 
-// Update caches with an incoming Kubernetes event.
 func Update(informerName string, providerName string, announce chan interface{}) func(oldObj, newObj interface{}) {
 	return func(oldObj, newObj interface{}) {
 		glog.V(level.Trace).Infof("[%s][%s] Update event %+v", providerName, informerName, oldObj)
@@ -46,7 +43,6 @@ func Update(informerName string, providerName string, announce chan interface{})
 	}
 }
 
-// Delete Kubernetes cache from an incoming Kubernetes event.
 func Delete(informerName string, providerName string, announce chan interface{}) func(obj interface{}) {
 	return func(obj interface{}) {
 		glog.V(level.Trace).Infof("[%s][%s] Delete event: %+v", providerName, informerName, obj)
