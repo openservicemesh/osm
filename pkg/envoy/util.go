3764e0d3f77a1feb8807dee8beec5d280e88b784
Delyan Raychev <delyan.raychev@microsoft.com>
2020-03-27 14:03:55 -0700

    util: Delete unused getNamespacedService

--- pkg/envoy/util.go
+++ pkg/envoy/util.go
@@ -1,8 +1,6 @@
 package envoy
 
 import (
-	"strings"
-
 	"github.com/open-service-mesh/osm/pkg/endpoint"
 )
 
@@ -14,17 +12,4 @@ func Contains(proxyService endpoint.NamespacedService, services []endpoint.Names
 		}
 	}
 	return false
-}
-
-func getNamespacedService(serviceName string) endpoint.NamespacedService {
-	split := strings.Split(serviceName, "/")
-	var namespacedService endpoint.NamespacedService
-	if len(split) == 0 {
-		namespacedService.Namespace = "default"
-		namespacedService.Service = split[0]
-	} else {
-		namespacedService.Namespace = split[0]
-		namespacedService.Service = split[1]
-	}
-	return namespacedService
 }
