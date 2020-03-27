cc3b0603c8863e38fe5e62a0c4b373ea18cd62eb
Delyan Raychev <delyan.raychev@microsoft.com>
2020-03-27 10:04:10 -0700

    lint: Removing innefectual err assignment (#368)

--- pkg/injector/sidecar.go
+++ pkg/injector/sidecar.go
@@ -13,7 +13,7 @@ const (
 	envoyBootstrapConfigFile  = "/etc/envoy/bootstrap.yaml"
 )
 
-func getEnvoySidecarContainerSpec(pod *corev1.Pod, data *EnvoySidecarData) (corev1.Container, error) {
+func getEnvoySidecarContainerSpec(pod *corev1.Pod, data *EnvoySidecarData) corev1.Container {
 	container := corev1.Container{
 		Name:            data.Name,
 		Image:           data.Image,
@@ -57,5 +57,5 @@ func getEnvoySidecarContainerSpec(pod *corev1.Pod, data *EnvoySidecarData) (core
 		},
 	}
 
-	return container, nil
+	return container
 }
