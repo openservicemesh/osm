b1dfcd7d6cdc2c87f27476b7c70d12f830287483
Delyan Raychev <delyan.raychev@microsoft.com>
2020-03-27 10:23:52 -0700

    sidecar: Remove unused fn param (#367)

--- pkg/injector/patch.go
+++ pkg/injector/patch.go
@@ -69,10 +69,9 @@ func (wh *Webhook) createPatch(pod *corev1.Pod, namespace string) ([]byte, error
 		Image:          wh.config.SidecarImage,
 		ServiceAccount: pod.Spec.ServiceAccountName,
 	}
-
 	patches = append(patches, addContainer(
 		pod.Spec.Containers,
-		[]corev1.Container{getEnvoySidecarContainerSpec(pod, &envoySidecarData)},
+		[]corev1.Container{getEnvoySidecarContainerSpec(&envoySidecarData)},
 		"/spec/containers")...,
 	)
 
--- pkg/injector/sidecar.go
+++ pkg/injector/sidecar.go
@@ -13,7 +13,7 @@ const (
 	envoyBootstrapConfigFile  = "/etc/envoy/bootstrap.yaml"
 )
 
-func getEnvoySidecarContainerSpec(pod *corev1.Pod, data *EnvoySidecarData) corev1.Container {
+func getEnvoySidecarContainerSpec(data *EnvoySidecarData) corev1.Container {
 	container := corev1.Container{
 		Name:            data.Name,
 		Image:           data.Image,
