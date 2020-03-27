c420db95c8c08316128f68d9d5794733ae13fd2f
Delyan Raychev <delyan.raychev@microsoft.com>
2020-03-27 11:39:57 -0700

    kubernetes: Carving out Kubernetes event handlers in their own package so we can reuse them (#371)
    * kubernetes: Carving out Kubernetes event handlers in their own package so we can reuse them
    
    * Delete emtpy files
    
    * Adding comments

---
+++ pkg/kubernetes/event_handlers.go
@@ -0,0 +1,54 @@
+package kubernetes
+
+import (
+	"reflect"
+
+	"github.com/golang/glog"
+	"github.com/open-service-mesh/osm/pkg/log/level"
+	"k8s.io/client-go/tools/cache"
+)
+
+// GetKubernetesEventHandlers creates Kubernetes events handlers.
+func GetKubernetesEventHandlers(informerName string, providerName string, announcements chan interface{}) cache.ResourceEventHandlerFuncs {
+	return cache.ResourceEventHandlerFuncs{
+		AddFunc:    Add(informerName, providerName, announcements),
+		UpdateFunc: Update(informerName, providerName, announcements),
+		DeleteFunc: Delete(informerName, providerName, announcements),
+	}
+}
+
+// Add a new item to Kubernetes caches from an incoming Kubernetes event.
+func Add(informerName string, providerName string, announce chan interface{}) func(obj interface{}) {
+	return func(obj interface{}) {
+		glog.V(level.Trace).Infof("[%s][%s] Add event: %+v", providerName, informerName, obj)
+		announce <- Event{
+			Type:  CreateEvent,
+			Value: obj,
+		}
+	}
+}
+
+// Update caches with an incoming Kubernetes event.
+func Update(informerName string, providerName string, announce chan interface{}) func(oldObj, newObj interface{}) {
+	return func(oldObj, newObj interface{}) {
+		glog.V(level.Trace).Infof("[%s][%s] Update event %+v", providerName, informerName, oldObj)
+		if reflect.DeepEqual(oldObj, newObj) {
+			return
+		}
+		announce <- Event{
+			Type:  UpdateEvent,
+			Value: newObj,
+		}
+	}
+}
+
+// Delete Kubernetes cache from an incoming Kubernetes event.
+func Delete(informerName string, providerName string, announce chan interface{}) func(obj interface{}) {
+	return func(obj interface{}) {
+		glog.V(level.Trace).Infof("[%s][%s] Delete event: %+v", providerName, informerName, obj)
+		announce <- Event{
+			Type:  DeleteEvent,
+			Value: obj,
+		}
+	}
+}
