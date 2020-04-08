995f4c8b9056bf26c9f9e53ab44f98b3ba756bb9
Delyan Raychev <delyan.raychev@microsoft.com>
2020-04-08 14:44:38 -0700

    cert: Adding --genca flag dedicated to CA creation (#418)
    * cert: Adding --genca flag dedicated to CA creation

--- demo/gen-ca.sh
+++ demo/gen-ca.sh
@@ -11,21 +11,13 @@ CRT="$DIR/root-cert.pem"
 
 mkdir -p "$DIR"
 
-openssl req -x509 -sha256 -nodes -days 365 \
-        -newkey rsa:2048 \
-        -subj "/CN=$(uuidgen).$K8S_NAMESPACE.azure.mesh/O=Az Mesh/C=US" \
-        -keyout "$KEY"  \
-        -out "$CRT"
-
+./bin/cert \
+    --caPEMFileOut="${CRT}" \
+    --caKeyPEMFileOut="${KEY}" \
+    --org="Azure Mesh" \
+    --genca=true
 
 echo -e "Creating configmap for root cert"
 
 kubectl -n "$K8S_NAMESPACE" create configmap "ca-rootcertpemstore" --from-file="$CRT"
 kubectl -n "$K8S_NAMESPACE" create configmap "ca-rootkeypemstore" --from-file="$KEY"
-
-exit 0
-
-./bin/cert \
-    --caPEMFileOut="$CRT" \
-    --caKeyPEMFileOut="$KEY" \
-    --org="Azure Mesh"
