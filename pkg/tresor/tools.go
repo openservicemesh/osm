ab8459bfefd91d86cef2df9e2e7d12420b0d4d4d
Delyan Raychev <delyan.raychev@microsoft.com>
2020-04-09 12:37:26 -0700

    tresor: Delete unused makeTemplate()

--- pkg/tresor/tools.go
+++ pkg/tresor/tools.go
@@ -2,13 +2,9 @@ package tresor
 
 import (
 	"bytes"
-	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
-	"crypto/x509/pkix"
 	pemEnc "encoding/pem"
-	"math/big"
-	"time"
 
 	"github.com/open-service-mesh/osm/pkg/tresor/pem"
 	"github.com/pkg/errors"
@@ -32,33 +28,4 @@ func encodeKey(priv *rsa.PrivateKey) (pem.PrivateKey, error) {
 		return nil, errors.Wrap(err, errEncodeKey.Error())
 	}
 	return keyOut.Bytes(), nil
-}
-
-func makeTemplate(host string, org string, validity time.Duration) (*x509.Certificate, error) {
-	// Validity duration of the certificate
-	notBefore := time.Now()
-	notAfter := notBefore.Add(validity)
-
-	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
-	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
-	if err != nil {
-		return nil, errors.Wrap(err, errGeneratingSerialNumber.Error())
-	}
-
-	template := x509.Certificate{
-		SerialNumber: serialNumber,
-		DNSNames:     []string{host},
-		Subject: pkix.Name{
-			CommonName:   host,
-			Organization: []string{org},
-		},
-		NotBefore: notBefore,
-		NotAfter:  notAfter,
-
-		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
-		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
-		BasicConstraintsValid: true,
-	}
-
-	return &template, nil
 }
