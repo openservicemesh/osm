9c2119b466f0d3cd8b6d1985f33f330d3f9d5eb4
Delyan Raychev <delyan.raychev@microsoft.com>
2020-04-17 18:28:44 -0700

    certificates: Change format of CN to UUID.svc.ns...

--- pkg/utils/certificate.go
+++ pkg/utils/certificate.go
@@ -1,55 +1,27 @@
 package utils
 
 import (
-	"fmt"
 	"strings"
 
-	"github.com/open-service-mesh/osm/pkg/constants"
+	"github.com/open-service-mesh/osm/pkg/certificate"
 )
 
 const (
 	domainDelimiter = "."
 )
 
-// NewCertCommonNameWithUUID returns a newly generated CommonName for a certificate of the form: <UUID>;<domain>
-func NewCertCommonNameWithUUID(serviceAccountName, namespace, subDomain string) string {
-	return fmt.Sprintf("%s%s%s.%s.%s", NewUUIDStr(), constants.CertCommonNameUUIDServiceDelimiter, serviceAccountName, namespace, subDomain)
+// NewCertCommonNameWithUUID returns a newly generated CommonName for a certificate of the form: <UUID>.<domain>
+func NewCertCommonNameWithUUID(serviceAccountName, namespace, subDomain string) certificate.CommonName {
+	return certificate.CommonName(strings.Join([]string{NewUUIDStr(), serviceAccountName, namespace, subDomain}, domainDelimiter))
 }
 
 // GetCertificateCommonNameMeta returns the metadata information in the CommonName of a certificate
-func GetCertificateCommonNameMeta(cn string) CertificateCommonNameMeta {
-	var cnMeta CertificateCommonNameMeta
-	var cnWithUUIDStripped string
-
-	if strings.Contains(cn, constants.CertCommonNameUUIDServiceDelimiter) {
-		tmp := strings.Split(cn, constants.CertCommonNameUUIDServiceDelimiter)
-		cnMeta.UUID = tmp[0]
-		cnWithUUIDStripped = tmp[1]
-	} else {
-		cnWithUUIDStripped = cn
+func GetCertificateCommonNameMeta(cn certificate.CommonName) CertificateCommonNameMeta {
+	chunks := strings.Split(cn.String(), domainDelimiter)
+	return CertificateCommonNameMeta{
+		UUID:               chunks[0],
+		ServiceAccountName: chunks[1],
+		Namespace:          chunks[2],
+		SubDomain:          strings.Join(chunks[3:], domainDelimiter),
 	}
-
-	maxSplits := 3 // <service_account_name>.<namespace>.<sub_domain>
-	tmp := strings.SplitN(cnWithUUIDStripped, domainDelimiter, maxSplits)
-	if len(tmp) != maxSplits {
-		panic("Certificate Common Name domain should be of the form <service_account_name>.<namespace>.<sub_domain>")
-	}
-
-	cnMeta.ServiceAccountName = tmp[0]
-	cnMeta.Namespace = tmp[1]
-	cnMeta.SubDomain = tmp[2]
-
-	return cnMeta
-
-}
-
-// GetCertCommonNameWithoutUUID returns the CommonName with the UUID stripped if the UUID delimiter exists, otherwise returns the existing name
-func GetCertCommonNameWithoutUUID(cn string) string {
-	if strings.Contains(cn, constants.CertCommonNameUUIDServiceDelimiter) {
-		tmp := strings.Split(cn, constants.CertCommonNameUUIDServiceDelimiter)
-		cnWithUUIDStripped := tmp[1]
-		return cnWithUUIDStripped
-	}
-
-	return cn
 }
