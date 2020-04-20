7a3dde35810449fa6fede81c4b2dc72dbbc6acf0
Delyan Raychev <delyan.raychev@microsoft.com>
2020-04-20 14:10:20 -0700

    tresor: Create fake Cert Manager for testing

---
+++ pkg/tresor/fake.go
@@ -0,0 +1,21 @@
+package tresor
+
+import (
+	"time"
+
+	"github.com/open-service-mesh/osm/pkg/certificate"
+)
+
+// NewFakeCertManager creates a fake CertManager used for testing.
+func NewFakeCertManager() *CertManager {
+	ca, err := NewCA(1 * time.Hour)
+	if err != nil {
+		log.Error().Err(err).Msg("Error creating CA for fake cert manager")
+	}
+	return &CertManager{
+		ca:            ca,
+		validity:      1 * time.Hour,
+		announcements: make(chan interface{}),
+		cache:         make(map[certificate.CommonName]Certificate),
+	}
+}
