ea786cc6f3e64dbbdb4a5cacd97b9724c5a46d8c
Shashank Ram <shashank08@gmail.com>
2020-04-20 14:38:44 -0700

    demo: Allow tail scripts to work with different backends (#491)

--- demo/tail-bookstore-envoy.sh
+++ demo/tail-bookstore-envoy.sh
@@ -3,6 +3,14 @@
 # shellcheck disable=SC1091
 source .env
 
-POD="$(kubectl get pods -n "$BOOKSTORE_NAMESPACE" --show-labels --selector app=bookstore-1 --no-headers | grep -v 'Terminating' | awk '{print $1}' | head -n1)"
+backend="$1"
+thisScript="$(dirname "$0")/$(basename "$0")"
+
+if [ -z "$backend" ]; then
+    echo "Usage: $thisScript <backend-name>"
+    exit 1
+fi
+
+POD="$(kubectl get pods -n "$BOOKSTORE_NAMESPACE" --show-labels --selector app="$backend" --no-headers | grep -v 'Terminating' | awk '{print $1}' | head -n1)"
 
 kubectl logs "$POD" -n "$BOOKSTORE_NAMESPACE" -c envoyproxy --tail=100 -f
