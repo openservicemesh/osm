7b3af307ce4a49d31da680e0a1cdb9b436792e60
Delyan Raychev <delyan.raychev@microsoft.com>
2020-04-21 12:11:38 -0700

    tresor: Tests for the DecodePEMCertificate tool

--- pkg/tresor/ca_test.go
+++ pkg/tresor/ca_test.go
@@ -13,9 +13,13 @@ var _ = Describe("Test creation of a new CA", func() {
 		cert, err := NewCA(2 * time.Second)
 		It("should create a new CA", func() {
 			Expect(err).ToNot(HaveOccurred())
-			Expect(cert.x509Cert.NotAfter.Sub(cert.x509Cert.NotBefore)).To(Equal(2 * time.Second))
-			Expect(cert.x509Cert.KeyUsage).To(Equal(x509.KeyUsageCertSign | x509.KeyUsageCRLSign))
-			Expect(cert.x509Cert.IsCA).To(BeTrue())
+
+			x509Cert, err := DecodePEMCertificate(cert.GetCertificateChain())
+			Expect(err).ToNot(HaveOccurred())
+
+			Expect(x509Cert.NotAfter.Sub(x509Cert.NotBefore)).To(Equal(2 * time.Second))
+			Expect(x509Cert.KeyUsage).To(Equal(x509.KeyUsageCertSign | x509.KeyUsageCRLSign))
+			Expect(x509Cert.IsCA).To(BeTrue())
 		})
 	})
 })
