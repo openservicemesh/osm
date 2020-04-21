9d42b7a137420e0c9bd498280133336ecf83ff43
Delyan Raychev <delyan.raychev@microsoft.com>
2020-04-21 12:14:53 -0700

    tresor: Export simplified LoadCertificateFromFile and LoadPrivateKeyFromFile helpers

--- pkg/tresor/certificate.go
+++ pkg/tresor/certificate.go
@@ -8,7 +8,7 @@ import (
 
 // GetName implements certificate.Certificater and returns the CN of the cert.
 func (c Certificate) GetName() string {
-	return c.name
+	return c.commonName.String()
 }
 
 // GetCertificateChain implements certificate.Certificater and returns the certificate chain.
@@ -23,49 +23,59 @@ func (c Certificate) GetPrivateKey() []byte {
 
 // GetIssuingCA implements certificate.Certificater and returns the root certificate for the given cert.
 func (c Certificate) GetIssuingCA() []byte {
-	if c.ca == nil {
-		log.Info().Msgf("No root certificate available for certificate with CN=%s", c.x509Cert.Subject.CommonName)
+	if c.issuingCA == nil {
+		log.Fatal().Msgf("No issuing CA available for cert %s", c.commonName)
 		return nil
 	}
-	cert, err := encodeCertDERtoPEM(c.ca.x509Cert.Raw)
-	if err != nil {
-		log.Error().Err(err).Msg("Error PEM encoding root certificate")
-	}
-	return cert
+
+	return c.issuingCA.GetCertificateChain()
 }
 
 // LoadCA loads the certificate and its key from the supplied PEM files.
 func LoadCA(certFilePEM string, keyFilePEM string) (*Certificate, error) {
-	x509Cert, pemCert, err := certFromFile(certFilePEM)
+	pemCert, err := LoadCertificateFromFile(certFilePEM)
 	if err != nil {
 		log.Error().Err(err).Msgf("Error loading certificate from file %s", certFilePEM)
 		return nil, err
 	}
 
-	rsaKey, pemKey, err := privKeyFromFile(keyFilePEM)
+	pemKey, err := LoadPrivateKeyFromFile(keyFilePEM)
 	if err != nil {
 		log.Error().Err(err).Msgf("Error loading private key from file %s", keyFilePEM)
 		return nil, err
 	}
 
+	x509RootCert, err := DecodePEMCertificate(pemCert)
+	if err != nil {
+		log.Error().Err(err).Msgf("Error converting certificate from PEM to x509 - CN=%s", rootCertificateName)
+	}
+
 	rootCertificate := Certificate{
-		name:       rootCertificateName,
+		commonName: rootCertificateName,
 		certChain:  pemCert,
 		privateKey: pemKey,
-		x509Cert:   x509Cert,
-		rsaKey:     rsaKey,
-		ca:         nil, // this is the CA itself
+		expiration: x509RootCert.NotAfter,
 	}
 	return &rootCertificate, nil
 }
 
 // NewCertManager creates a new CertManager with the passed CA and CA Private Key
 func NewCertManager(ca *Certificate, validity time.Duration) (*CertManager, error) {
-	cm := CertManager{
-		ca:            ca,
-		validity:      validity,
-		announcements: make(chan interface{}),
-		cache:         make(map[certificate.CommonName]Certificate),
+	if ca == nil {
+		return nil, errNoIssuingCA
 	}
-	return &cm, nil
+
+	return &CertManager{
+		// The root certificate signing all newly issued certificates
+		ca: ca,
+
+		// Newly issued certificates will be valid for this duration
+		certificatesValidFor: validity,
+
+		// Channel used to inform other components of cert changes (rotation etc.)
+		announcements: make(chan interface{}),
+
+		// Certificate cache
+		cache: make(map[certificate.CommonName]Certificate),
+	}, nil
 }
