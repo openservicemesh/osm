d1901937f21dfce0cb0d696fbd8dac78dde697eb
Delyan Raychev <delyan.raychev@microsoft.com>
2020-04-23 10:44:04 -0700

    injector: Move envoyBootstrapConfigTmpl to test

--- pkg/injector/config_test.go
+++ pkg/injector/config_test.go
@@ -5,6 +5,57 @@ import (
 	. "github.com/onsi/gomega"
 )
 
+const envoyBootstrapConfigTmpl = `
+admin:
+  access_log_path: /dev/stdout
+  address:
+    socket_address:
+      address: 0.0.0.0
+      port_value: '{{.EnvoyAdminPort}}'
+dynamic_resources:
+  ads_config:
+    api_type: GRPC
+    grpc_services:
+    - envoy_grpc:
+        cluster_name: '{{.XDSClusterName}}'
+    set_node_on_first_message_only: true
+  cds_config:
+    ads: {}
+  lds_config:
+    ads: {}
+static_resources:
+  clusters:
+  - connect_timeout: 0.25s
+    http2_protocol_options: {}
+    load_assignment:
+      cluster_name: '{{.XDSClusterName}}'
+      endpoints:
+      - lb_endpoints:
+        - endpoint:
+            address:
+              socket_address:
+                address: '{{.XDSHost}}'
+                port_value: '{{.XDSPort}}'
+    name: '{{.XDSClusterName}}'
+    tls_context:
+      common_tls_context:
+        alpn_protocols:
+        - h2
+        tls_certificates:
+        - certificate_chain:
+            filename: '{{.CertPath}}'
+          private_key:
+            filename: '{{.KeyPath}}'
+        tls_params:
+          cipher_suites: '[ECDHE-ECDSA-AES128-GCM-SHA256|ECDHE-ECDSA-CHACHA20-POLY1305]'
+          tls_maximum_protocol_version: TLSv1_3
+          tls_minimum_protocol_version: TLSv1_2
+        validation_context:
+          trusted_ca:
+            filename: '{{.RootCertPath}}'
+    type: LOGICAL_DNS
+`
+
 var _ = Describe("Test Envoy configuration creation", func() {
 	Context("create envoy config", func() {
 		It("creates envoy config", func() {
