565d09548e1462fea833140ec7b928c3e1e366f4
Delyan Raychev <delyan.raychev@microsoft.com>
2020-04-22 17:50:10 -0700

    injector: Reformat Envoy config template

--- pkg/injector/config.go
+++ pkg/injector/config.go
@@ -22,51 +22,53 @@ const (
 
 const envoyBootstrapConfigTmpl = `
 admin:
-  access_log_path: "/dev/stdout"
+  access_log_path: /dev/stdout
   address:
-    socket_address: {address: 0.0.0.0, port_value: {{.EnvoyAdminPort}}}
-
+    socket_address:
+      address: 0.0.0.0
+      port_value: '{{.EnvoyAdminPort}}'
 dynamic_resources:
   ads_config:
     api_type: GRPC
     grpc_services:
     - envoy_grpc:
-        cluster_name: {{.XDSClusterName}}
-    set_node_on_first_message_only: true
+        cluster_name: '{{.XDSClusterName}}'
+    set_node_on_first_message_only: "true"
   cds_config:
     ads: {}
   lds_config:
     ads: {}
-
 static_resources:
   clusters:
-
-  - name: {{.XDSClusterName}}
-    connect_timeout: 0.25s
-    type: LOGICAL_DNS
+  - connect_timeout: 0.25s
     http2_protocol_options: {}
-    tls_context:
-      common_tls_context:
-        alpn_protocols:
-          - h2
-        validation_context:
-          trusted_ca: { filename: "{{.RootCertPath}}" }
-        tls_params:
-          tls_minimum_protocol_version: TLSv1_2
-          tls_maximum_protocol_version: TLSv1_3
-          cipher_suites: "[ECDHE-ECDSA-AES128-GCM-SHA256|ECDHE-ECDSA-CHACHA20-POLY1305]"
-        tls_certificates:
-          - certificate_chain: { filename: "{{.CertPath}}" }
-            private_key: { filename: "{{.KeyPath}}" }
     load_assignment:
-      cluster_name: {{.XDSClusterName}}
+      cluster_name: '{{.XDSClusterName}}'
       endpoints:
       - lb_endpoints:
         - endpoint:
             address:
               socket_address:
-                address: {{.XDSHost}}
-                port_value: {{.XDSPort}}
+                address: '{{.XDSHost}}'
+                port_value: '{{.XDSPort}}'
+    name: '{{.XDSClusterName}}'
+    tls_context:
+      common_tls_context:
+        alpn_protocols:
+        - h2
+        tls_certificates:
+        - certificate_chain:
+            filename: '{{.CertPath}}'
+          private_key:
+            filename: '{{.KeyPath}}'
+        tls_params:
+          cipher_suites: '[ECDHE-ECDSA-AES128-GCM-SHA256|ECDHE-ECDSA-CHACHA20-POLY1305]'
+          tls_maximum_protocol_version: TLSv1_3
+          tls_minimum_protocol_version: TLSv1_2
+        validation_context:
+          trusted_ca:
+            filename: '{{.RootCertPath}}'
+    type: LOGICAL_DNS
 `
 
 type envoyBootstrapConfigMeta struct {
