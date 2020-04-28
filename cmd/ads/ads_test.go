bcbba11a019bc1d3fad6a36e0bda6e71155700d1
Delyan Raychev <delyan.raychev@microsoft.com>
2020-04-28 11:47:05 -0700

    ads: Adding a test for CA bundle k8s secret creation

---
+++ cmd/ads/ads_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/open-service-mesh/osm/pkg/tresor"
+	testclient "k8s.io/client-go/kubernetes/fake"
+)
+
+var _ = Describe("Test creation of CA bundle k8s secret", func() {
+	Context("Testing createCABundleKubernetesSecret", func() {
+		It("creates a k8s secret", func() {
+
+			certManager := tresor.NewFakeCertManager()
+			secretName := "--secret--name--"
+			namespace := "--namespace--"
+			k8sClient := testclient.NewSimpleClientset()
+
+			err := createCABundleKubernetesSecret(k8sClient, certManager, namespace, secretName)
+			Expect(err).ToNot(HaveOccurred())
+
+			actual, err := k8sClient.CoreV1().Secrets(namespace).Get(context.Background(), secretName, v1.GetOptions{})
+			Expect(err).ToNot(HaveOccurred())
+			expected := "-----BEGIN CERTIFICATE-----\nMIIFWj"
+			stringPEM := string(actual.Data[tlsCAKey])[:len(expected)]
+			Expect(stringPEM).To(Equal(expected))
+		})
+	})
+})
