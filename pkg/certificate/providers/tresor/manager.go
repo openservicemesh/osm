1b5d61dc49c83b25257261fa7b6d86e6a9ca68f1
Delyan Raychev <delyan.raychev@microsoft.com>
2020-05-01 12:48:43 -0700

    tresor: Carve out NewCertificate from tresor/manager

--- pkg/certificate/providers/tresor/manager.go
+++ pkg/certificate/providers/tresor/manager.go
@@ -25,58 +25,8 @@ func (cm *CertManager) IssueCertificate(cn certificate.CommonName) (certificate.
 		return nil, errNoIssuingCA
 	}
 
-	certPrivKey, err := rsa.GenerateKey(rand.Reader, rsaBits)
+	certPEM, privKeyPEM, notAfter, err := NewCertificate(cn, cm.validityPeriod, cm.ca)
 	if err != nil {
-		log.Error().Err(err).Msgf("Error generating private key for certificate with CN=%s", cn)
-		return nil, errors.Wrap(err, errGeneratingPrivateKey.Error())
-	}
-
-	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
-	if err != nil {
-		return nil, errors.Wrap(err, errGeneratingSerialNumber.Error())
-	}
-
-	now := time.Now()
-	template := x509.Certificate{
-		SerialNumber: serialNumber,
-		DNSNames:     []string{string(cn)},
-		Subject: pkix.Name{
-			CommonName:   string(cn),
-			Organization: []string{org},
-		},
-		NotBefore: now,
-		NotAfter:  now.Add(cm.validityPeriod),
-
-		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
-		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
-		BasicConstraintsValid: true,
-	}
-
-	x509Root, err := DecodePEMCertificate(cm.ca.GetCertificateChain())
-	if err != nil {
-		log.Error().Err(err).Msg("Error decoding Root Certificate's PEM")
-	}
-
-	rsaKeyRoot, err := DecodePEMPrivateKey(cm.ca.GetPrivateKey())
-	if err != nil {
-		log.Error().Err(err).Msg("Error decoding Root Certificate's Private Key PEM ")
-	}
-
-	derBytes, err := x509.CreateCertificate(rand.Reader, &template, x509Root, &certPrivKey.PublicKey, rsaKeyRoot)
-	if err != nil {
-		log.Error().Err(err).Msgf("Error issuing x509.CreateCertificate command for CN=%s", template.Subject.CommonName)
-		return nil, errors.Wrap(err, errCreateCert.Error())
-	}
-
-	certPEM, err := encodeCertDERtoPEM(derBytes)
-	if err != nil {
-		log.Error().Err(err).Msgf("Error encoding certificate with CN=%s", template.Subject.CommonName)
-		return nil, err
-	}
-
-	privKeyPEM, err := encodeKeyDERtoPEM(certPrivKey)
-	if err != nil {
-		log.Error().Err(err).Msgf("Error encoding private key for certificate with CN=%s", template.Subject.CommonName)
 		return nil, err
 	}
 
@@ -85,7 +35,7 @@ func (cm *CertManager) IssueCertificate(cn certificate.CommonName) (certificate.
 		certChain:  certPEM,
 		privateKey: privKeyPEM,
 		issuingCA:  cm.ca,
-		expiration: template.NotAfter,
+		expiration: *notAfter,
 	}
 	cm.cache[cn] = cert
 	return cert, nil
@@ -94,4 +44,65 @@ func (cm *CertManager) IssueCertificate(cn certificate.CommonName) (certificate.
 // GetAnnouncementsChannel implements certificate.Manager and returns the channel on which the certificate manager announces changes made to certificates.
 func (cm CertManager) GetAnnouncementsChannel() <-chan interface{} {
 	return cm.announcements
+}
+
+func NewCertificate(cn certificate.CommonName, validityPeriod time.Duration, ca certificate.Certificater) ([]byte, []byte, *time.Time, error) {
+
+	certPrivKey, err := rsa.GenerateKey(rand.Reader, rsaBits)
+	if err != nil {
+		log.Error().Err(err).Msgf("Error generating private key for certificate with CN=%s", cn)
+		return nil, nil, nil, errors.Wrap(err, errGeneratingPrivateKey.Error())
+	}
+
+	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
+	if err != nil {
+		return nil, nil, nil, errors.Wrap(err, errGeneratingSerialNumber.Error())
+	}
+
+	now := time.Now()
+	notAfter := now.Add(validityPeriod)
+	template := x509.Certificate{
+		SerialNumber: serialNumber,
+		DNSNames:     []string{string(cn)},
+		Subject: pkix.Name{
+			CommonName:   string(cn),
+			Organization: []string{org},
+		},
+		NotBefore: now,
+		NotAfter:  notAfter,
+
+		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
+		BasicConstraintsValid: true,
+	}
+
+	x509Root, err := DecodePEMCertificate(ca.GetCertificateChain())
+	if err != nil {
+		log.Error().Err(err).Msg("Error decoding Root Certificate's PEM")
+	}
+
+	rsaKeyRoot, err := DecodePEMPrivateKey(ca.GetPrivateKey())
+	if err != nil {
+		log.Error().Err(err).Msg("Error decoding Root Certificate's Private Key PEM ")
+	}
+
+	derBytes, err := x509.CreateCertificate(rand.Reader, &template, x509Root, &certPrivKey.PublicKey, rsaKeyRoot)
+	if err != nil {
+		log.Error().Err(err).Msgf("Error issuing x509.CreateCertificate command for CN=%s", template.Subject.CommonName)
+		return nil, nil, nil, errors.Wrap(err, errCreateCert.Error())
+	}
+
+	certPEM, err := encodeCertDERtoPEM(derBytes)
+	if err != nil {
+		log.Error().Err(err).Msgf("Error encoding certificate with CN=%s", template.Subject.CommonName)
+		return nil, nil, nil, err
+	}
+
+	privKeyPEM, err := encodeKeyDERtoPEM(certPrivKey)
+	if err != nil {
+		log.Error().Err(err).Msgf("Error encoding private key for certificate with CN=%s", template.Subject.CommonName)
+		return nil, nil, nil, err
+	}
+
+	return certPEM, privKeyPEM, &notAfter, err
 }
