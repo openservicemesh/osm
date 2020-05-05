65ff2353ecaccc8d955f6cb51abb4a3c05384ab3
Delyan Raychev <delyan.raychev@microsoft.com>
2020-05-04 21:18:09 -0700

    kube: Adding NewProvider - Kubernetes Endpoints Provider for tests

---
+++ pkg/providers/kube/client_test.go
@@ -0,0 +1,29 @@
+package kube
+
+import (
+	"net"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/open-service-mesh/osm/pkg/endpoint"
+)
+
+var _ = Describe("Test Kubernetes Provider", func() {
+	Context("Testing ListServicesForServiceAccount", func() {
+		It("returns empty list", func() {
+			c := NewFakeProvider()
+			Ω(func() { c.ListEndpointsForService("blah") }).Should(Panic())
+
+		})
+
+		It("returns the services for a given service account", func() {
+			c := NewFakeProvider()
+			actual := c.ListEndpointsForService("default/bookstore")
+			expected := []endpoint.Endpoint{{
+				IP:   net.IP("8.8.8.8"),
+				Port: 8888,
+			}}
+			Expect(actual).To(Equal(expected))
+		})
+	})
+})
