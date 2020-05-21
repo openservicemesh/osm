364192accc7e0a76b5b7f7b264b45aaa0b21d42d
Shashank Ram <shashank08@gmail.com>
2020-05-21 16:33:33 -0700

    envoy: refactor xdsutil helpers to make it unit testable (#695)

--- pkg/envoy/xdsutil_test.go
+++ pkg/envoy/xdsutil_test.go
@@ -5,8 +5,7 @@ import (
 
 	envoy_api_v2_auth "github.com/envoyproxy/go-control-plane/envoy/api/v2/auth"
 	envoy_api_v2_core "github.com/envoyproxy/go-control-plane/envoy/api/v2/core"
-	"github.com/golang/protobuf/ptypes"
-	"github.com/golang/protobuf/ptypes/any"
+	"github.com/golang/protobuf/ptypes/wrappers"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -37,18 +36,50 @@ var _ = Describe("Test Envoy tools", func() {
 		})
 	})
 
+	Context("Test GetDownstreamTLSContext()", func() {
+		It("should return TLS context", func() {
+			tlsContext := GetDownstreamTLSContext(tests.BookstoreService)
+
+			expectedTLSContext := envoy_api_v2_auth.DownstreamTlsContext{
+				CommonTlsContext: &envoy_api_v2_auth.CommonTlsContext{
+					TlsParams: &envoy_api_v2_auth.TlsParameters{
+						TlsMinimumProtocolVersion: 3,
+						TlsMaximumProtocolVersion: 4,
+					},
+					TlsCertificates: nil,
+					TlsCertificateSdsSecretConfigs: []*envoy_api_v2_auth.SdsSecretConfig{{
+						Name: "service-cert:default/bookstore",
+						SdsConfig: &envoy_api_v2_core.ConfigSource{
+							ConfigSourceSpecifier: &envoy_api_v2_core.ConfigSource_Ads{
+								Ads: &envoy_api_v2_core.AggregatedConfigSource{},
+							},
+						},
+					}},
+					ValidationContextType: &envoy_api_v2_auth.CommonTlsContext_ValidationContextSdsSecretConfig{
+						ValidationContextSdsSecretConfig: &envoy_api_v2_auth.SdsSecretConfig{
+							Name: fmt.Sprintf("%s%s%s", RootCertPrefix, Separator, "default/bookstore"),
+							SdsConfig: &envoy_api_v2_core.ConfigSource{
+								ConfigSourceSpecifier: &envoy_api_v2_core.ConfigSource_Ads{
+									Ads: &envoy_api_v2_core.AggregatedConfigSource{},
+								},
+							},
+						},
+					},
+					AlpnProtocols: nil,
+				},
+				RequireClientCertificate: &wrappers.BoolValue{Value: true},
+			}
+			Expect(tlsContext.CommonTlsContext.TlsParams).To(Equal(expectedTLSContext.CommonTlsContext.TlsParams))
+			Expect(tlsContext.CommonTlsContext.TlsCertificates).To(Equal(expectedTLSContext.CommonTlsContext.TlsCertificates))
+			Expect(tlsContext.CommonTlsContext.TlsCertificateSdsSecretConfigs).To(Equal(expectedTLSContext.CommonTlsContext.TlsCertificateSdsSecretConfigs))
+			Expect(tlsContext.CommonTlsContext.ValidationContextType).To(Equal(expectedTLSContext.CommonTlsContext.ValidationContextType))
+			Expect(*tlsContext).To(Equal(expectedTLSContext))
+		})
+	})
+
 	Context("Test GetUpstreamTLSContext()", func() {
 		It("should return TLS context", func() {
-			actual := GetUpstreamTLSContext(tests.BookstoreService)
-			expected := &any.Any{
-				TypeUrl: string(TypeUpstreamTLSContext),
-				Value:   []byte{10, 79, 10, 4, 8, 3, 16, 4, 50, 36, 10, 30, 115, 101, 114, 118, 105, 99, 101, 45, 99, 101, 114, 116, 58, 100, 101, 102, 97, 117, 108, 116, 47, 98, 111, 111, 107, 115, 116, 111, 114, 101, 18, 2, 26, 0, 58, 33, 10, 27, 114, 111, 111, 116, 45, 99, 101, 114, 116, 58, 100, 101, 102, 97, 117, 108, 116, 47, 98, 111, 111, 107, 115, 116, 111, 114, 101, 18, 2, 26, 0, 18, 17, 100, 101, 102, 97, 117, 108, 116, 47, 98, 111, 111, 107, 115, 116, 111, 114, 101},
-			}
-			Expect(actual).To(Equal(expected))
-
-			tlsContext := envoy_api_v2_auth.UpstreamTlsContext{}
-			err := ptypes.UnmarshalAny(actual, &tlsContext)
-			Expect(err).ToNot(HaveOccurred())
+			tlsContext := GetUpstreamTLSContext(tests.BookstoreService)
 
 			expectedTLSContext := envoy_api_v2_auth.UpstreamTlsContext{
 				CommonTlsContext: &envoy_api_v2_auth.CommonTlsContext{
@@ -84,7 +115,7 @@ var _ = Describe("Test Envoy tools", func() {
 			Expect(tlsContext.CommonTlsContext.TlsCertificates).To(Equal(expectedTLSContext.CommonTlsContext.TlsCertificates))
 			Expect(tlsContext.CommonTlsContext.TlsCertificateSdsSecretConfigs).To(Equal(expectedTLSContext.CommonTlsContext.TlsCertificateSdsSecretConfigs))
 			Expect(tlsContext.CommonTlsContext.ValidationContextType).To(Equal(expectedTLSContext.CommonTlsContext.ValidationContextType))
-			Expect(tlsContext).To(Equal(expectedTLSContext))
+			Expect(*tlsContext).To(Equal(expectedTLSContext))
 		})
 	})
 
