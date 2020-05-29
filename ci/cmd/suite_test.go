d9925ec79378004897ee8b539c6e8a3f964e2a57
Delyan Raychev <delyan.raychev@microsoft.com>
2020-05-29 11:02:09 -0700

    ci: Show less logs on failure (#729)
    We know that when common.Success or common.Failure are emitted in stdout by bookbuyer or bookthief - this is all we care about - the logs after that do not matter much.
    
    So let's cut these out to lower the amount of logs we show in CI and make this easier to scroll through.

---
+++ ci/cmd/suite_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestEndpoints(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Maestro Test Suite")
+}
