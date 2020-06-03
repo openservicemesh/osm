3a28a73f1dd465bf211a644e3ef6b54e4d989a45
Delyan Raychev <delyan.raychev@microsoft.com>
2020-06-02 19:21:17 -0700

    Add a Book Warehouse to demo

--- demo/clean-kubernetes.sh
+++ demo/clean-kubernetes.sh
@@ -8,7 +8,7 @@ source .env
 WEBHOOK_NAME="osm-webhook-$OSM_ID"
 
 kubectl delete mutatingwebhookconfiguration "$WEBHOOK_NAME" --ignore-not-found=true
-for ns in "$BOOKBUYER_NAMESPACE" "$BOOKSTORE_NAMESPACE" "$BOOKTHIEF_NAMESPACE" "$K8S_NAMESPACE"; do
+for ns in "$BOOKWAREHOUSE_NAMESPACE" "$BOOKBUYER_NAMESPACE" "$BOOKSTORE_NAMESPACE" "$BOOKTHIEF_NAMESPACE" "$K8S_NAMESPACE"; do
     kubectl delete namespace "$ns" || true
 done
 kubectl delete clusterrole osm || true
