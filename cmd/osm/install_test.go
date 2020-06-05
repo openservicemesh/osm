c357c1ba5267a7537346b39b3e1076e53f938ad2
Jon Huhn <huhnjon@gmail.com>
2020-06-05 14:40:21 -0500

    ref(chart): replace .Values.namespace with .Release.Namespace

--- cmd/osm/install_test.go
+++ cmd/osm/install_test.go
@@ -77,7 +77,6 @@ func TestInstallRun(t *testing.T) {
 				"name": testRegistrySecret,
 			},
 		},
-		"namespace":                  settings.Namespace(),
 		"serviceCertValidityMinutes": int64(1),
 		"vault": map[string]interface{}{
 			"host":     "",
@@ -155,7 +154,6 @@ func TestInstallRunVault(t *testing.T) {
 				"name": testRegistrySecret,
 			},
 		},
-		"namespace":                  settings.Namespace(),
 		"serviceCertValidityMinutes": int64(1),
 		"vault": map[string]interface{}{
 			"host":     testVaultHost,
@@ -239,7 +237,6 @@ func TestResolveValues(t *testing.T) {
 				"name": testRegistrySecret,
 			},
 		},
-		"namespace":                  settings.Namespace(),
 		"serviceCertValidityMinutes": int64(1),
 		"vault": map[string]interface{}{
 			"host":     testVaultHost,
