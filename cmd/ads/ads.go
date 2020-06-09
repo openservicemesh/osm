de9138ecf5174d93cab6d08a219a5966e37fae14
Shashank Ram <shashank08@gmail.com>
2020-06-09 13:23:24 -0700

    Set cmd line bool options correctly (#793)
    The golang flags package works different with bool options.
    A bool option does not require a value of true/false. The
    presence of the option is sufficient to determine if the
    option is set. Moreover, the `flags` package expects values
    for bool options to be passed using `-option=value` as opposed
    to `--option=value`. This results in confusion and is prone to
    bugs, so use `--option` without a value to signify the setting
    of a boolean option.
    
    This change adds a new boolean option to the cli to disable
    SMI access control policy and updates the debug server option
    to be set correctly.

--- cmd/ads/ads.go
+++ cmd/ads/ads.go
@@ -86,7 +86,7 @@ func init() {
 	flags.StringVar(&webhookName, "webhookName", "", "Name of the MutatingWebhookConfiguration to be created by ADS")
 	flags.IntVar(&serviceCertValidityMinutes, "serviceCertValidityMinutes", defaultServiceCertValidityMinutes, "Certificate validityPeriod duration in minutes")
 	flags.StringVar(&caBundleSecretName, caBundleSecretNameCLIParam, "", "Name of the Kubernetes Secret for the OSM CA bundle")
-	flags.BoolVar(&enableDebugServer, "enableDebugServer", false, "Set to true to enable OSM debug HTTP server")
+	flags.BoolVar(&enableDebugServer, "enableDebugServer", false, "Enable OSM debug HTTP server")
 
 	// sidecar injector options
 	flags.BoolVar(&injectorConfig.DefaultInjection, "default-injection", true, "Enable sidecar injection by default")
--- cmd/cli/install.go
+++ cmd/cli/install.go
@@ -47,19 +47,20 @@ const (
 var chartTGZSource string
 
 type installCmd struct {
-	out                        io.Writer
-	containerRegistry          string
-	containerRegistrySecret    string
-	chartPath                  string
-	osmImageTag                string
-	certManager                string
-	vaultHost                  string
-	vaultProtocol              string
-	vaultToken                 string
-	vaultRole                  string
-	serviceCertValidityMinutes int
-	prometheusRetentionTime    string
-	enableDebugServer          bool
+	out                           io.Writer
+	containerRegistry             string
+	containerRegistrySecret       string
+	chartPath                     string
+	osmImageTag                   string
+	certManager                   string
+	vaultHost                     string
+	vaultProtocol                 string
+	vaultToken                    string
+	vaultRole                     string
+	serviceCertValidityMinutes    int
+	prometheusRetentionTime       string
+	enableDebugServer             bool
+	disableSMIAccessControlPolicy bool
 }
 
 func newInstallCmd(config *helm.Configuration, out io.Writer) *cobra.Command {
@@ -89,7 +90,8 @@ func newInstallCmd(config *helm.Configuration, out io.Writer) *cobra.Command {
 	f.StringVar(&inst.vaultRole, "vault-role", "open-service-mesh", "Vault role to be used by Open Service Mesh")
 	f.IntVar(&inst.serviceCertValidityMinutes, "service-cert-validity-minutes", int(1), "Certificate TTL in minutes")
 	f.StringVar(&inst.prometheusRetentionTime, "promethues-retention-time", constants.PrometheusDefaultRetentionTime, "Duration for which data will be retained in prometheus")
-	f.BoolVar(&inst.enableDebugServer, "enable-debug-server", true, "Set to true to enable the debug HTTP server")
+	f.BoolVar(&inst.enableDebugServer, "enable-debug-server", false, "Enable the debug HTTP server")
+	f.BoolVar(&inst.disableSMIAccessControlPolicy, "disable-smi-access-control-policy", false, "Disable SMI access control policy")
 
 	return cmd
 }
@@ -148,6 +150,7 @@ func (i *installCmd) resolveValues() (map[string]interface{}, error) {
 		fmt.Sprintf("serviceCertValidityMinutes=%d", i.serviceCertValidityMinutes),
 		fmt.Sprintf("prometheus.retention.time=%s", i.prometheusRetentionTime),
 		fmt.Sprintf("enableDebugServer=%t", i.enableDebugServer),
+		fmt.Sprintf("disableSMIAccessControlPolicy=%t", i.disableSMIAccessControlPolicy),
 	}
 
 	for _, val := range valuesConfig {
