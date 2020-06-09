316f8d8f99eae90fd831b4a2da17d3c10935559c
Delyan Raychev <delyan.raychev@microsoft.com>
2020-06-09 14:57:48 -0700

    debug: Adding a helper script to port forward OSM's debug facility to localhost

---
+++ scripts/port-forward-osm-debug.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# shellcheck disable=SC1091
+set -aueo pipefail
+
+source .env
+
+OSM_POD=$(kubectl get pods -n "$K8S_NAMESPACE" --no-headers  --selector app=ads | awk '{print $1}')
+
+kubectl port-forward -n "$K8S_NAMESPACE" "$OSM_POD"  9091:9091
