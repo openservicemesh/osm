6f2db4862b0770bccc0291eee9fc84a8e8ccdd2e
Delyan Raychev <delyan.raychev@microsoft.com>
2020-06-10 18:19:20 -0700

    ads: Send auth.Secret to all Envoys on announcement

---
+++ pkg/envoy/ads/response_test.go
@@ -0,0 +1,114 @@
+package ads
+
+import (
+	"context"
+	"fmt"
+
+	envoy_api_v2 "github.com/envoyproxy/go-control-plane/envoy/api/v2"
+	envoy_api_v2_auth "github.com/envoyproxy/go-control-plane/envoy/api/v2/auth"
+	"github.com/golang/protobuf/ptypes"
+	"github.com/google/uuid"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/open-service-mesh/osm/pkg/catalog"
+	"github.com/open-service-mesh/osm/pkg/certificate"
+	"github.com/open-service-mesh/osm/pkg/constants"
+	"github.com/open-service-mesh/osm/pkg/smi"
+	"github.com/open-service-mesh/osm/pkg/tests"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	testclient "k8s.io/client-go/kubernetes/fake"
+
+	"github.com/open-service-mesh/osm/pkg/envoy"
+)
+
+var _ = Describe("Test ADS response functions", func() {
+
+	// --- setup
+	kubeClient := testclient.NewSimpleClientset()
+	namespace := tests.Namespace
+	envoyUID := tests.EnvoyUID
+	serviceName := uuid.New().String()
+	labels := map[string]string{constants.EnvoyUniqueIDLabelName: tests.EnvoyUID}
+	mc := catalog.NewFakeMeshCatalog(kubeClient)
+
+	// Create a Pod
+	pod := tests.NewPodTestFixture(namespace, fmt.Sprintf("pod-0-%s", uuid.New()))
+	pod.Labels[constants.EnvoyUniqueIDLabelName] = envoyUID
+	_, err := kubeClient.CoreV1().Pods(namespace).Create(context.TODO(), &pod, metav1.CreateOptions{})
+	It("should have created a pod", func() {
+		Expect(err).ToNot(HaveOccurred())
+	})
+
+	service := tests.NewServiceFixture(serviceName, namespace, labels)
+	_, err = kubeClient.CoreV1().Services(namespace).Create(context.TODO(), service, metav1.CreateOptions{})
+	It("should have created a service", func() {
+		Expect(err).ToNot(HaveOccurred())
+	})
+	cn := certificate.CommonName(fmt.Sprintf("%s.%s.%s", envoyUID, tests.BookbuyerServiceAccountName, namespace))
+	proxy := envoy.NewProxy(cn, nil)
+
+	expectedSecretOneName := fmt.Sprintf("service-cert:default/%s", serviceName)
+	expectedSecretTwoName := fmt.Sprintf("root-cert:default/%s", serviceName)
+
+	Context("Test getRequestedCertType()", func() {
+		It("returns service cert", func() {
+
+			actual := makeRequestForAllSecrets(proxy, mc)
+			expected := envoy_api_v2.DiscoveryRequest{
+				TypeUrl: string(envoy.TypeSDS),
+				ResourceNames: []string{
+					expectedSecretOneName,
+					expectedSecretTwoName,
+				},
+			}
+			Expect(actual).ToNot(BeNil())
+			Expect(*actual).To(Equal(expected))
+		})
+	})
+
+	Context("Test sendAllResponses()", func() {
+		server, actualResponses := tests.NewFakeXDSServer()
+		It("returns Aggregated Discovery Service response", func() {
+			s := Server{
+				ctx:         context.TODO(),
+				catalog:     mc,
+				meshSpec:    smi.NewFakeMeshSpecClient(),
+				xdsHandlers: getHandlers(),
+			}
+
+			s.sendAllResponses(proxy, &server)
+
+			Expect(actualResponses).ToNot(BeNil())
+			Expect(len(*actualResponses)).To(Equal(5))
+
+			Expect((*actualResponses)[0].VersionInfo).To(Equal("1"))
+			Expect((*actualResponses)[0].TypeUrl).To(Equal(string(envoy.TypeCDS)))
+
+			Expect((*actualResponses)[1].VersionInfo).To(Equal("1"))
+			Expect((*actualResponses)[1].TypeUrl).To(Equal(string(envoy.TypeEDS)))
+
+			Expect((*actualResponses)[2].VersionInfo).To(Equal("1"))
+			Expect((*actualResponses)[2].TypeUrl).To(Equal(string(envoy.TypeLDS)))
+
+			Expect((*actualResponses)[3].VersionInfo).To(Equal("1"))
+			Expect((*actualResponses)[3].TypeUrl).To(Equal(string(envoy.TypeRDS)))
+
+			Expect((*actualResponses)[4].VersionInfo).To(Equal("1"))
+			Expect((*actualResponses)[4].TypeUrl).To(Equal(string(envoy.TypeSDS)))
+			Expect(len((*actualResponses)[4].Resources)).To(Equal(2))
+
+			secretOne := envoy_api_v2_auth.Secret{}
+			firstSecret := (*actualResponses)[4].Resources[0]
+			err = ptypes.UnmarshalAny(firstSecret, &secretOne)
+			Expect(secretOne.Name).To(Equal(expectedSecretOneName))
+
+			secretTwo := envoy_api_v2_auth.Secret{}
+			secondSecret := (*actualResponses)[4].Resources[1]
+			err = ptypes.UnmarshalAny(secondSecret, &secretTwo)
+			Expect(secretTwo.Name).To(Equal(expectedSecretTwoName))
+
+		})
+	})
+})
