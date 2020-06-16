534662bb3e66d05d3811250e0ab72356c9a511ed
Delyan Raychev <delyan.raychev@microsoft.com>
2020-06-16 13:31:54 -0700

    Adding script

---
+++ scripts/gen-backpressure-client.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -auexo pipefail
+
+DIR="$GOPATH/src/k8s.io/"
+
+if [ ! -d "${DIR}code-generator" ]; then
+  mkdir -p "$DIR"
+  pushd "$DIR"
+  git clone git@github.com:kubernetes/code-generator.git
+  popd
+fi
+
+../code-generator/generate-groups.sh \
+    all \
+    github.com/open-service-mesh/osm/pkg/client \
+    github.com/open-service-mesh/osm/pkg/apis \
+    "osmbackpressureconfig:v1"
