7c7fbd7c7c25b627d44f55f55991b1c27998769b
Delyan Raychev <delyan.raychev@microsoft.com>
2020-06-17 13:18:01 -0700

    Correct Backpressure API struct for client generation (#848)
    * Adding script
    
    * gen script
    
    * Add register.go
    
    * blah

--- pkg/apis/osmbackpressureconfig/v1/types.go
+++ pkg/apis/osmbackpressureconfig/v1/types.go
@@ -4,8 +4,9 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// OSMBackpressureConfig is the targets AGIC is allowed to mutate
-//+k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// OSMBackpressureConfig is ...
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 type OSMBackpressureConfig struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
@@ -13,20 +14,21 @@ type OSMBackpressureConfig struct {
 	Spec OSMBackpressureConfigSpec `json:"spec"`
 }
 
-// OSMBackpressureConfigSpec defines a list of uniquely identifiable targets for which the AGIC is not allowed to mutate config.
+// OSMBackpressureConfigSpec is ...
 type OSMBackpressureConfigSpec struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	// Max number of requests a connection can make
+	// MaxRequestsPerConnection is the max number of requests a connection can make
 	MaxRequestsPerConnection int32 `json:"maxrequestsperconnection,omitempty"`
 }
 
-// OSMBackpressureConfigList is the list of prohibited targets
+// OSMBackpressureConfigList is ...
 //+k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 type OSMBackpressureConfigList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata"`
 
+	// Items is the list of OSMBackpressureConfig
 	Items []OSMBackpressureConfig `json:"items"`
 }
