15b0588a3eefc5a10f746494aec3ce9aa68d2558
Delyan Raychev <delyan.raychev@microsoft.com>
2020-06-17 10:20:58 -0700

    Adding tests

--- pkg/debugger/policy.go
+++ pkg/debugger/policy.go
@@ -3,18 +3,10 @@ package debugger
 import (
 	"fmt"
 	"net/http"
-	"sort"
-	"time"
-
-	"github.com/open-service-mesh/osm/pkg/catalog"
-	"github.com/open-service-mesh/osm/pkg/certificate"
 )
-
-
 
 func (ds debugServer) getPolicies() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		_, _ = fmt.Fprintf(w, "hello" )
-		
+		_, _ = fmt.Fprintf(w, "hello")
 	})
 }
