18b0d77b02e7d4ced1764c42abedeb08f2356041
Shashank Ram <shashank.ram@microsoft.com>
2020-06-25 11:23:57 -0700

    cds/test: fix tests by unit testing each cluster
    This change refactors the CDS unit test to test each
    cluster separately as opposed to testing all clusters
    as a part of the CDS response. This allows to not
    rely on ordering of resources in the serialized CDS
    response, and instead unit tests each cluster separately.
    
    Resolves #899

--- pkg/envoy/cds/service.go
+++ pkg/envoy/cds/service.go
@@ -69,11 +69,11 @@ func getServiceClusterLocal(catalog catalog.MeshCataloger, proxyServiceName serv
 	return &xdsCluster, nil
 }
 
-func getPrometheusCluster(clusterName string) xds.Cluster {
+func getPrometheusCluster() xds.Cluster {
 	return xds.Cluster{
 		// The name must match the domain being cURLed in the demo
-		Name:           clusterName,
-		AltStatName:    clusterName,
+		Name:           constants.EnvoyMetricsCluster,
+		AltStatName:    constants.EnvoyMetricsCluster,
 		ConnectTimeout: ptypes.DurationProto(connectionTimeout),
 		ClusterDiscoveryType: &xds.Cluster_Type{
 			Type: xds.Cluster_STATIC,
@@ -81,7 +81,7 @@ func getPrometheusCluster(clusterName string) xds.Cluster {
 		LbPolicy: xds.Cluster_ROUND_ROBIN,
 		LoadAssignment: &xds.ClusterLoadAssignment{
 			// NOTE: results.ServiceName is the top level service that is cURLed.
-			ClusterName: clusterName,
+			ClusterName: constants.EnvoyMetricsCluster,
 			Endpoints: []*envoyEndpoint.LocalityLbEndpoints{
 				{
 					LbEndpoints: []*envoyEndpoint.LbEndpoint{{
