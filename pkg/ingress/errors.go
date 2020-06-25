0028fd3dd50039d4859382ba0449675cf0935516
Shashank Ram <shashank08@gmail.com>
2020-06-24 19:45:19 -0700

    ingress: remove monitor annotation (#895)
    The monitor annotation doesn't add much value, considering
    the ingress resource needs to belong to a monitored namespace.
    When a user applies an ingress resource with backend services
    that belong to the service mesh, the intent is clear that the
    the service needs to be exposed outside the cluster via ingress.
    OSM doesn't use the monitor annotation to match a service against
    an ingress resource, but rather the service name and the namespace
    of the ingress resource.
    
    Co-authored-by: Shashank Ram <shashank.ram@microsoft.com>

--- pkg/ingress/errors.go
+++ pkg/ingress/errors.go
@@ -3,8 +3,7 @@ package ingress
 import "errors"
 
 var (
-	errSyncingCaches            = errors.New("Failed initial cache sync for Ingress informer")
-	errInitInformers            = errors.New("Ingress informer not initialized")
-	errInvalidObject            = errors.New("Ingress object is invalid")
-	errInvalidMonitorAnnotation = errors.New("Ingress resource as an invalid monitor annoation")
+	errSyncingCaches = errors.New("Failed initial cache sync for Ingress informer")
+	errInitInformers = errors.New("Ingress informer not initialized")
+	errInvalidObject = errors.New("Ingress object is invalid")
 )
