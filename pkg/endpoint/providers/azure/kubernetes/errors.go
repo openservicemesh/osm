bc6024053ea893cfec8c9dcb40095da5d154cbaa
Shashank Ram <shashank08@gmail.com>
2020-07-08 15:02:56 -0700

    kube/informers: assert for correct types to prevent panic
    If the object type in caches are not as expected, prevent panics
    and log an error.
    
    Part of #667

--- pkg/endpoint/providers/azure/kubernetes/errors.go
+++ pkg/endpoint/providers/azure/kubernetes/errors.go
@@ -2,4 +2,7 @@ package azure
 
 import "errors"
 
-var errSyncingCaches = errors.New("syncing caches")
+var (
+	errSyncingCaches     = errors.New("syncing caches")
+	errInvalidObjectType = errors.New("invalid object type in cache")
+)
