86c68371650dab2018713006b95ddd223c77064b
Delyan Raychev <delyan.raychev@microsoft.com>
2020-07-09 12:43:41 -0700

    Adding get-pcap helper script

---
+++ scripts/get-pcap-bookstore.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+# This script captures certain amount of network traffic from the BOOKSTORE V1 pod.
+# The capture is then moved from pod to local host.
+# This is a pcap file that can be inspected with tools like Wireshark.
+# This is useful to show encrypted/unencrypted traffic (demo enabling mTLS).
+
+
+# shellcheck disable=SC1091
+source .env
+
+VERSION="bookstore-v1"
+
+POD="$(kubectl get pods --selector app=${VERSION} -n "$BOOKSTORE_NAMESPACE" --no-headers | grep 'Running' | awk '{print $1}')"
+
+
+kubectl exec -it -n "$BOOKSTORE_NAMESPACE"  \
+        "$POD" \
+        -c "$VERSION" -- \
+        tcpdump -A -U -c 1024 -s 0 -w /tmp/pcap.pcap
+
+
+# Move the file from pod to local host
+kubectl cp "$BOOKSTORE_NAMESPACE"/"$POD":/tmp/pcap.pcap ./
