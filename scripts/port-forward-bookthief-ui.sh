27e118e29e38a9d2a5ef0fa362ab11203ffe188d
Delyan Raychev <delyan.raychev@microsoft.com>
2020-07-10 10:18:50 -0700

    Adding get-pcap helper script + port forwarding scripts (#1053)
    This script captures certain amount of network traffic from the BOOKSTORE V1 pod. The capture is then moved from pod to local host. This is a pcap file that can be inspected with tools like Wireshark. This is useful to show encrypted/unencrypted traffic (demo enabling mTLS).
    
    This PR also adds helper scripts that enable port forwarding from the various demo pods. This makes it easy / possible to bring the various web UIs to local host and show the count of books.

---
+++ scripts/port-forward-bookthief-ui.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+# This script forwards port 80 from the BOOKTHIEF pod to local port 8082
+
+
+# shellcheck disable=SC1091
+source .env
+
+POD="$(kubectl get pods --selector app=bookthief -n "$BOOKTHIEF_NAMESPACE" --no-headers | grep 'Running' | awk '{print $1}')"
+
+kubectl describe pod "$POD" -n "$BOOKTHIEF_NAMESPACE"
+
+kubectl port-forward "$POD" -n "$BOOKTHIEF_NAMESPACE" 8082:80
