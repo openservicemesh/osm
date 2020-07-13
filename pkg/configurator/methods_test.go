1172c425938f54438d33169bffea1fe1646ed42a
Shashank Ram <shashank08@gmail.com>
2020-07-13 14:24:46 -0700

    fix comment

--- pkg/configurator/methods_test.go
+++ pkg/configurator/methods_test.go
@@ -29,7 +29,7 @@ var _ = Describe("Test Envoy configuration creation", func() {
 		})
 	})
 
-	Context("create OSM config for permissive_traffic_policy_mode", func() {
+	Context("create OSM config with default values", func() {
 		kubeClient := testclient.NewSimpleClientset()
 		stop := make(chan struct{})
 		osmNamespace := "-test-osm-namespace-"
