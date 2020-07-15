2d0b04bdf16f056195bea70c12ac6fc8f5d0ba93
Delyan Raychev <delyan.raychev@microsoft.com>
2020-07-15 13:34:06 -0700

    Add env variables

--- demo/join-namespaces.sh
+++ demo/join-namespaces.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
+
+
+# This script joins the list of namespaces to the existing service mesh.
+# This is a helper script part of brownfield OSM demo.
+
+
+
 set -aueo pipefail
 
 # shellcheck disable=SC1091
 source .env
 
+
+
+K8S_NAMESPACE="${K8S_NAMESPACE:-osm-system}"
+BOOKBUYER_NAMESPACE="${BOOKBUYER_NAMESPACE:-bookbuyer}"
+BOOKSTORE_NAMESPACE="${BOOKSTORE_NAMESPACE:-bookstore}"
+BOOKTHIEF_NAMESPACE="${BOOKTHIEF_NAMESPACE:-bookthief}"
+BOOKWAREHOUSE_NAMESPACE="${BOOKWAREHOUSE_NAMESPACE:-bookwarehouse}"
+
+
+
 for ns in "$BOOKWAREHOUSE_NAMESPACE" "$BOOKBUYER_NAMESPACE" "$BOOKSTORE_NAMESPACE" "$BOOKTHIEF_NAMESPACE"; do
-    kubectl label namespaces "$ns" openservicemesh.io/monitor="$K8S_NAMESPACE" --overwrite="true"
     kubectl label namespaces "$ns" openservicemesh.io/monitored-by=osm --overwrite="true"
 done
 
