367d954bbbc58acf547753c800d5611b21526e35
Delyan Raychev <delyan.raychev@microsoft.com>
2020-07-15 14:37:59 -0700

    Add comments and MESH_NAME to unjoin-namespaces.sh

--- demo/unjoin-namespaces.sh
+++ demo/unjoin-namespaces.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 
+
 # This script removes the list of namespaces from the OSM.
 # This is a helper script part of the OSM Brownfield Deployment Demo.
+
+
 
 set -aueo pipefail
 
 # shellcheck disable=SC1091
 source .env
+
 
 
 K8S_NAMESPACE="${K8S_NAMESPACE:-osm-system}"
@@ -16,9 +20,14 @@ BOOKSTORE_NAMESPACE="${BOOKSTORE_NAMESPACE:-bookstore}"
 BOOKTHIEF_NAMESPACE="${BOOKTHIEF_NAMESPACE:-bookthief}"
 BOOKWAREHOUSE_NAMESPACE="${BOOKWAREHOUSE_NAMESPACE:-bookwarehouse}"
 
+MESH_NAME="${MESH_NAME:-osm}"
+
 
 for ns in "$BOOKWAREHOUSE_NAMESPACE" "$BOOKBUYER_NAMESPACE" "$BOOKSTORE_NAMESPACE" "$BOOKTHIEF_NAMESPACE"; do
-    kubectl label namespaces "$ns" openservicemesh.io/monitored-by=osm- || true
+
+    # The trailing minus (-) sign after MESH_NAME below is deliberate - this REMOVES the label
+    kubectl label namespaces "$ns" openservicemesh.io/monitored-by="$MESH_NAME"- || true
+
 done
 
 
