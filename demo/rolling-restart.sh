0c4460b621c4b207e7b6d740e5bcd0b3a065a6c7
Delyan Raychev <delyan.raychev@microsoft.com>
2020-07-15 12:28:54 -0700

    Add env variables

--- demo/rolling-restart.sh
+++ demo/rolling-restart.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
+
+
 # This script performs a rolling restart of the deployments listed below.
 # This is part of the OSM Bookstore demo helper scripts.
 
-kubectl rollout restart deployment -n bookbuyer       bookbuyer
-kubectl rollout restart deployment -n bookstore       bookstore-v1
-kubectl rollout restart deployment -n bookstore       bookstore-v2
-kubectl rollout restart deployment -n bookthief       bookthief
-kubectl rollout restart deployment -n bookwarehouse   bookwarehouse
+
+
+set -aueo pipefail
+
+# shellcheck disable=SC1091
+source .env
+
+BOOKBUYER_NAMESPACE="${BOOKBUYER_NAMESPACE:-bookbuyer}"
+BOOKSTORE_NAMESPACE="${BOOKSTORE_NAMESPACE:-bookstore}"
+BOOKTHIEF_NAMESPACE="${BOOKTHIEF_NAMESPACE:-bookthief}"
+BOOKWAREHOUSE_NAMESPACE="${BOOKWAREHOUSE_NAMESPACE:-bookwarehouse}"
+
+
+
+kubectl rollout restart deployment bookbuyer      -n "$BOOKBUYER_NAMESPACE"
+kubectl rollout restart deployment bookstore-v1   -n "$BOOKSTORE_NAMESPACE"
+kubectl rollout restart deployment bookstore-v2   -n "$BOOKSTORE_NAMESPACE"
+kubectl rollout restart deployment bookthief      -n "$BOOKTHIEF_NAMESPACE"
+kubectl rollout restart deployment bookwarehouse  -n "$BOOKWAREHOUSE_NAMESPACE"
