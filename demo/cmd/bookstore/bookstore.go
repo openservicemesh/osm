19d22f9f93b8d13f2020bb109dfa7068a4deb141
Delyan Raychev <delyan.raychev@microsoft.com>
2020-07-16 10:38:39 -0700

    default to port 8080

--- demo/cmd/bookstore/bookstore.go
+++ demo/cmd/bookstore/bookstore.go
@@ -21,7 +21,7 @@ var (
 	booksSold = 0
 	log       = logger.New("bookstore")
 	identity  = flag.String("ident", "unidentified", "the identity of the container where this demo app is running (VM, K8s, etc)")
-	port      = flag.Int("port", 80, "port on which this app is listening for incoming HTTP")
+	port      = flag.Int("port", 8080, "port on which this app is listening for incoming HTTP")
 	path      = flag.String("path", ".", "path to the HTML template")
 )
 
