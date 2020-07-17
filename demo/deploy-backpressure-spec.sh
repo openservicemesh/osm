5421346f929d8b91d838915eb7432baf82ae8545
Delyan Raychev <delyan.raychev@microsoft.com>
2020-07-17 15:08:17 -0700

    changes

---
+++ demo/deploy-backpressure-spec.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -aueo pipefail
+
+# shellcheck disable=SC1091
+source .env
+
+echo "Create Backpressure Spec"
+kubectl apply -f - <<EOF
+apiVersion: policy.openservicemesh.io/v1alpha1
+kind: Backpressure
+
+metadata:
+  name: max-requests-per-second
+  namespace: "${BOOKSTORE_NAMESPACE}"
+
+spec:
+  maxRequestsPerConnection: 5
+
+EOF
