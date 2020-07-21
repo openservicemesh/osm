baa51f19201c8c3175a8e51efad74788774fad78
Shashank Ram <shashank08@gmail.com>
2020-07-21 15:10:57 -0700

    don't enable backpressure by default

--- demo/run-osm-demo.sh
+++ demo/run-osm-demo.sh
@@ -118,7 +118,6 @@ if [ "$CERT_MANAGER" = "vault" ]; then
       --container-registry-secret "$CTR_REGISTRY_CREDS_NAME" \
       --osm-image-tag "$CTR_TAG" \
       --enable-debug-server \
-      --enable-backpressure-experimental \
       $optionalInstallArgs
 else
   # shellcheck disable=SC2086
@@ -129,7 +128,6 @@ else
       --container-registry-secret "$CTR_REGISTRY_CREDS_NAME" \
       --osm-image-tag "$CTR_TAG" \
       --enable-debug-server \
-      --enable-backpressure-experimental \
       $optionalInstallArgs
 fi
 
