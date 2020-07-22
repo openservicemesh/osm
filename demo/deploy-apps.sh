788124e6b1365fea878e32c3aa7336ed5af18067
Michelle Noorali <michellemolu@gmail.com>
2020-07-21 20:42:13 -0400

    ref(demo): configure ns in separate file

--- demo/deploy-apps.sh
+++ demo/deploy-apps.sh
@@ -5,23 +5,6 @@ set -auexo pipefail
 # shellcheck disable=SC1091
 source .env
 
-if [[ "${CI:-}" != "true" ]]; then
-  REGISTRY=$(echo "$CTR_REGISTRY" | awk -F'.' '{print $1}')
-  REGISTRY_URL=$(echo "$CTR_REGISTRY" | awk -F'.' '{print $1 "." $2 "." $3}')
-
-  for ns in "$BOOKWAREHOUSE_NAMESPACE" "$BOOKBUYER_NAMESPACE" "$BOOKSTORE_NAMESPACE" "$BOOKTHIEF_NAMESPACE"; do
-      kubectl delete secrets "$CTR_REGISTRY_CREDS_NAME" -n "$ns" || true
-      kubectl create secret docker-registry "$CTR_REGISTRY_CREDS_NAME" \
-          -n "$ns" \
-          --docker-server="$REGISTRY_URL" \
-          --docker-username="$REGISTRY" \
-          --docker-email="noone@example.com" \
-          --docker-password="$CTR_REGISTRY_PASSWORD"
-  done
-else
-    # This script is specifically for CI
-    ./ci/create-app-container-registry-creds.sh
-fi
 # Deploy bookstore
 ./demo/deploy-bookstore.sh "v1"
 ./demo/deploy-bookstore.sh "v2"
