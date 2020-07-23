22a197ca0d5498bf29226c5958eefde4e36441e7
Delyan Raychev <delyan.raychev@microsoft.com>
2020-07-23 16:55:33 -0700

    Remove GetIngressWeightedCluster (#1171)

--- pkg/envoy/rds/response.go
+++ pkg/envoy/rds/response.go
@@ -6,9 +6,9 @@ import (
 	set "github.com/deckarep/golang-set"
 	xds "github.com/envoyproxy/go-control-plane/envoy/api/v2"
 	"github.com/golang/protobuf/ptypes"
-
 	"github.com/open-service-mesh/osm/pkg/catalog"
 	"github.com/open-service-mesh/osm/pkg/configurator"
+	"github.com/open-service-mesh/osm/pkg/constants"
 	"github.com/open-service-mesh/osm/pkg/envoy"
 	"github.com/open-service-mesh/osm/pkg/envoy/route"
 	"github.com/open-service-mesh/osm/pkg/service"
@@ -126,11 +126,11 @@ func updateRoutesForIngress(proxyServiceName service.NamespacedService, catalog 
 		return nil
 	}
 
-	ingressWeightedCluster, err := catalog.GetIngressWeightedCluster(proxyServiceName)
-	if err != nil {
-		log.Error().Err(err).Msgf("Failed to get weighted ingress clusters for proxy %s", proxyServiceName)
-		return err
+	ingressWeightedCluster := service.WeightedCluster{
+		ClusterName: service.ClusterName(proxyServiceName.String()),
+		Weight:      constants.ClusterWeightAcceptAll,
 	}
+
 	for domain, routePolicies := range domainRoutePoliciesMap {
 		for _, routePolicy := range routePolicies {
 			aggregateRoutesByDomain(domainRoutesMap, routePolicy, ingressWeightedCluster, domain)
