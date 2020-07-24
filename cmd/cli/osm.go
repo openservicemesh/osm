c316de3f666fabe15c96515720b01be4990a94d6
Cecile Robert-Michon <cerobert@microsoft.com>
2020-07-24 09:35:10 -0700

    cli: Default the mesh name for `osm mesh delete` (#1142)

--- cmd/cli/osm.go
+++ cmd/cli/osm.go
@@ -22,7 +22,7 @@ To install and configure OSM, run:
 
 var settings = cli.New()
 
-func newRootCmd(config *action.Configuration, out io.Writer, args []string) *cobra.Command {
+func newRootCmd(config *action.Configuration, in io.Reader, out io.Writer, args []string) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:          "osm",
 		Short:        "Install and manage Open Service Mesh",
@@ -36,7 +36,7 @@ func newRootCmd(config *action.Configuration, out io.Writer, args []string) *cob
 
 	// Add subcommands here
 	cmd.AddCommand(
-		newMeshCmd(config, out),
+		newMeshCmd(config, in, out),
 		newEnvCmd(out),
 		newInstallCmd(config, out),
 		newCheckCmd(out),
@@ -52,7 +52,7 @@ func newRootCmd(config *action.Configuration, out io.Writer, args []string) *cob
 
 func main() {
 	actionConfig := new(action.Configuration)
-	cmd := newRootCmd(actionConfig, os.Stdout, os.Args[1:])
+	cmd := newRootCmd(actionConfig, os.Stdin, os.Stdout, os.Args[1:])
 	actionConfig.Init(settings.RESTClientGetter(), settings.Namespace(), "secret", debug)
 
 	// run when each command's execute method is called
---
+++ cmd/cli/util.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"strings"
+)
+
+// confirm displays a prompt `s` to the user and returns a bool indicating yes / no
+// If the lowercased, trimmed input begins with anything other than 'y', it returns false
+// It accepts an int `tries` representing the number of attempts before returning false
+func confirm(stdin io.Reader, stdout io.Writer, s string, tries int) (bool, error) {
+	r := bufio.NewReader(stdin)
+
+	for ; tries > 0; tries-- {
+		fmt.Fprintf(stdout, "%s [y/n]: ", s)
+
+		res, err := r.ReadString('\n')
+		if err != nil {
+			return false, err
+		}
+
+		// Empty input (i.e. "\n")
+		if len(res) < 2 {
+			continue
+		}
+
+		switch strings.ToLower(strings.TrimSpace(res)) {
+		case "y":
+			return true, nil
+		case "n":
+			return false, nil
+		default:
+			continue
+		}
+	}
+
+	return false, nil
+}
