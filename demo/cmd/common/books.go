80e50d84795f570b6447f22b872fb7b8c1afc108
Delyan Raychev <delyan.raychev@microsoft.com>
2020-07-27 10:37:56 -0700

    ci: Convert log.Info() to log.Error() (#1212)
    This PR switches from log.Info() to log.Error() for demo/cmd/common/books.go

--- demo/cmd/common/books.go
+++ demo/cmd/common/books.go
@@ -64,7 +64,7 @@ func RestockBooks(amount int) {
 	requestBody := strings.NewReader(strconv.Itoa(1))
 	req, err := http.NewRequest("POST", chargeAccountURL, requestBody)
 	if err != nil {
-		log.Info().Msgf("RestockBooks: error posting to %s: %s ", chargeAccountURL, err)
+		log.Error().Err(err).Msgf("RestockBooks: error posting to %s", chargeAccountURL)
 		return
 	}
 
@@ -72,7 +72,7 @@ func RestockBooks(amount int) {
 
 	resp, err := client.Do(req)
 	if err != nil {
-		log.Info().Msgf("RestockBooks: Error posting to %s: %s ", chargeAccountURL, err)
+		log.Error().Err(err).Msgf("RestockBooks: Error posting to %s", chargeAccountURL)
 		return
 	}
 
