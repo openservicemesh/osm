8a05be67db4d4bb54bece837639ac249163b0362
Delyan Raychev <delyan.raychev@microsoft.com>
2020-07-27 13:39:54 -0700

    configurator: Refactor ConfigMap parsing

--- pkg/configurator/methods.go
+++ pkg/configurator/methods.go
@@ -2,6 +2,8 @@ package configurator
 
 import (
 	"encoding/json"
+	"net"
+	"sort"
 	"strings"
 )
 
@@ -51,9 +53,33 @@ func (c *Client) IsZipkinTracingEnabled() bool {
 
 // GetMeshCIDRRanges returns a list of mesh CIDR ranges
 func (c *Client) GetMeshCIDRRanges() []string {
-	cidrStr := c.getConfigMap().MeshCIDRRanges
-	cidrRanges := strings.Fields(cidrStr)
-	return cidrRanges
+	noSpaces := strings.ReplaceAll(c.getConfigMap().MeshCIDRRanges, " ", ",")
+	commaSeparatedCIDRs := strings.Split(noSpaces, ",")
+
+	cidrSet := make(map[string]interface{})
+	for _, cidr := range commaSeparatedCIDRs {
+		trimmedCIDR := strings.Trim(cidr, " ")
+		if len(trimmedCIDR) == 0 {
+			continue
+		}
+
+		_, _, err := net.ParseCIDR(trimmedCIDR)
+		if err != nil {
+			log.Error().Err(err).Msgf("Found incorrectly formatted in-mesh CIDR %s from ConfigMap %s/%s; Skipping CIDR", trimmedCIDR, c.osmNamespace, c.osmConfigMapName)
+			continue
+		}
+
+		cidrSet[trimmedCIDR] = nil
+	}
+
+	var cidrs []string
+	for cidr := range cidrSet {
+		cidrs = append(cidrs, cidr)
+	}
+
+	sort.Strings(cidrs)
+
+	return cidrs
 }
 
 // UseHTTPSIngress determines whether traffic between ingress and backend pods should use HTTPS protocol
