2a72ed17cd5c7cac24d49feb6d96b19fe0f7be10
Elena <elenaspasovaspasova@gmail.com>
2020-07-30 10:54:02 -0700

    fixed namespace imports with new openservicemesh label (removed dashes)

--- pkg/debugger/namespace.go
+++ pkg/debugger/namespace.go
@@ -4,9 +4,17 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+<<<<<<< HEAD
 )
 
 type namespaces struct {
+=======
+
+	"github.com/open-service-mesh/osm/pkg/service"
+)
+
+type namespace struct {
+>>>>>>> fixed namespace imports with new openservicemesh label (removed dashes)
 	Namespaces    []string            `json:"namespaces"`
 }
 
@@ -16,9 +24,15 @@ func (ds debugServer) getMonitoredNamespacesHandler() http.Handler {
 		var n namespaces
 		n.Namespaces = ds.meshCatalogDebugger.ListMonitoredNamespaces()
 
+<<<<<<< HEAD
 		jsonPolicies, err := json.Marshal(n)
 		if err != nil {
 			log.Error().Err(err).Msgf("Error marshalling policy %+v", n)
+=======
+		jsonPolicies, err := json.Marshal(p)
+		if err != nil {
+			log.Error().Err(err).Msgf("Error marshalling policy %+v", p)
+>>>>>>> fixed namespace imports with new openservicemesh label (removed dashes)
 		}
 
 		_, _ = fmt.Fprint(w, string(jsonPolicies))
--- pkg/namespace/client.go
+++ pkg/namespace/client.go
@@ -82,7 +82,11 @@ func (c Client) IsMonitoredNamespace(namespace string) bool {
 	return exists
 }
 
+<<<<<<< HEAD
 // ListMonitoredNamespaces returns all namespaces that the mesh is monitoring.
+=======
+// Shashank's function for getting namespaces
+>>>>>>> fixed namespace imports with new openservicemesh label (removed dashes)
 func (c Client) ListMonitoredNamespaces() []string {
     var namespaces []string
  
