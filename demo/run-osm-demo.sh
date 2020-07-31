5aa09c28cbd8d6ce8a2837674f5b94d24cb93861
Shashank Ram <shashank08@gmail.com>
2020-07-31 16:46:59 -0700

    add example and move var init to top

--- demo/run-osm-demo.sh
+++ demo/run-osm-demo.sh
@@ -25,6 +25,7 @@ CERT_MANAGER="${CERT_MANAGER:-tresor}"
 CTR_REGISTRY="${CTR_REGISTRY:-osmci.azurecr.io/osm}"
 CTR_REGISTRY_CREDS_NAME="${CTR_REGISTRY_CREDS_NAME:-acr-creds}"
 CTR_TAG="${CTR_TAG:-latest}"
+DEPLOY_TRAFFIC_SPLIT="${DEPLOY_TRAFFIC_SPLIT:-true}"
 MESH_CIDR=$(./scripts/get_mesh_cidr.sh)
 
 optionalInstallArgs=$*
@@ -133,7 +134,7 @@ bin/osm namespace add --mesh-name "$MESH_NAME" "$BOOKWAREHOUSE_NAMESPACE" "$BOOK
 ./demo/deploy-apps.sh
 
 # Apply SMI policies
-if [ "${DEPLOY_TRAFFIC_SPLIT:-true}" = "true" ]; then
+if [ "$DEPLOY_TRAFFIC_SPLIT" = "true" ]; then
     ./demo/deploy-traffic-split.sh
 fi
 
