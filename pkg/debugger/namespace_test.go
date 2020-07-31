4e024b328972aaa3fb7066c23c0347501c3e564e
Elena <elenaspasovaspasova@gmail.com>
2020-07-31 14:53:19 -0700

    fix namespace test, remove errors for listMonitoredNamespaces in debugger folder. have namespace endpoint working now + need PR

--- pkg/debugger/namespace_test.go
+++ pkg/debugger/namespace_test.go
@@ -3,22 +3,13 @@ package debugger
 import (
 	"fmt"
 	"net/http/httptest"
-	"testing"
-	"time"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
-	"github.com/open-service-mesh/osm/pkg/certificate"
-	"github.com/open-service-mesh/osm/pkg/envoy"
-	"github.com/open-service-mesh/osm/pkg/namespace"
 )
 
-func TestEndpoints(t *testing.T) {
-	RegisterFailHandler(Fail)
-	RunSpecs(t, "Test Suite")
-}
-
+// Tests if namespace handler returns default namespace correctly
 var _ = Describe("Test debugger methods", func() {
 	Context("Testing getMonitoredNamespacesHandler()", func() {
 		It("returns JSON serialized monitored namespaces", func() {
@@ -30,20 +21,8 @@ var _ = Describe("Test debugger methods", func() {
 			responseRecorder := httptest.NewRecorder()
 			monitoredNamespacesHandler.ServeHTTP(responseRecorder, nil)
 			actualResponseBody := responseRecorder.Body.String()
-			expectedResponseBody := `{default}`
+			expectedResponseBody := `{"namespaces":["default"]}`
 			Expect(actualResponseBody).To(Equal(expectedResponseBody), fmt.Sprintf("Actual value did not match expectations:\n%s", actualResponseBody))
 		})
 	})
 })
-
-type fakeMeshCatalogDebuger struct{}
-
-// ListMonitoredNamespaces implements MeshCatalogDebugger
-func (f fakeMeshCatalogDebuger) ListMonitoredNamespaces() []string {
-	return []string{tests.Namespace}
-}
-
-// NewFakeMeshCatalogDebugger implements and creates a new MeshCatalogDebugger
-func NewFakeMeshCatalogDebugger() MeshCatalogDebugger {
-	return fakeMeshCatalogDebuger{}
-}
