13dbaa9dc5f36b42927fa6d7f49f5993c36c9a70
Delyan Raychev <delyan.raychev@microsoft.com>
2020-08-03 17:19:25 -0700

    pkg/version: Cleanup unused function

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,10 +1,5 @@
 package version
 
-import (
-	"fmt"
-	"os"
-)
-
 // BuildDate is the date when the binary was built
 var BuildDate string
 
@@ -13,9 +8,3 @@ var GitCommit string
 
 // Version is the version of the compiled software
 var Version string
-
-// PrintVersionAndExit prints the version and exits
-func PrintVersionAndExit() {
-	fmt.Printf("Version: %s; Commit: %s; Date: %s\n", Version, GitCommit, BuildDate)
-	os.Exit(0)
-}
