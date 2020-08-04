2d0c567ad8636f84f381a827d96e870d815b9268
Michelle Noorali <michellemolu@gmail.com>
2020-08-03 22:07:15 -0400

    feat(*): update TrafficSpecs v1alpha2->v1alpha3

--- demo/deploy-traffic-spec.sh
+++ demo/deploy-traffic-spec.sh
@@ -7,42 +7,43 @@ source .env
 
 echo "Create Bookstore HTTPRouteGroup"
 kubectl apply -f - <<EOF
-apiVersion: specs.smi-spec.io/v1alpha2
+apiVersion: specs.smi-spec.io/v1alpha3
 kind: HTTPRouteGroup
 metadata:
   name: bookstore-service-routes
   namespace: "$BOOKSTORE_NAMESPACE"
-matches:
-- name: books-bought
-  pathRegex: /books-bought
-  methods:
-  - GET
-  headers:
-  - "user-agent": ".*-http-client/*.*"
-  - "client-app": "bookbuyer"
-- name: buy-a-book
-  pathRegex: ".*a-book.*new"
-  methods:
-  - GET
-- name: update-books-bought
-  pathRegex: /update-books-bought
-  methods:
-  - POST
+spec:
+  matches:
+  - name: books-bought
+    pathRegex: /books-bought
+    methods:
+    - GET
+    headers:
+    - "user-agent": ".*-http-client/*.*"
+    - "client-app": "bookbuyer"
+  - name: buy-a-book
+    pathRegex: ".*a-book.*new"
+    methods:
+    - GET
+  - name: update-books-bought
+    pathRegex: /update-books-bought
+    methods:
+    - POST
 EOF
 
 
 echo "Create Bookwarehouse HTTPRouteGroup"
 kubectl apply -f - <<EOF
-apiVersion: specs.smi-spec.io/v1alpha2
+apiVersion: specs.smi-spec.io/v1alpha3
 kind: HTTPRouteGroup
 metadata:
   name: bookwarehouse-service-routes
   namespace: "$BOOKWAREHOUSE_NAMESPACE"
-matches:
-- name: restock-books
-  methods:
-  - POST
-  headers:
-  - host: "bookwarehouse.$BOOKWAREHOUSE_NAMESPACE"
-
+spec:
+  matches:
+    - name: restock-books
+      methods:
+      - POST
+      headers:
+      - host: "bookwarehouse.$BOOKWAREHOUSE_NAMESPACE"
 EOF
