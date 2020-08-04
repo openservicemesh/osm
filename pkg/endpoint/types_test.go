1b2dbec43051ab9f8bd80f2ea49146063080333f
Delyan Raychev <delyan.raychev@microsoft.com>
2020-08-03 17:26:30 -0700

    tests: Test Endpoint struct methods

--- pkg/endpoint/types_test.go
+++ pkg/endpoint/types_test.go
@@ -1,22 +1,20 @@
 package endpoint
 
 import (
+	"net"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-
-	"github.com/openservicemesh/osm/pkg/certificate"
-	"github.com/openservicemesh/osm/pkg/service"
 )
 
-var _ = Describe("Test MeshService methods", func() {
-	Context("Testing GetCommonName", func() {
-		It("should return DNS-1123 of the MeshService struct", func() {
-			meshService := service.MeshService{
-				Namespace: "namespace-here",
-				Name:      "service-name-here",
-			}
-			actual := meshService.GetCommonName()
-			expected := certificate.CommonName("service-name-here.namespace-here.svc.cluster.local")
+var _ = Describe("Test Endpoint methods", func() {
+	Context("Testing Endpoint{}.String()", func() {
+		It("should return a proper string representation of the Endpoint struct", func() {
+			actual := Endpoint{
+				IP:   net.ParseIP("8.8.8.8"),
+				Port: Port(8888),
+			}.String()
+			expected := "(ip=8.8.8.8, port=8888)"
 			Expect(actual).To(Equal(expected))
 		})
 	})
