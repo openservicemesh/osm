cb4420410f9f5589cebf22effa953dfb72e05051
Delyan Raychev <delyan.raychev@microsoft.com>
2020-08-04 21:40:12 -0700

    fix port forward bookstore v2

--- scripts/port-forward-bookstore-ui-v2.sh
+++ scripts/port-forward-bookstore-ui-v2.sh
@@ -7,16 +7,6 @@
 # shellcheck disable=SC1091
 source .env
 
-backend="${1:-bookstore-v2}"
-thisScript="$(dirname "$0")/$(basename "$0")"
-
-if [ -z "$backend" ]; then
-    echo "Usage: $thisScript <backend-name>"
-    exit 1
-fi
-
-POD="$(kubectl get pods --selector app="$backend" -n "$BOOKSTORE_NAMESPACE" --no-headers | grep 'Running' | awk '{print $1}')"
-
-kubectl describe pod "$POD" -n "$BOOKSTORE_NAMESPACE"
+POD="$(kubectl get pods --selector app=bookstore,version=v2 -n "$BOOKSTORE_NAMESPACE" --no-headers | grep 'Running' | awk '{print $1}')"
 
 kubectl port-forward "$POD" -n "$BOOKSTORE_NAMESPACE" 8082:80
