9fc8b4beca1e808fbb913676584a6c12fe61ce19
Jon Huhn <huhnjon@gmail.com>
2020-08-06 13:57:26 -0500

    Update tags

--- cmd/cli/install.go
+++ cmd/cli/install.go
@@ -106,7 +106,7 @@ func newInstallCmd(config *helm.Configuration, out io.Writer) *cobra.Command {
 
 	f := cmd.Flags()
 	f.StringVar(&inst.containerRegistry, "container-registry", "openservicemesh", "container registry that hosts control plane component images")
-	f.StringVar(&inst.osmImageTag, "osm-image-tag", "v0.1.0", "osm image tag")
+	f.StringVar(&inst.osmImageTag, "osm-image-tag", "v0.2.0", "osm image tag")
 	f.StringVar(&inst.containerRegistrySecret, "container-registry-secret", "acr-creds", "name of Kubernetes secret for container registry credentials to be created if it doesn't already exist")
 	f.StringVar(&inst.chartPath, "osm-chart-path", "", "path to osm chart to override default chart")
 	f.StringVar(&inst.certManager, "cert-manager", defaultCertManager, "certificate manager to use (tresor or vault)")
