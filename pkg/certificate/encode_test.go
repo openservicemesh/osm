d892248402b25890b394371e6f9d956cd86bee28
JoshVanL <vleeuwenjoshua@gmail.com>
2020-08-10 12:47:17 +0100

    Updates certifcate/pem and types to include certificate requests
    Signed-off-by: JoshVanL <vleeuwenjoshua@gmail.com>

--- pkg/certificate/encode_test.go
+++ pkg/certificate/encode_test.go
@@ -73,6 +73,14 @@ var _ = Describe("Test Tresor Tools", func() {
 		})
 	})
 
+	Context("Test EncodeCertReqDERtoPEM function", func() {
+		cert, err := EncodeCertReqDERtoPEM([]byte{1, 2, 3})
+		It("should have encoded DER bytes into a PEM certificate request", func() {
+			Expect(err).ShouldNot(HaveOccurred())
+			Expect(cert).NotTo(Equal(nil))
+		})
+	})
+
 	Context("Test EncodeKeyDERtoPEM function", func() {
 		var pemKey pem.PrivateKey
 		var privKey *rsa.PrivateKey
