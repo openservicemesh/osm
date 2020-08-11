82cc40e797d16892fcd6cff126b335d3111584cc
Delyan Raychev <delyan.raychev@microsoft.com>
2020-08-11 15:11:26 -0700

    catalog: Test ListEndpointsForService

--- pkg/catalog/endpoint_test.go
+++ pkg/catalog/endpoint_test.go
@@ -1,1 +1,24 @@
 package catalog
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/openservicemesh/osm/pkg/endpoint"
+	"github.com/openservicemesh/osm/pkg/tests"
+)
+
+var _ = Describe("Test catalog functions", func() {
+	Context("Testing ListEndpointsForService()", func() {
+		mc := newFakeMeshCatalog()
+		It("lists endpoints for a given service", func() {
+			actual, err := mc.ListEndpointsForService(tests.BookstoreService)
+			Expect(err).ToNot(HaveOccurred())
+
+			expected := []endpoint.Endpoint{
+				tests.Endpoint,
+			}
+			Expect(actual).To(Equal(expected))
+		})
+	})
+})
