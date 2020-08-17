21007246863feb187b12f5dffb24aa341bcf12b5
Kalya Subramanian <kasubra@microsoft.com>
2020-08-17 13:32:11 -0700

    feat(mesh catalog): Return list of services for a given service account

--- DESIGN.md
+++ DESIGN.md
@@ -307,8 +307,8 @@ type MeshCataloger interface {
 	// UnregisterProxy unregisters an existing proxy from the service mesh catalog
 	UnregisterProxy(*envoy.Proxy)
 
-	// GetServiceForServiceAccount returns the service corresponding to a service account
-	GetServiceForServiceAccount(service.K8sServiceAccount) (service.MeshService, error)
+	// GetServicesForServiceAccount returns a list of services corresponding to a service account
+	GetServicesForServiceAccount(service.K8sServiceAccount) ([]service.MeshService, error)
 
 	// GetHostnamesForService returns the hostnames for a service
 	GetHostnamesForService(service service.MeshService) (string, error)
--- pkg/catalog/service.go
+++ pkg/catalog/service.go
@@ -5,16 +5,26 @@ import (
 	"github.com/openservicemesh/osm/pkg/service"
 )
 
-// GetServiceForServiceAccount returns a service corresponding to a service account
-func (mc *MeshCatalog) GetServiceForServiceAccount(sa service.K8sServiceAccount) (service.MeshService, error) {
+// GetServicesForServiceAccount returns a list of services corresponding to a service account
+func (mc *MeshCatalog) GetServicesForServiceAccount(sa service.K8sServiceAccount) ([]service.MeshService, error) {
+	services := []service.MeshService{}
 	for _, provider := range mc.endpointsProviders {
 		// TODO (#88) : remove this provider check once we have figured out the service account story for azure vms
 		if provider.GetID() != constants.AzureProviderName {
 			log.Trace().Msgf("[%s] Looking for Services for Name=%s", provider.GetID(), sa)
 			service, err := provider.GetServiceForServiceAccount(sa)
-			log.Trace().Msgf("[%s] Found service %s for Name=%s", provider.GetID(), service.String(), sa)
-			return service, err
+			if err != nil {
+				log.Warn().Msgf("Error getting services from provider %s: %s", provider.GetID(), err)
+			} else {
+				log.Trace().Msgf("[%s] Found service %s for Name=%s", provider.GetID(), service.String(), sa)
+				services = append(services, service)
+			}
 		}
 	}
-	return service.MeshService{}, errServiceNotFoundForAnyProvider
+
+	if len(services) == 0 {
+		return []service.MeshService{}, errServiceNotFoundForAnyProvider
+	}
+
+	return services, nil
 }
