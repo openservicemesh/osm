4e8ff0fd4b005123c404fed2ef560a5553f9017e
Delyan Raychev <delyan.raychev@microsoft.com>
2020-08-17 20:43:29 -0700

    catalog: Simplify GetServicesForServiceAccount()

--- pkg/catalog/service.go
+++ pkg/catalog/service.go
@@ -7,23 +7,27 @@ import (
 
 // GetServicesForServiceAccount returns a list of services corresponding to a service account
 func (mc *MeshCatalog) GetServicesForServiceAccount(sa service.K8sServiceAccount) ([]service.MeshService, error) {
-	services := []service.MeshService{}
+	var services []service.MeshService
 	for _, provider := range mc.endpointsProviders {
+
 		// TODO (#88) : remove this provider check once we have figured out the service account story for azure vms
-		if provider.GetID() != constants.AzureProviderName {
-			log.Trace().Msgf("[%s] Looking for Services for Name=%s", provider.GetID(), sa)
-			service, err := provider.GetServiceForServiceAccount(sa)
-			if err != nil {
-				log.Warn().Msgf("Error getting services from provider %s: %s", provider.GetID(), err)
-			} else {
-				log.Trace().Msgf("[%s] Found service %s for Name=%s", provider.GetID(), service.String(), sa)
-				services = append(services, service)
-			}
+		if provider.GetID() == constants.AzureProviderName {
+			continue
 		}
+
+		log.Trace().Msgf("[%s] Looking for Services for Name=%s", provider.GetID(), sa)
+		svc, err := provider.GetServiceForServiceAccount(sa)
+		if err != nil {
+			log.Warn().Msgf("Error getting services from provider %s: %s", provider.GetID(), err)
+		} else {
+			log.Trace().Msgf("[%s] Found service %s for Name=%s", provider.GetID(), svc, sa)
+			services = append(services, svc)
+		}
+
 	}
 
 	if len(services) == 0 {
-		return []service.MeshService{}, errServiceNotFoundForAnyProvider
+		return nil, errServiceNotFoundForAnyProvider
 	}
 
 	return services, nil
