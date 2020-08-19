eabce95f77da2703e7c10f5a4e36a6342085a787
Delyan Raychev <delyan.raychev@microsoft.com>
2020-08-18 18:45:19 -0700

    reorder imports

--- pkg/catalog/service.go
+++ pkg/catalog/service.go
@@ -1,13 +1,14 @@
 package catalog
 
 import (
+	"strings"
+
 	"github.com/openservicemesh/osm/pkg/constants"
 	"github.com/openservicemesh/osm/pkg/service"
-	"strings"
 )
 
 // GetServicesForServiceAccount returns a list of services corresponding to a service account
-func (mc *MeshCatalog) GetServicesForServiceAccount(serviceAccount service.K8sServiceAccount) ([]service.MeshService, error) {
+func (mc *MeshCatalog) GetServicesForServiceAccount(sa service.K8sServiceAccount) ([]service.MeshService, error) {
 	var services []service.MeshService
 	for _, provider := range mc.endpointsProviders {
 
@@ -16,15 +17,15 @@ func (mc *MeshCatalog) GetServicesForServiceAccount(serviceAccount service.K8sSe
 			continue
 		}
 
-		if providerServices, err := provider.GetServicesForServiceAccount(serviceAccount); err != nil {
-			log.Warn().Msgf("Error getting K8s Services linked to Service Account %s from provider %s: %s", provider.GetID(), serviceAccount, err)
+		if providerServices, err := provider.GetServicesForServiceAccount(sa); err != nil {
+			log.Warn().Msgf("Error getting K8s Services linked to Service Account %s from provider %s: %s", provider.GetID(), sa, err)
 		} else {
 			var svcs []string
 			for _, svc := range providerServices {
 				svcs = append(svcs, svc.String())
 			}
 
-			log.Trace().Msgf("Found K8s Services %s linked to Service Account %s from endpoint provider %s", strings.Join(svcs, ","), serviceAccount, provider.GetID())
+			log.Trace().Msgf("Found K8s Services %s linked to Service Account %s from endpoint provider %s", strings.Join(svcs, ","), sa, provider.GetID())
 			services = append(services, providerServices...)
 		}
 	}
