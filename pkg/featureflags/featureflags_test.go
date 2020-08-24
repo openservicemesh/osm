73975c026059f7e542a546fd4c50b89fe6d5b6dd
Kalya Subramanian <kasubra@microsoft.com>
2020-08-24 15:49:03 -0700

    update

--- pkg/featureflags/featureflags_test.go
+++ pkg/featureflags/featureflags_test.go
@@ -8,10 +8,22 @@ import (
 var _ = Describe("Test FeatureFlags", func() {
 	Context("Testing OptionalFeatures", func() {
 		It("should initialize OptionalFeatures", func() {
+			defaultBackpressure := IsBackpressureEnabled()
+			Expect(defaultBackpressure).ToNot(BeTrue())
+
 			optionalFeatures := OptionalFeatures{Backpressure: true}
 			Initialize(optionalFeatures)
+
 			initializedBackpressure := IsBackpressureEnabled()
 			Expect(initializedBackpressure).To(BeTrue())
+		})
+
+		It("should not re-initialize OptionalFeatures", func() {
+			optionalFeatures2 := OptionalFeatures{Backpressure: false}
+			Initialize(optionalFeatures2)
+
+			backpressure := IsBackpressureEnabled()
+			Expect(backpressure).To(BeTrue())
 		})
 	})
 })
