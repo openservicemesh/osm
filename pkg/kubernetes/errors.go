ae969eb3116633a59c29ac2376a7a15c2e8cccb3
Eduard Serra <eduser25@gmail.com>
2020-09-04 10:38:21 -0700

    more CR

--- pkg/kubernetes/errors.go
+++ pkg/kubernetes/errors.go
@@ -4,6 +4,6 @@ import "github.com/pkg/errors"
 
 var (
 	errSyncingCaches     = errors.New("Failed initial cache sync for Namespace informers")
-	errInitInformers     = errors.New("Namespace informers are not initialized")
+	errInitInformers     = errors.New("Informer not initialized")
 	errListingNamespaces = errors.New("Failed to list monitored namespaces")
 )
