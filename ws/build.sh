94d8ff6b6948acd15894246993574ee3f69b6614
Fred stanley <fred@witesand.io>
2020-09-11 15:31:30 -0700

    add aws docker push

--- ws/build.sh
+++ ws/build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+
+#ws docker registry push
 export CTR_REGISTRY=docker.dev.ws:5000
 export CTR_TAG=osmlatest6
 make docker-push-osm-controller
 make docker-push-init
-#make build-osm
+
+#aws docker push
+export CTR_REGISTRY=978944737929.dkr.ecr.us-west-2.amazonaws.com
+make docker-push-osm-controller
+make docker-push-init
