b8931fed277038c28b53f096e91667e7a748be41
Eduard Serra <eduser25@gmail.com>
2020-09-16 13:09:50 -0700

    Speedup cleanup
    Run kubectl in parallel and wait for all of them to return.

--- demo/clean-kubernetes.sh
+++ demo/clean-kubernetes.sh
@@ -6,9 +6,11 @@ set -aueo pipefail
 source .env
 
 for ns in "$BOOKWAREHOUSE_NAMESPACE" "$BOOKBUYER_NAMESPACE" "$BOOKSTORE_NAMESPACE" "$BOOKTHIEF_NAMESPACE" "$K8S_NAMESPACE"; do
-    kubectl delete namespace "$ns" --ignore-not-found
+    kubectl delete namespace "$ns" --ignore-not-found &
 done
 
 # Clean up Hashicorp Vault deployment
-kubectl delete deployment vault -n "$K8S_NAMESPACE" --ignore-not-found
-kubectl delete service vault -n "$K8S_NAMESPACE" --ignore-not-found
+kubectl delete deployment vault -n "$K8S_NAMESPACE" --ignore-not-found &
+kubectl delete service vault -n "$K8S_NAMESPACE" --ignore-not-found &
+
+wait
