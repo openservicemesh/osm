34427fba0bcb18c4af8300782b322692c70b7cb9
Shashank Ram <shashank08@gmail.com>
2020-10-01 10:59:10 -0700

    Update cmd/cli/namespace_add.go
    Co-authored-by: Jon Huhn <nojnhuh@users.noreply.github.com>

--- cmd/cli/namespace_add.go
+++ cmd/cli/namespace_add.go
@@ -76,7 +76,7 @@ func newNamespaceAdd(out io.Writer) *cobra.Command {
 	f.StringVar(&namespaceAdd.meshName, "mesh-name", "osm", "Name of the service mesh")
 
 	//add sidecar injection flag
-	f.BoolVar(&namespaceAdd.enableSidecarInjection, "enable-sidecar-injection", true, "Enable or disable automatic sidecar injection, disabling requires explicitly setting this flag.")
+	f.BoolVar(&namespaceAdd.enableSidecarInjection, "enable-sidecar-injection", true, "Enable or disable automatic sidecar injection, disabling requires explicitly setting this flag to false.")
 
 	return cmd
 }
