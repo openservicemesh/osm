44a5fe264af05f017ab930ba00e71702e4fdf0eb
Eduard Serra <eduser25@gmail.com>
2020-10-08 14:59:20 -0700

    E2E first commit
    - Adds first E2E helper framework and functions
    - Adds First test utilizing the framework
    - Added README.md
    
    Co-authored-by: Jon Huhn <jon.huhn@microsoft.com>

---
+++ tests/e2e/README.md
@@ -0,0 +1,94 @@
+# E2E OSM Testing
+This folder contains End-to-End (E2E) OSM tests.
+
+E2E testing is mostly motivated by use-case driven scenarios, starting from a blank or empty cluster (or not even a cluster if Kind is backing the test, see details below), installing OSM from scratch, deploying applications and policies and ultimately testing traffic between specific cluster members to verify policy enforcement and proper mesh functioning. 
+
+## Files and structure
+Tests have been written under Ginkgo framework, and most of the helpers, wrappers and accessibility functions are provided in respective `common_*.go`, based on their areas of effect.
+
+The framework exposes an OSM test data structure or handle, which is in effect the interaction mechanism for the test itself.  The test framework takes care to collect and initialize most of the common functionalities a test would expect when deploying on K8s, including but not limited to Kubernetes and SMI clientsets, flag parsing, container registry values, cleanup hooks, etc., and provides accessibility functions through the handle for the test to use at its own discretion.
+
+The hooks for initialization and cleanup are set at Ginkgo's `BeforeEach` at the top level of test execution (between  Ginkgo `Describes`); we hencforce recommend keeping every test in its own `Describe` section, as well as on a separate file for clarity. You can refer to `suite_test.go` for more details.
+
+## Quick Start
+### Kind cluster
+The following `make` target will create local containers for the OSM components, tagging them with `CTR_TAG`, and will launch the tests using Kind cluster. A kind cluster is created at test start, and requires a docker interface to be available on the host running the test.
+When using Kind, we push the `CTR_TAG` images to the Kind nodes (instead of providing a registry).
+```
+CTR_TAG=not-latest make test-e2e
+```
+Note: If you use `latest` tag, and have not defined a registry, K8s will, by default, check the images on dockerhub, pulling and overriding the locally pushed images on the node.
+
+### Any other K8s deployment
+Have your `Kubeconf` pointing to your testing cluster of choice.
+The following code uses `latest` tag by default.
+```
+export CTR_REGISTRY=<myacr>.dockerhub.io # if needed, set CTR_REGISTRY_USER and CTR_REGISTRY_PASSWORD 
+make build-osm
+make docker-push
+go test ./tests/e2e -test.v -ginkgo.v -ginkgo.progress
+```
+
+## Flags
+### (TODO) Kubeconf selection
+Currently, test init will load a `Kubeconf` based on Defalut Kubeconf Loading rules. 
+If Kind is used, the kubeconf is temporarily replaced and Kind's kubeconf is used instead.
+
+### Container registry (required)
+A container registry where to load the images from (OSM, init container, etc.) is necessary. Can be set by flag or env value:
+```
+-ctrRegistry string
+		Container registry
+-ctrRegistrySecret string
+		Container registry secret
+-ctrRegistryUser string
+		Container registry username
+```
+If CR user and password are provided, the test framework will take care to add those as Docker secret credentials for the given container registry whenever appropriate (tenant namespaces for `init` containers, OSM intallation, etc).
+You can also set them through env:
+```
+export CTR_REGISTRY=<your_cr>.dockerhub.io
+export CTR_REGISTRY_USER=<uername>             # opt
+export CTR_REGISTRY_PASSWORD=<password>        # opt
+```
+
+### OSM Tag
+It will refer to the version of the OSM platform containers (OSM and init) for test to use:
+```
+-osmImageTag string
+		OSM image tag (default "latest")
+```
+Make sure you have compiled the images and pushed them on your registry first:
+```
+export CTR_REGISTRY=myacr.dockerhub.io
+export CTR_TAG=mytag               # Optional, 'latest' used by default
+make docker-push
+```
+
+### Use Kind for testing 
+Testing implements support for Kind. If `kindCluster` is enabled, a new Kind cluster will be provisioned and it will be automatically used for the test.
+```
+-kindCluster
+		Creates kind cluster
+-kindClusterName string
+		Name of the Kind cluster to be created (default "osm-e2e")
+-cleanupKindCluster
+		Cleanup kind cluster upon exit (default true)
+-cleanupKindClusterBetweenTests
+		Cleanup kind cluster between tests (default true)
+```
+
+### Test specific flags
+
+Worth mentioning `cleanupTest` is specially useful for debugging or leaving the test in a certain state at test-exit.
+When using Kind, you need to use `cleanupKindCluster` and `cleanupKindClusterBetweenTests` in conjunction, or else the cluster
+will anyway be destroyed.
+```
+-cleanupTest
+		Cleanup test resources when done (default true)
+-meshName string
+		OSM mesh name (default "osm-system")
+-waitForCleanup
+		Wait for effective deletion of resources (default true)
+```
+Plus, `go test` and `Ginkgo` specific flags, of course.
---
+++ tests/e2e/common_apps.go
@@ -0,0 +1,258 @@
+package e2e
+
+import (
+	"context"
+	"fmt"
+
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+)
+
+// CreateServiceAccount is a wrapper to create a service account
+func (td *OsmTestData) CreateServiceAccount(ns string, svcAccount *corev1.ServiceAccount) (*corev1.ServiceAccount, error) {
+	svcAc, err := td.client.CoreV1().ServiceAccounts(ns).Create(context.Background(), svcAccount, metav1.CreateOptions{})
+	if err != nil {
+		err := fmt.Errorf("Could not create Service Account: %v", err)
+		return nil, err
+	}
+	return svcAc, nil
+}
+
+// CreatePod is a wrapper to create a pod
+func (td *OsmTestData) CreatePod(ns string, pod corev1.Pod) (*corev1.Pod, error) {
+	podRet, err := td.client.CoreV1().Pods(ns).Create(context.Background(), &pod, metav1.CreateOptions{})
+	if err != nil {
+		err := fmt.Errorf("Could not create Pod: %v", err)
+		return nil, err
+	}
+	return podRet, nil
+}
+
+// CreateDeployment is a wrapper to create a deployment
+func (td *OsmTestData) CreateDeployment(ns string, deployment appsv1.Deployment) (*appsv1.Deployment, error) {
+	deploymentRet, err := td.client.AppsV1().Deployments(ns).Create(context.Background(), &deployment, metav1.CreateOptions{})
+	if err != nil {
+		err := fmt.Errorf("Could not create Deployment: %v", err)
+		return nil, err
+	}
+	return deploymentRet, nil
+}
+
+// CreateService is a wrapper to create a service
+func (td *OsmTestData) CreateService(ns string, svc corev1.Service) (*corev1.Service, error) {
+	sv, err := td.client.CoreV1().Services(ns).Create(context.Background(), &svc, metav1.CreateOptions{})
+	if err != nil {
+		err := fmt.Errorf("Could not create Service: %v", err)
+		return nil, err
+	}
+	return sv, nil
+}
+
+/* Application templates
+ * The following functions contain high level helpers to create and get test application definitions.
+ *
+ * These abstractions aim to simplify and avoid tests having to individually type the the same k8s definitions for
+ * some common or recurrent deployment forms.
+ */
+
+// SimplePodAppDef defines some parametrization to create a pod-based application from template
+type SimplePodAppDef struct {
+	namespace string
+	name      string
+	image     string
+	command   []string
+	args      []string
+	ports     []int
+}
+
+// SimplePodApp creates returns a set of k8s typed definitions for a pod-based k8s definition.
+// Includes Pod, Service and ServiceAccount types
+func (td *OsmTestData) SimplePodApp(def SimplePodAppDef) (corev1.ServiceAccount, corev1.Pod, corev1.Service) {
+	serviceAccountDefinition := corev1.ServiceAccount{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: def.name,
+		},
+	}
+
+	podDefinition := corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: def.name,
+			Labels: map[string]string{
+				"app": def.name,
+			},
+		},
+		Spec: corev1.PodSpec{
+			ServiceAccountName: def.name,
+			Containers: []corev1.Container{
+				{
+					Name:            def.name,
+					Image:           def.image,
+					ImagePullPolicy: corev1.PullIfNotPresent,
+					Resources: corev1.ResourceRequirements{
+						Limits: corev1.ResourceList{
+							corev1.ResourceCPU:    resource.MustParse("100m"),
+							corev1.ResourceMemory: resource.MustParse("128Mi"),
+						},
+					},
+				},
+			},
+		},
+	}
+
+	if td.AreRegistryCredsPresent() {
+		podDefinition.Spec.ImagePullSecrets = []corev1.LocalObjectReference{
+			{
+				Name: registrySecretName,
+			},
+		}
+	}
+	if def.command != nil && len(def.command) > 0 {
+		podDefinition.Spec.Containers[0].Command = def.command
+	}
+	if def.args != nil && len(def.args) > 0 {
+		podDefinition.Spec.Containers[0].Args = def.args
+	}
+
+	serviceDefinition := corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: def.name,
+			Labels: map[string]string{
+				"app": def.name,
+			},
+		},
+		Spec: corev1.ServiceSpec{
+			Selector: map[string]string{
+				"app": def.name,
+			},
+		},
+	}
+
+	if def.ports != nil && len(def.ports) > 0 {
+		podDefinition.Spec.Containers[0].Ports = []corev1.ContainerPort{}
+		serviceDefinition.Spec.Ports = []corev1.ServicePort{}
+
+		for _, p := range def.ports {
+			podDefinition.Spec.Containers[0].Ports = append(podDefinition.Spec.Containers[0].Ports,
+				corev1.ContainerPort{
+					ContainerPort: int32(p),
+				},
+			)
+			serviceDefinition.Spec.Ports = append(serviceDefinition.Spec.Ports, corev1.ServicePort{
+				Port:       int32(p),
+				TargetPort: intstr.FromInt(p),
+			})
+		}
+	}
+
+	return serviceAccountDefinition, podDefinition, serviceDefinition
+}
+
+// SimpleDeploymentAppDef defines some parametrization to create a deployment-based application from template
+type SimpleDeploymentAppDef struct {
+	namespace    string
+	name         string
+	image        string
+	replicaCount int32
+	command      []string
+	args         []string
+	ports        []int
+}
+
+// SimpleDeploymentApp creates returns a set of k8s typed definitions for a deployment-based k8s definition.
+// Includes Deployment, Service and ServiceAccount types
+func (td *OsmTestData) SimpleDeploymentApp(def SimpleDeploymentAppDef) (corev1.ServiceAccount, appsv1.Deployment, corev1.Service) {
+	serviceAccountDefinition := corev1.ServiceAccount{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      def.name,
+			Namespace: def.namespace,
+		},
+	}
+
+	// Required, as replica count is a pointer to distinguish between 0 and not specified
+	replicaCountExplicitDeclaration := def.replicaCount
+
+	deploymentDefinition := appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      def.name,
+			Namespace: def.namespace,
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: &replicaCountExplicitDeclaration,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app": def.name,
+				},
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"app": def.name,
+					},
+				},
+				Spec: corev1.PodSpec{
+					ServiceAccountName: def.name,
+					Containers: []corev1.Container{
+						{
+							Name:            def.name,
+							Image:           def.image,
+							ImagePullPolicy: corev1.PullIfNotPresent,
+						},
+					},
+				},
+			},
+		},
+	}
+
+	if td.AreRegistryCredsPresent() {
+		deploymentDefinition.Spec.Template.Spec.ImagePullSecrets = []corev1.LocalObjectReference{
+			{
+				Name: registrySecretName,
+			},
+		}
+	}
+
+	if def.command != nil && len(def.command) > 0 {
+		deploymentDefinition.Spec.Template.Spec.Containers[0].Command = def.command
+	}
+	if def.args != nil && len(def.args) > 0 {
+		deploymentDefinition.Spec.Template.Spec.Containers[0].Args = def.args
+	}
+
+	serviceDefinition := corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      def.name,
+			Namespace: def.namespace,
+			Labels: map[string]string{
+				"app": def.name,
+			},
+		},
+		Spec: corev1.ServiceSpec{
+			Selector: map[string]string{
+				"app": def.name,
+			},
+		},
+	}
+
+	if def.ports != nil && len(def.ports) > 0 {
+		deploymentDefinition.Spec.Template.Spec.Containers[0].Ports = []corev1.ContainerPort{}
+		serviceDefinition.Spec.Ports = []corev1.ServicePort{}
+
+		for _, p := range def.ports {
+			deploymentDefinition.Spec.Template.Spec.Containers[0].Ports = append(deploymentDefinition.Spec.Template.Spec.Containers[0].Ports,
+				corev1.ContainerPort{
+					ContainerPort: int32(p),
+				},
+			)
+
+			serviceDefinition.Spec.Ports = append(serviceDefinition.Spec.Ports, corev1.ServicePort{
+				Port:       int32(p),
+				TargetPort: intstr.FromInt(p),
+			})
+		}
+	}
+
+	return serviceAccountDefinition, deploymentDefinition, serviceDefinition
+}
