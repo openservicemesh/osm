47cab3dae477418340e5cc41cd1b03c46246a6c4
Shashank Ram <shashank08@gmail.com>
2020-10-21 09:09:25 -0700

    cli: add proxy command to dump config
    Adds a cli command to dump proxy configuration
    for a given pod.
    
    Resolves #1762

--- cmd/cli/osm.go
+++ cmd/cli/osm.go
@@ -43,6 +43,7 @@ func newRootCmd(config *action.Configuration, in io.Reader, out io.Writer, args 
 		newNamespaceCmd(out),
 		newMetricsCmd(out),
 		newVersionCmd(out),
+		newProxyCmd(config, out),
 	)
 
 	flags.Parse(args)
