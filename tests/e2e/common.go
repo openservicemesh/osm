f4636713cd17430b246d41053f6320260182cdfb
Michelle Noorali <michellemolu@gmail.com>
2020-10-22 15:35:31 -0400

    fix value

--- tests/e2e/common.go
+++ tests/e2e/common.go
@@ -191,35 +191,29 @@ func (td *OsmTestData) GetOSMInstallOpts() InstallOSMOpts {
 }
 
 // HelmInstallOSM installs an osm control plane using the osm chart which lives in charts/osm
-func (td *OsmTestData) HelmInstallOSM() error {
+func (td *OsmTestData) HelmInstallOSM(release, namespace string) error {
 	if td.kindCluster {
 		if err := td.loadOSMImagesIntoKind(); err != nil {
 			return err
 		}
 	}
-	namespace := "helm-install-namespace"
-	release := "helm-install-osm"
 
-	values := fmt.Sprintf("OpenServiceMesh.image.registry=%s,OpenServiceMesh.image.tag=%s,meshName=%s", td.ctrRegistryServer, td.osmImageTag, release)
+	values := fmt.Sprintf("OpenServiceMesh.image.registry=%s,OpenServiceMesh.image.tag=%s,OpenServiceMesh.meshName=%s", td.ctrRegistryServer, td.osmImageTag, release)
 	args := []string{"install", release, "../../charts/osm", "--set", values, "--namespace", namespace, "--create-namespace", "--wait"}
 	stdout, stderr, err := td.RunLocal("helm", args)
 	if err != nil {
 		td.T.Logf("stdout:\n%s", stdout)
 		return errors.Errorf("failed to run helm install with osm chart: %s", stderr)
 	}
-	if err = td.deleteHelmRelease(release, namespace); err != nil {
-		td.T.Fatal(err)
-	}
 
 	return nil
 }
 
-func (td *OsmTestData) deleteHelmRelease(name, namespace string) error {
+func (td *OsmTestData) DeleteHelmRelease(name, namespace string) error {
 	args := []string{"uninstall", name, "--namespace", namespace}
-	stdout, stderr, err := td.RunLocal("helm", args)
+	_, _, err := td.RunLocal("helm", args)
 	if err != nil {
-		td.T.Logf("stdout:\n%s", stdout)
-		return errors.Errorf("Could not delete helm release: %s", stderr)
+		td.T.Fatal(err)
 	}
 	return nil
 }
@@ -290,6 +284,15 @@ func (td *OsmTestData) InstallOSM(instOpts InstallOSMOpts) error {
 	}
 
 	return nil
+}
+
+// GetConfigMap is a wrapper to get a config map by name in a particular namespace
+func (td *OsmTestData) GetConfigMap(name, namespace string) (*corev1.ConfigMap, error) {
+	configmap, err := td.client.CoreV1().ConfigMaps(namespace).Get(context.Background(), name, metav1.GetOptions{})
+	if err != nil {
+		return nil, err
+	}
+	return configmap, nil
 }
 
 func (td *OsmTestData) loadOSMImagesIntoKind() error {
