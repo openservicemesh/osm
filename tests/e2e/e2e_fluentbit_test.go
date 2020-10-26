a6552193040df4698a3ebf5e7e77177c26512c56
Jon Huhn <huhnjon@gmail.com>
2020-10-26 15:46:58 -0500

    ref(e2e): divide tests into buckets
    This change adds the capability to run e2e tests in parallel in CI.
    Tests are now organized into buckets, where each bucket runs in parallel
    and tests within each bucket run sequentially. How e2e tests are defined
    at the top-level has been refactored to include a new parameter
    identifying in which bucket a test should run.
    
    Tests were not organized into buckets initially based on any heuristic,
    so rebalancing may be necessary. Adding more buckets is also simple.

--- tests/e2e/e2e_fluentbit_test.go
+++ tests/e2e/e2e_fluentbit_test.go
@@ -11,53 +11,58 @@ import (
 	"k8s.io/apimachinery/pkg/labels"
 )
 
-var _ = DescribeTier2("Test deployment of Fluent Bit sidecar", func() {
-	Context("Fluentbit", func() {
-		It("Deploys a Fluent Bit sidecar only when enabled", func() {
-			// Install OSM with Fluentbit
-			installOpts := td.GetOSMInstallOpts()
-			installOpts.deployFluentbit = true
-			Expect(td.InstallOSM(installOpts)).To(Succeed())
+var _ = OSMDescribe("Test deployment of Fluent Bit sidecar",
+	OSMDescribeInfo{
+		tier:   2,
+		bucket: 2,
+	},
+	func() {
+		Context("Fluentbit", func() {
+			It("Deploys a Fluent Bit sidecar only when enabled", func() {
+				// Install OSM with Fluentbit
+				installOpts := td.GetOSMInstallOpts()
+				installOpts.deployFluentbit = true
+				Expect(td.InstallOSM(installOpts)).To(Succeed())
 
-			pods, err := td.client.CoreV1().Pods(td.osmNamespace).List(context.TODO(), metav1.ListOptions{
-				LabelSelector: labels.SelectorFromSet(map[string]string{"app": "osm-controller"}).String(),
-			})
+				pods, err := td.client.CoreV1().Pods(td.osmNamespace).List(context.TODO(), metav1.ListOptions{
+					LabelSelector: labels.SelectorFromSet(map[string]string{"app": "osm-controller"}).String(),
+				})
 
-			Expect(err).NotTo(HaveOccurred())
-			cond := false
-			for _, pod := range pods.Items {
-				for _, container := range pod.Spec.Containers {
-					if strings.Contains(container.Image, "fluent-bit") {
-						cond = true
+				Expect(err).NotTo(HaveOccurred())
+				cond := false
+				for _, pod := range pods.Items {
+					for _, container := range pod.Spec.Containers {
+						if strings.Contains(container.Image, "fluent-bit") {
+							cond = true
+						}
 					}
 				}
-			}
-			Expect(cond).To(BeTrue())
+				Expect(cond).To(BeTrue())
 
-			err = td.DeleteNs(td.osmNamespace)
-			Expect(err).NotTo(HaveOccurred())
-			err = td.WaitForNamespacesDeleted([]string{td.osmNamespace}, 60*time.Second)
-			Expect(err).NotTo(HaveOccurred())
+				err = td.DeleteNs(td.osmNamespace)
+				Expect(err).NotTo(HaveOccurred())
+				err = td.WaitForNamespacesDeleted([]string{td.osmNamespace}, 60*time.Second)
+				Expect(err).NotTo(HaveOccurred())
 
-			// Install OSM without Fluentbit (default)
-			installOpts = td.GetOSMInstallOpts()
-			Expect(td.InstallOSM(installOpts)).To(Succeed())
-			Expect(td.WaitForPodsRunningReady(td.osmNamespace, 60*time.Second, 1)).To(Succeed())
+				// Install OSM without Fluentbit (default)
+				installOpts = td.GetOSMInstallOpts()
+				Expect(td.InstallOSM(installOpts)).To(Succeed())
+				Expect(td.WaitForPodsRunningReady(td.osmNamespace, 60*time.Second, 1)).To(Succeed())
 
-			pods, err = td.client.CoreV1().Pods(td.osmNamespace).List(context.TODO(), metav1.ListOptions{
-				LabelSelector: labels.SelectorFromSet(map[string]string{"app": "osm-controller"}).String(),
-			})
+				pods, err = td.client.CoreV1().Pods(td.osmNamespace).List(context.TODO(), metav1.ListOptions{
+					LabelSelector: labels.SelectorFromSet(map[string]string{"app": "osm-controller"}).String(),
+				})
 
-			Expect(err).NotTo(HaveOccurred())
-			cond = false
-			for _, pod := range pods.Items {
-				for _, container := range pod.Spec.Containers {
-					if strings.Contains(container.Image, "fluent-bit") {
-						cond = true
+				Expect(err).NotTo(HaveOccurred())
+				cond = false
+				for _, pod := range pods.Items {
+					for _, container := range pod.Spec.Containers {
+						if strings.Contains(container.Image, "fluent-bit") {
+							cond = true
+						}
 					}
 				}
-			}
-			Expect(cond).To(BeFalse())
+				Expect(cond).To(BeFalse())
+			})
 		})
 	})
-})
