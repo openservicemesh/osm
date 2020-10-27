a9a1ea6bb1309a70a0352098030763123ab8ce87
Shalier Xia <shalierxia@microsoft.com>
2020-10-27 12:08:26 -0400

    unit test changes

--- pkg/httpserver/debugger.go
+++ pkg/httpserver/debugger.go
@@ -14,8 +14,14 @@ type DebugServer struct {
 	Server *http.Server
 }
 
+//DebugServerInterface contains debug server functions
+type DebugServerInterface interface {
+	Stop() error
+	Start()
+}
+
 // NewDebugHTTPServer creates a new API Server for Debug
-func NewDebugHTTPServer(debugServer debugger.DebugServer, apiPort int32) *DebugServer {
+func NewDebugHTTPServer(debugServer debugger.DebugServer, apiPort int32) DebugServerInterface {
 	return &DebugServer{
 		Server: &http.Server{
 			Addr:    fmt.Sprintf(":%d", apiPort),
