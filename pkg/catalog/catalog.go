2181d86f03265ecd343d5cab2fc983e43c438eaf
Delyan Raychev <delyan.raychev@microsoft.com>
2020-10-29 16:09:21 -0700

    catalog: Remove the announcementChannels set field from the MeshCatalog struct (#1954)

--- pkg/catalog/catalog.go
+++ pkg/catalog/catalog.go
@@ -3,7 +3,6 @@ package catalog
 import (
 	"time"
 
-	set "github.com/deckarep/golang-set"
 	"k8s.io/client-go/kubernetes"
 
 	"github.com/openservicemesh/osm/pkg/certificate"
@@ -17,17 +16,16 @@ import (
 // NewMeshCatalog creates a new service catalog
 func NewMeshCatalog(kubeController k8s.Controller, kubeClient kubernetes.Interface, meshSpec smi.MeshSpec, certManager certificate.Manager, ingressMonitor ingress.Monitor, stop <-chan struct{}, cfg configurator.Configurator, endpointsProviders ...endpoint.Provider) *MeshCatalog {
 	log.Info().Msg("Create a new Service MeshCatalog.")
-	sc := MeshCatalog{
+	mc := MeshCatalog{
 		endpointsProviders: endpointsProviders,
 		meshSpec:           meshSpec,
 		certManager:        certManager,
 		ingressMonitor:     ingressMonitor,
 		configurator:       cfg,
 
-		expectedProxies:      make(map[certificate.CommonName]expectedProxy),
-		connectedProxies:     make(map[certificate.CommonName]connectedProxy),
-		disconnectedProxies:  make(map[certificate.CommonName]disconnectedProxy),
-		announcementChannels: set.NewSet(),
+		expectedProxies:     make(map[certificate.CommonName]expectedProxy),
+		connectedProxies:    make(map[certificate.CommonName]connectedProxy),
+		disconnectedProxies: make(map[certificate.CommonName]disconnectedProxy),
 
 		// Kubernetes needed to determine what Services a pod that connects to XDS belongs to.
 		// In multicluster scenarios this would be a map of cluster ID to Kubernetes client.
@@ -36,12 +34,8 @@ func NewMeshCatalog(kubeController k8s.Controller, kubeClient kubernetes.Interfa
 		kubeController: kubeController,
 	}
 
-	for _, announcementChannel := range sc.getAnnouncementChannels() {
-		sc.announcementChannels.Add(announcementChannel)
-	}
-
-	go sc.repeater()
-	return &sc
+	go mc.repeater()
+	return &mc
 }
 
 // GetSMISpec returns a MeshCatalog's SMI Spec
@@ -59,6 +53,8 @@ func (mc *MeshCatalog) getAnnouncementChannels() []announcementChannel {
 		{"Namespace", mc.kubeController.GetAnnouncementsChannel(k8s.Namespaces)},
 		{"Services", mc.kubeController.GetAnnouncementsChannel(k8s.Services)},
 	}
+
+	// There could be many Endpoint Providers - iterate over all of them!
 	for _, ep := range mc.endpointsProviders {
 		annCh := announcementChannel{ep.GetID(), ep.GetAnnouncementsChannel()}
 		announcementChannels = append(announcementChannels, annCh)
@@ -71,5 +67,6 @@ func (mc *MeshCatalog) getAnnouncementChannels() []announcementChannel {
 		ticker := time.NewTicker(updateAtLeastEvery)
 		ticking <- ticker.C
 	}()
+
 	return announcementChannels
 }
--- pkg/catalog/repeater.go
+++ pkg/catalog/repeater.go
@@ -44,8 +44,7 @@ func (mc *MeshCatalog) handleAnnouncement(ann announcements.Announcement) {
 func (mc *MeshCatalog) getCases() ([]reflect.SelectCase, []string) {
 	var caseNames []string
 	var cases []reflect.SelectCase
-	for _, channelInterface := range mc.announcementChannels.ToSlice() {
-		annCh := channelInterface.(announcementChannel)
+	for _, annCh := range mc.getAnnouncementChannels() {
 		cases = append(cases, reflect.SelectCase{Dir: reflect.SelectRecv, Chan: reflect.ValueOf(annCh.channel)})
 		caseNames = append(caseNames, annCh.announcer)
 	}
