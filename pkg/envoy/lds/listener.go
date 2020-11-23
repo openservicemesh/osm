fb072dc90c44c1018d6c222a21f2b81848d279b4
Shashank Ram <shashank08@gmail.com>
2020-11-23 15:10:46 -0800

    envoy/lds: consolidate HTTP filter chain building (#2113)
    Consolidates logic to build the outbound HTTP filter chain.
    This is in preparation to build the outbound TCP filter chain.
    
    Signed-off-by: Shashank Ram <shashank08@gmail.com>

--- pkg/envoy/lds/listener.go
+++ pkg/envoy/lds/listener.go
@@ -149,26 +149,13 @@ func (lb *listenerBuilder) getOutboundFilterChains(downstreamSvc []service.MeshS
 	}
 
 	// Iterate all destination services
-	for keyService := range dstServicesSet {
-		// Get HTTP filter for service
-		filter, err := lb.getOutboundHTTPFilter()
-		if err != nil {
-			log.Error().Err(err).Msgf("Error getting filter for dst service %s", keyService.String())
-			continue
+	for upstream := range dstServicesSet {
+		// Construct HTTP filter chain
+		if httpFilterChain, err := lb.getOutboundHTTPFilterChainForService(upstream); err != nil {
+			log.Error().Err(err).Msgf("Error constructing outbount HTTP filter chain for upstream service %q", upstream)
+		} else {
+			filterChains = append(filterChains, httpFilterChain)
 		}
-
-		// Get filter match criteria for destination service
-		filterChainMatch, err := lb.getOutboundHTTPFilterChainMatchForService(keyService)
-		if err != nil {
-			log.Error().Err(err).Msgf("Error getting Chain Match for service %s", keyService.String())
-			continue
-		}
-
-		filterChains = append(filterChains, &xds_listener.FilterChain{
-			Name:             keyService.String(),
-			Filters:          []*xds_listener.Filter{filter},
-			FilterChainMatch: filterChainMatch,
-		})
 	}
 
 	// This filterchain matches any traffic not filtered by allow rules, it will be treated as egress
