f010b977a214605a925bbb0aa6bcc68c360164a2
Shashank Ram <shashank08@gmail.com>
2020-12-02 15:38:05 -0800

    catalog: add api to fetch service's port:protocol mapping across providers
    Adds an api to the MeshCataloger interface to fetch the port:protocol
    mapping for a service's ports. Some safety checks have been put in place
    to ensure the port:protocol mapping for a service's endpoints is the
    same across providers.
    
    Signed-off-by: Shashank Ram <shashank08@gmail.com>

--- pkg/catalog/service.go
+++ pkg/catalog/service.go
@@ -1,7 +1,10 @@
 package catalog
 
 import (
+	"reflect"
 	"strings"
+
+	"github.com/pkg/errors"
 
 	"github.com/openservicemesh/osm/pkg/service"
 )
@@ -34,4 +37,30 @@ func (mc *MeshCatalog) GetServicesForServiceAccount(sa service.K8sServiceAccount
 func (mc *MeshCatalog) ListServiceAccountsForService(svc service.MeshService) ([]service.K8sServiceAccount, error) {
 	// Currently OSM uses kubernetes service accounts as service identities
 	return mc.kubeController.ListServiceAccountsForService(svc)
+}
+
+// GetPortToProtocolMappingForService returns a mapping of the service's ports to their corresponding application protocol.
+// The function ensures the port:protocol mapping is the same across different endpoint providers for the service, and returns
+// an error otherwise.
+func (mc *MeshCatalog) GetPortToProtocolMappingForService(svc service.MeshService) (map[uint32]string, error) {
+	var portToProtocolMap, previous map[uint32]string
+
+	for _, provider := range mc.endpointsProviders {
+		current, err := provider.GetPortToProtocolMappingForService(svc)
+		if err != nil {
+			return nil, err
+		}
+
+		if previous != nil && !reflect.DeepEqual(previous, current) {
+			log.Error().Msgf("Service %s does not have the same port:protocol map across providers: expected=%v, got=%v", svc, previous, current)
+			return nil, errors.Errorf("Service %s does not have the same port:protocol mapping across providers", svc)
+		}
+		previous = current
+	}
+	portToProtocolMap = previous
+	if portToProtocolMap == nil {
+		return nil, errors.Errorf("Error fetching port:protocol mapping for service %s", svc)
+	}
+
+	return portToProtocolMap, nil
 }
