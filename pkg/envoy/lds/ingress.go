8a6ed0f262790404db6e26971fba30432ef2e51b
Shashank Ram <shashank08@gmail.com>
2020-12-03 15:51:44 -0800

    envoy/lds: create inbound filter chains per service port (#2140)
    This change makes the inbound filter chains for in-mesh
    and ingress a per port construct, so that protocol
    specific filters and rules can be enforced per port.
    Since protocol specific filters (ex. TCP proxy vs HTTP
    Connection Manager) cannot be filter on the same filter
    chain, a port specific filter chain is created to be
    able to handle per port:protocol filtering.
    
    Additionally, this change adds new tests for the ingress
    filter chain and updates the existing tests for mesh filter
    chains.
    
    This is required for TCP filtering and routing: #1521
    
    Signed-off-by: Shashank Ram <shashank08@gmail.com>

--- pkg/envoy/lds/ingress.go
+++ pkg/envoy/lds/ingress.go
@@ -1,12 +1,14 @@
 package lds
 
 import (
+	"fmt"
+
 	xds_core "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
 	xds_listener "github.com/envoyproxy/go-control-plane/envoy/config/listener/v3"
 	"github.com/envoyproxy/go-control-plane/pkg/wellknown"
-
 	"github.com/golang/protobuf/ptypes"
 	"github.com/golang/protobuf/ptypes/any"
+	"google.golang.org/protobuf/types/known/wrapperspb"
 
 	"github.com/openservicemesh/osm/pkg/configurator"
 	"github.com/openservicemesh/osm/pkg/envoy"
@@ -15,18 +17,21 @@ import (
 )
 
 const (
+	// inboundIngressHTTPSFilterChain is the name of the ingress filter chain to handle HTTPS traffic with SNI set
 	inboundIngressHTTPSFilterChain = "inbound-ingress-https-filter-chain"
-	inboundIngressHTTPFilterChain  = "inbound-ingress-http-filter-chain"
+
+	// inboundIngressNonSNIFilterChain is the name of the ingress filter chain that handles either HTTP or HTTPS traffic without SNI set
+	inboundIngressNonSNIFilterChain = "inbound-ingress-non-sni-filter-chain"
 )
 
-func getIngressTransportProtocol(cfg configurator.Configurator) string {
-	if cfg.UseHTTPSIngress() {
+func getIngressTransportProtocol(forHTTPS bool) string {
+	if forHTTPS {
 		return envoy.TransportProtocolTLS
 	}
 	return ""
 }
 
-func newIngressFilterChain(cfg configurator.Configurator, svc service.MeshService) *xds_listener.FilterChain {
+func newIngressHTTPFilterChain(cfg configurator.Configurator, svc service.MeshService, svcPort uint32) *xds_listener.FilterChain {
 	marshalledDownstreamTLSContext, err := ptypes.MarshalAny(envoy.GetDownstreamTLSContext(svc, false /* TLS */))
 	if err != nil {
 		log.Error().Err(err).Msgf("Error marshalling DownstreamTLSContext object for proxy %s", svc)
@@ -43,9 +48,12 @@ func newIngressFilterChain(cfg configurator.Configurator, svc service.MeshServic
 	return &xds_listener.FilterChain{
 		// Filter chain with SNI matching enabled for clients that set the SNI
 		FilterChainMatch: &xds_listener.FilterChainMatch{
-			TransportProtocol: getIngressTransportProtocol(cfg),
+			DestinationPort: &wrapperspb.UInt32Value{
+				Value: svcPort,
+			},
+			TransportProtocol: getIngressTransportProtocol(cfg.UseHTTPSIngress()),
 		},
-		TransportSocket: getIngressTransportSocket(cfg, marshalledDownstreamTLSContext),
+		TransportSocket: getIngressTransportSocket(cfg.UseHTTPSIngress(), marshalledDownstreamTLSContext),
 		Filters: []*xds_listener.Filter{
 			{
 				Name: wellknown.HTTPConnectionManager,
@@ -57,27 +65,43 @@ func newIngressFilterChain(cfg configurator.Configurator, svc service.MeshServic
 	}
 }
 
-func getIngressFilterChains(svc service.MeshService, cfg configurator.Configurator) []*xds_listener.FilterChain {
+func (lb *listenerBuilder) getIngressFilterChains(svc service.MeshService) []*xds_listener.FilterChain {
 	var ingressFilterChains []*xds_listener.FilterChain
 
-	if cfg.UseHTTPSIngress() {
-		// Filter chain with SNI matching enabled for HTTPS clients that set the SNI
-		ingressFilterChainWithSNI := newIngressFilterChain(cfg, svc)
-		ingressFilterChainWithSNI.Name = inboundIngressHTTPSFilterChain
-		ingressFilterChainWithSNI.FilterChainMatch.ServerNames = []string{svc.ServerName()}
-		ingressFilterChains = append(ingressFilterChains, ingressFilterChainWithSNI)
+	protocolToPortMap, err := lb.meshCatalog.GetPortToProtocolMappingForService(svc)
+	if err != nil {
+		log.Error().Err(err).Msgf("Error retrieving port to protocol mapping for service %s", svc)
+		return ingressFilterChains
 	}
 
-	// Filter chain without SNI matching enabled for HTTP clients and HTTPS clients that don't set the SNI
-	ingressFilterChainWithoutSNI := newIngressFilterChain(cfg, svc)
-	ingressFilterChainWithoutSNI.Name = inboundIngressHTTPFilterChain
-	ingressFilterChains = append(ingressFilterChains, ingressFilterChainWithoutSNI)
+	// Create protocol specific inbound filter chains per port to handle different ports serving different protocols
+	for port, appProtocol := range protocolToPortMap {
+		switch appProtocol {
+		case httpAppProtocol:
+			// Ingress filter chain for HTTP port
+			if lb.cfg.UseHTTPSIngress() {
+				// Filter chain with SNI matching enabled for HTTPS clients that set the SNI
+				ingressFilterChainWithSNI := newIngressHTTPFilterChain(lb.cfg, svc, port)
+				ingressFilterChainWithSNI.Name = fmt.Sprintf("%s:%d", inboundIngressHTTPSFilterChain, port)
+				ingressFilterChainWithSNI.FilterChainMatch.ServerNames = []string{svc.ServerName()}
+				ingressFilterChains = append(ingressFilterChains, ingressFilterChainWithSNI)
+			}
+
+			// Filter chain without SNI matching enabled for HTTP clients and HTTPS clients that don't set the SNI
+			ingressFilterChainWithoutSNI := newIngressHTTPFilterChain(lb.cfg, svc, port)
+			ingressFilterChainWithoutSNI.Name = fmt.Sprintf("%s:%d", inboundIngressNonSNIFilterChain, port)
+			ingressFilterChains = append(ingressFilterChains, ingressFilterChainWithoutSNI)
+
+		default:
+			log.Error().Msgf("Cannot build inbound filter chain, unsupported protocol %s for proxy:port %s:%d", appProtocol, svc, port)
+		}
+	}
 
 	return ingressFilterChains
 }
 
-func getIngressTransportSocket(cfg configurator.Configurator, marshalledDownstreamTLSContext *any.Any) *xds_core.TransportSocket {
-	if cfg.UseHTTPSIngress() {
+func getIngressTransportSocket(forHTTPS bool, marshalledDownstreamTLSContext *any.Any) *xds_core.TransportSocket {
+	if forHTTPS {
 		return &xds_core.TransportSocket{
 			Name: wellknown.TransportSocketTls,
 			ConfigType: &xds_core.TransportSocket_TypedConfig{
---
+++ pkg/envoy/lds/ingress_test.go
@@ -0,0 +1,152 @@
+package lds
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+
+	xds_listener "github.com/envoyproxy/go-control-plane/envoy/config/listener/v3"
+	"github.com/envoyproxy/go-control-plane/pkg/wellknown"
+	"google.golang.org/protobuf/types/known/wrapperspb"
+
+	"github.com/openservicemesh/osm/pkg/catalog"
+	"github.com/openservicemesh/osm/pkg/configurator"
+	"github.com/openservicemesh/osm/pkg/tests"
+)
+
+func TestGetIngressFilterChains(t *testing.T) {
+	assert := assert.New(t)
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	proxyService := tests.BookstoreV1Service
+
+	testCases := []struct {
+		name                 string
+		httpsIngress         bool // true for https, false for http
+		svcPortToProtocolMap map[uint32]string
+		portToProtocolErr    error // error to return if port:protocol mapping returns an error
+
+		expectedFilterChainCount               int
+		expectedFilterNamesPerFilterChain      []string
+		expectedFilterChainMatchPerFilterChain []*xds_listener.FilterChainMatch
+	}{
+		{
+			// Test case 1
+			name:                 "HTTP ingress filter chain for service with multiple ports",
+			httpsIngress:         false,
+			svcPortToProtocolMap: map[uint32]string{80: "http", 90: "http"},
+			portToProtocolErr:    nil,
+
+			expectedFilterChainCount:          2, // 1 per http port on the service
+			expectedFilterNamesPerFilterChain: []string{wellknown.HTTPConnectionManager},
+			expectedFilterChainMatchPerFilterChain: []*xds_listener.FilterChainMatch{
+				{
+					DestinationPort:   &wrapperspb.UInt32Value{Value: 80},
+					TransportProtocol: "",
+				},
+				{
+					DestinationPort:   &wrapperspb.UInt32Value{Value: 90},
+					TransportProtocol: "",
+				},
+			},
+		},
+
+		{
+			// Test case 2
+			name:                 "HTTPS ingress filter chain for service with multiple ports",
+			httpsIngress:         true,
+			svcPortToProtocolMap: map[uint32]string{80: "http", 90: "http"},
+			portToProtocolErr:    nil,
+
+			expectedFilterChainCount:          4, // number of ports * 2; 2 because for HTTPS 2 filter chains are created: with and without SNI matching
+			expectedFilterNamesPerFilterChain: []string{wellknown.HTTPConnectionManager},
+			expectedFilterChainMatchPerFilterChain: []*xds_listener.FilterChainMatch{
+				{
+					DestinationPort:   &wrapperspb.UInt32Value{Value: 80},
+					TransportProtocol: "tls",
+				},
+				{
+					DestinationPort:   &wrapperspb.UInt32Value{Value: 90},
+					TransportProtocol: "tls",
+				},
+				{
+					DestinationPort:   &wrapperspb.UInt32Value{Value: 80},
+					TransportProtocol: "tls",
+					ServerNames:       []string{proxyService.ServerName()},
+				},
+				{
+					DestinationPort:   &wrapperspb.UInt32Value{Value: 90},
+					TransportProtocol: "tls",
+					ServerNames:       []string{proxyService.ServerName()},
+				},
+			},
+		},
+	}
+
+	for i, tc := range testCases {
+		t.Run(fmt.Sprintf("Testing test case %d: %s", i, tc.name), func(t *testing.T) {
+			mockCatalog := catalog.NewMockMeshCataloger(mockCtrl)
+			mockConfigurator := configurator.NewMockConfigurator(mockCtrl)
+
+			lb := &listenerBuilder{
+				meshCatalog: mockCatalog,
+				cfg:         mockConfigurator,
+				svcAccount:  tests.BookstoreServiceAccount,
+			}
+
+			// Mock catalog call to get port:protocol mapping for service
+			mockCatalog.EXPECT().GetPortToProtocolMappingForService(proxyService).Return(tc.svcPortToProtocolMap, tc.portToProtocolErr).Times(1)
+			// Mock configurator calls to determine HTTP vs HTTPS ingress
+			mockConfigurator.EXPECT().UseHTTPSIngress().Return(tc.httpsIngress).AnyTimes()
+			// Mock calls used to build the HTTP connection manager
+			mockConfigurator.EXPECT().IsTracingEnabled().Return(false).AnyTimes()
+
+			filterChains := lb.getIngressFilterChains(proxyService)
+
+			// Test the filter chains
+			assert.Len(filterChains, tc.expectedFilterChainCount)
+
+			var actualFilterChainMatchPerFilterChain []*xds_listener.FilterChainMatch
+			for _, filterChain := range filterChains {
+				assert.Len(filterChain.Filters, len(tc.expectedFilterNamesPerFilterChain))
+				for i, filter := range filterChain.Filters {
+					assert.Equal(tc.expectedFilterNamesPerFilterChain[i], filter.Name)
+				}
+				actualFilterChainMatchPerFilterChain = append(actualFilterChainMatchPerFilterChain, filterChain.FilterChainMatch)
+			}
+
+			assert.ElementsMatch(tc.expectedFilterChainMatchPerFilterChain, actualFilterChainMatchPerFilterChain)
+		})
+	}
+}
+
+func TestGetIngressTransportProtocol(t *testing.T) {
+	assert := assert.New(t)
+
+	testCases := []struct {
+		name                      string
+		forHTTPS                  bool
+		expectedTransportProtocol string
+	}{
+		{
+			name:                      "for HTTP",
+			forHTTPS:                  false,
+			expectedTransportProtocol: "",
+		},
+		{
+			name:                      "for HTTPS",
+			forHTTPS:                  true,
+			expectedTransportProtocol: "tls",
+		},
+	}
+
+	for i, tc := range testCases {
+		t.Run(fmt.Sprintf("Testing test case %d: %s", i, tc.name), func(t *testing.T) {
+			actual := getIngressTransportProtocol(tc.forHTTPS)
+			assert.Equal(tc.expectedTransportProtocol, actual)
+		})
+	}
+}
