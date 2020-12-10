d73efa2c60f0bbb4ff42c5b89443651dc46db36e
Sriharsha J <sriharshaj70@yahoo.co.in>
2020-12-10 08:35:12 -0800

    fixing an issue with historyd talking to apiserver

--- ws/build.sh
+++ ws/build.sh
@@ -2,7 +2,7 @@
 
 #ws docker registry push
 export CTR_REGISTRY=docker.dev.ws:5000
-export CTR_TAG=osmlatest19subm2
+export CTR_TAG=osmlatest19subm3
 make docker-push-osm-controller
 make docker-push-init
 
