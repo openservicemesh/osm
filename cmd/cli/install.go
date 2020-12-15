23f20b35697bdbb281238ff684ea2da2b6fc5e96
Sanya Kochhar <42152676+SanyaKochhar@users.noreply.github.com>
2020-12-15 14:49:59 -0500

    Update versions for release 0.6.0 (#2200)
    Signed-off-by: Sanya Kochhar <kochhars@microsoft.com>

--- cmd/cli/install.go
+++ cmd/cli/install.go
@@ -63,7 +63,7 @@ const (
 	defaultContainerRegistrySecret        = ""
 	defaultMeshName                       = "osm"
 	defaultOsmImagePullPolicy             = "IfNotPresent"
-	defaultOsmImageTag                    = "v0.5.0"
+	defaultOsmImageTag                    = "v0.6.0"
 	defaultPrometheusRetentionTime        = constants.PrometheusDefaultRetentionTime
 	defaultVaultHost                      = ""
 	defaultVaultProtocol                  = "http"
