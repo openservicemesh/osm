d22836911ca9cb0a198a07e5323aa71a5b453398
Michelle Noorali <minooral@microsoft.com>
2021-02-11 23:09:18 -0500

    ref(*): update scripts to handle multiple replicas
    Currently, the kubectl get pod lines in scripts don't work
    correctly if there are multiple replicas of a deployment running
    (ex. more than 1 bookbuyer pod). This change updates all the
    kubectl get lines to handle this scenario. The update in the script
    will now return the first pod that matches the selector conditions
    and is running instead of assuming there is only one pod that matches
    those conditions.
    
    Signed-off-by: Michelle Noorali <minooral@microsoft.com>

--- experimental/routes_refactor/demo/scripts/port-forward-bookbuyer-envoy.sh
+++ experimental/routes_refactor/demo/scripts/port-forward-bookbuyer-envoy.sh
@@ -4,6 +4,6 @@
 source .env
 
 BOOKBUYER_LOCAL_PORT="${BOOKBUYER_LOCAL_PORT:-8080}"
-POD="$(kubectl get pods --selector app=bookbuyer -n "$BOOKBUYER_NAMESPACE" --no-headers | grep 'Running' | awk '{print $1}')"
+POD="$(kubectl get pods --selector app=bookbuyer -n "$BOOKBUYER_NAMESPACE" --no-headers | grep 'Running' | awk 'NR==1{print $1}')"
 
 kubectl port-forward "$POD" -n "$BOOKBUYER_NAMESPACE" 15000:15000
--- scripts/port-forward-bookstore-ui-v1.sh
+++ scripts/port-forward-bookstore-ui-v1.sh
@@ -16,6 +16,6 @@ if [ -z "$backend" ]; then
 fi
 
 BOOKSTOREv1_LOCAL_PORT="${BOOKSTOREv1_LOCAL_PORT:-8081}"
-POD="$(kubectl get pods --selector app="$backend" -n "$BOOKSTORE_NAMESPACE" --no-headers | grep 'Running' | awk '{print $1}')"
+POD="$(kubectl get pods --selector app="$backend" -n "$BOOKSTORE_NAMESPACE" --no-headers | grep 'Running' | awk 'NR==1{print $1}')"
 
 kubectl port-forward "$POD" -n "$BOOKSTORE_NAMESPACE" "$BOOKSTOREv1_LOCAL_PORT":80
