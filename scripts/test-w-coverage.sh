559ddc0acfc3cc9c6809e914b9ad89efe81ce831
Delyan Raychev <delyan.raychev@microsoft.com>
2021-02-18 17:49:54 -0800

    code coverage: ignore generated code from unit test coverage metrics
    This change strips autogenerated mock code from coverage analysis.
    Adds a suffix _generated to files with generated code so that they
    can be identified and stripped.
    
    Signed-off-by: Delyan Raychev <delyan.raychev@microsoft.com>

--- scripts/test-w-coverage.sh
+++ scripts/test-w-coverage.sh
@@ -2,14 +2,24 @@
 
 set -aueo pipefail
 
-readarray -t modules < <(go list ./... | grep -v tests/framework | grep -v tests/e2e | grep -v tests/scenarios | grep -v tests/scale | \
-   grep -v ci/ | grep -v demo/ | grep -v experimental/ | grep -v scripts/)
+readarray -t modules < <(go list ./... | \
+                             grep -v tests/framework | \
+                             grep -v tests/e2e | \
+                             grep -v tests/scenarios | \
+                             grep -v tests/scale | \
+                             grep -v ci/ | \
+                             grep -v demo/ | \
+                             grep -v experimental/ | \
+                             grep -v scripts/)
 
 go test -timeout 120s \
    -failfast \
    -v \
-   -coverprofile=coverage.txt \
+   -coverprofile=coverage.txt.with_generated_code \
    -covermode count "${modules[@]}" | tee testoutput.txt || { echo "go test returned non-zero"; exit 1; }
+
+# shellcheck disable=SC2002
+cat coverage.txt.with_generated_code | grep -v "_generated.go" > coverage.txt
 
 # shellcheck disable=SC2002
 cat testoutput.txt | go run github.com/jstemmer/go-junit-report > report.xml
