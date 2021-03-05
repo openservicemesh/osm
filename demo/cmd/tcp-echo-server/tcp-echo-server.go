6ba57dc2ce5a12de0f17ed3f2ce96a5086d9e8a3
Eduard Serra <eduser25@gmail.com>
2021-03-05 13:11:42 -0800

    tcp-client/serv: move all logging to log

--- demo/cmd/tcp-echo-server/tcp-echo-server.go
+++ demo/cmd/tcp-echo-server/tcp-echo-server.go
@@ -14,12 +14,17 @@ import (
 
 var (
 	log            = logger.NewPretty("tcp-echo-server")
+	logLevel       = flag.String("logLevel", "debug", "Log output level")
 	port           = flag.Int("port", 9090, "port on which this app is serving TCP connections")
 	responsePrefix = "echo response:"
 )
 
 func main() {
 	flag.Parse()
+	err := logger.SetLogLevel(*logLevel)
+	if err != nil {
+		log.Fatal().Msgf("Unknown log level: %s", *logLevel)
+	}
 
 	listenAddr := fmt.Sprintf(":%d", *port)
 
@@ -53,21 +58,22 @@ func echoResponse(conn net.Conn) {
 		switch err {
 		case nil:
 			// respond to the request, prepend the prefix to the response
-			fmt.Printf("Received request: [%s]\n", requestMsg)
+			log.Info().Msgf("Received request: [%s]", requestMsg)
 			response := fmt.Sprintf("%s %s\n", responsePrefix, requestMsg)
 			if bytesWritten, writeErr := conn.Write([]byte(response)); err != nil {
 				log.Error().Err(writeErr).Msg("Write error")
 			} else {
 				log.Debug().Msgf("Wrote %d bytes", bytesWritten)
 			}
-			fmt.Printf("Response sent: [%s]\n", response)
+			log.Info().Msgf("Response sent: [%s]", response)
 
 		case io.EOF:
+			log.Debug().Msgf("EOF received")
 			return
 
 		default:
 			// unexpected error
-			fmt.Printf("Unexpected error: %s\n", err)
+			log.Error().Err(err).Msgf("Unexpected error")
 			return
 		}
 	}
