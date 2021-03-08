5af4619e5c249e6b807dc71aa8a5d70dd30d263e
Shashank Ram <shashr2204@gmail.com>
2021-03-07 16:03:38 -0800

    cli/proxy: extend proxy cmd to other admin queries
    Currently the cli proxy command set only implements
    capability to get the proxy config dump. This change
    extends this command to make it generic enough to
    retrieve other queries  such as clusters, stats,
    listeners, health etc.
    
    This is makes troubleshooting easier when debugging
    proxy related issues.
    
    Signed-off-by: Shashank Ram <shashr2204@gmail.com>

--- cmd/cli/proxy_get.go
+++ cmd/cli/proxy_get.go
@@ -19,63 +19,73 @@ import (
 	k8s "github.com/openservicemesh/osm/pkg/kubernetes"
 )
 
-const dumpConfigDescription = `
-This command will dump the sidecar proxy configuration for the given pod.
+const getCmdDescription = `
+This command will get the Envoy proxy configuration for the given query and pod.
+The query is forwarded as is to the Envoy proxy sidecar.
+Refer to https://www.envoyproxy.io/docs/envoy/latest/operations/admin for the
+list of supported GET queries.
 `
 
-const dumpConfigExample = `
-# Dump the proxy configuration for pod 'bookbuyer-5ccf77f46d-rc5mg' in the 'bookbuyer' namespace
-osm proxy dump-config bookbuyer-5ccf77f46d-rc5mg -n bookbuyer
+const getCmdExample = `
+# Get the proxy config dump for the given pod 'bookbuyer-5ccf77f46d-rc5mg' in the 'bookbuyer' namespace
+osm proxy get config_dump bookbuyer-5ccf77f46d-rc5mg -n bookbuyer
+
+# Get the cluster config for the given pod 'bookbuyer-5ccf77f46d-rc5mg' in the 'bookbuyer' namespace and output to file 'clusters.txt'
+osm proxy get clusters bookbuyer-5ccf77f46d-rc5mg -n bookbuyer -f clusters.txt
 `
 
-type proxyDumpConfigCmd struct {
+type proxyGetCmd struct {
 	out        io.Writer
 	config     *rest.Config
 	clientSet  kubernetes.Interface
+	query      string
 	namespace  string
 	pod        string
 	localPort  uint16
+	outFile    string
 	sigintChan chan os.Signal
 }
 
-func newProxyDumpConfig(config *action.Configuration, out io.Writer) *cobra.Command {
-	dumpConfigCmd := &proxyDumpConfigCmd{
+func newProxyGetCmd(config *action.Configuration, out io.Writer) *cobra.Command {
+	getCmd := &proxyGetCmd{
 		out:        out,
 		sigintChan: make(chan os.Signal, 1),
 	}
 
 	cmd := &cobra.Command{
-		Use:   "dump-config POD",
-		Short: "dump proxy config",
-		Long:  dumpConfigDescription,
-		Args:  cobra.ExactArgs(1),
+		Use:   "get QUERY POD",
+		Short: "get query for proxy",
+		Long:  getCmdDescription,
+		Args:  cobra.ExactArgs(2),
 		RunE: func(_ *cobra.Command, args []string) error {
-			dumpConfigCmd.pod = args[0]
+			getCmd.query = args[0]
+			getCmd.pod = args[1]
 			conf, err := config.RESTClientGetter.ToRESTConfig()
 			if err != nil {
 				return errors.Errorf("Error fetching kubeconfig: %s", err)
 			}
-			dumpConfigCmd.config = conf
+			getCmd.config = conf
 
 			clientset, err := kubernetes.NewForConfig(conf)
 			if err != nil {
 				return errors.Errorf("Could not access Kubernetes cluster, check kubeconfig: %s", err)
 			}
-			dumpConfigCmd.clientSet = clientset
-			return dumpConfigCmd.run()
+			getCmd.clientSet = clientset
+			return getCmd.run()
 		},
-		Example: dumpConfigExample,
+		Example: getCmdExample,
 	}
 
 	//add mesh name flag
 	f := cmd.Flags()
-	f.StringVarP(&dumpConfigCmd.namespace, "namespace", "n", metav1.NamespaceDefault, "Namespace of pod")
-	f.Uint16VarP(&dumpConfigCmd.localPort, "local-port", "p", constants.EnvoyAdminPort, "Local port to use for port forwarding")
+	f.StringVarP(&getCmd.namespace, "namespace", "n", metav1.NamespaceDefault, "Namespace of pod")
+	f.StringVarP(&getCmd.outFile, "file", "f", "", "File to write output to")
+	f.Uint16VarP(&getCmd.localPort, "local-port", "p", constants.EnvoyAdminPort, "Local port to use for port forwarding")
 
 	return cmd
 }
 
-func (cmd *proxyDumpConfigCmd) run() error {
+func (cmd *proxyGetCmd) run() error {
 	// Check if the pod belongs to a mesh
 	pod, err := cmd.clientSet.CoreV1().Pods(cmd.namespace).Get(context.TODO(), cmd.pod, metav1.GetOptions{})
 	if err != nil {
@@ -100,14 +110,25 @@ func (cmd *proxyDumpConfigCmd) run() error {
 
 	err = portForwarder.Start(func(pf *k8s.PortForwarder) error {
 		defer pf.Stop()
-		url := fmt.Sprintf("http://localhost:%d/config_dump", cmd.localPort)
+		url := fmt.Sprintf("http://localhost:%d/%s", cmd.localPort, cmd.query)
 
 		// #nosec G107: Potential HTTP request made with variable url
 		resp, err := http.Get(url)
 		if err != nil {
 			return errors.Errorf("Error fetching url %s: %s", url, err)
 		}
-		if _, err := io.Copy(cmd.out, resp.Body); err != nil {
+
+		out := cmd.out // By default, output is written to stdout
+		if cmd.outFile != "" {
+			fd, err := os.Create(cmd.outFile)
+			if err != nil {
+				return errors.Errorf("Error opening file %s: %s", cmd.outFile, err)
+			}
+			defer fd.Close() //nolint: errcheck, gosec
+			out = fd         // write output to file
+		}
+
+		if _, err := io.Copy(out, resp.Body); err != nil {
 			return errors.Errorf("Error rendering HTTP response: %s", err)
 		}
 		return nil
