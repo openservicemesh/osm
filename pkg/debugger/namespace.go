707593c3a291ec9ac4a3e92836d8776cb3d84ce3
Delyan Raychev <delyan.raychev@microsoft.com>
2021-04-09 14:36:59 -0700

    Removing ListSMIPolicies() and ListMonitoredNamespaces() from MeshCataloger interface (#3109)
    * Removing ListSMIPolicies() and ListMonitoredNamespaces() from MeshCataloger interface
    
    Signed-off-by: Delyan Raychev <delyan.raychev@microsoft.com>

--- pkg/debugger/namespace.go
+++ pkg/debugger/namespace.go
@@ -13,7 +13,11 @@ type namespaces struct {
 func (ds DebugConfig) getMonitoredNamespacesHandler() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		var n namespaces
-		n.Namespaces = ds.meshCatalogDebugger.ListMonitoredNamespaces()
+		var err error
+		n.Namespaces, err = ds.kubeController.ListMonitoredNamespaces()
+		if err != nil {
+			log.Error().Err(err).Msgf("Error marshalling policy %+v", n)
+		}
 
 		jsonPolicies, err := json.Marshal(n)
 		if err != nil {
