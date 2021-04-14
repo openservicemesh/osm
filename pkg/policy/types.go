e1c784593fdde251d7622f81af6bb0384ecb4c53
Shashank Ram <shashr2204@gmail.com>
2021-04-14 15:25:19 -0700

    policy/client: add k8s client for the policy API
    Adds the k8s client informers and caches for the
    Egress API in the policy.openservicemesh.io API group.
    
    A subsequent change will initialize the policy controller
    during osm-controller init.
    
    Part of #3045
    
    Signed-off-by: Shashank Ram <shashr2204@gmail.com>

---
+++ pkg/policy/types.go
@@ -0,0 +1,39 @@
+// Package policy implements the Kubernetes client for the resources in the policy.openservicemesh.io API group
+package policy
+
+import (
+	"k8s.io/client-go/tools/cache"
+
+	policyV1alpha1 "github.com/openservicemesh/osm/pkg/apis/policy/v1alpha1"
+	"github.com/openservicemesh/osm/pkg/identity"
+	"github.com/openservicemesh/osm/pkg/kubernetes"
+	"github.com/openservicemesh/osm/pkg/logger"
+)
+
+var (
+	log = logger.New("policy-controller")
+)
+
+// informerCollection is the type used to represent the collection of informers for the policy.openservicemesh.io API group
+type informerCollection struct {
+	egress cache.SharedIndexInformer
+}
+
+// cacheCollection is the type used to represent the collection of caches for the policy.openservicemesh.io API group
+type cacheCollection struct {
+	egress cache.Store
+}
+
+// client is the type used to represent the Kubernetes client for the policy.openservicemesh.io API group
+type client struct {
+	informers      *informerCollection
+	caches         *cacheCollection
+	cacheSynced    chan interface{}
+	kubeController kubernetes.Controller
+}
+
+// Controller is the interface for the functionality provided by the resources part of the policy.openservicemesh.io API group
+type Controller interface {
+	// ListEgressPoliciesForSourceIdentity lists the Egress policies for the given source identity
+	ListEgressPoliciesForSourceIdentity(identity.K8sServiceAccount) []*policyV1alpha1.Egress
+}
