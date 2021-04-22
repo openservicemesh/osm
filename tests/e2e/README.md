2e4403f3756f596570810b5dbe5b6206893091eb
Allen Leigh <allenlsy@gmail.com>
2021-04-22 11:37:38 -0700

    (feat) add initContainer to osm-controller
    The initContainer creates a MeshConfig resource in k8s if it does not
    exist
    
    Signed-off-by: Allen Leigh <allenlsy@gmail.com>

--- tests/e2e/README.md
+++ tests/e2e/README.md
@@ -40,6 +40,7 @@ Independent of tiers, tests are also organized into buckets. Each bucket runs in
 | e2e_trafficsplit_same_sa_test.go | 1 | 4
 | e2e_pod_client_server_test.go | 1 | 4
 | e2e_helm_install_test.go | 2 | 1
+| e2e_init_controller_test.go | 2 | 1
 | e2e_controller_restart_test.go | 2 | 1
 | e2e_k8s_version_test.go | 2 | 1
 | e2e_hashivault_test.go| 2 | 2
---
+++ tests/e2e/e2e_init_controller_test.go
@@ -0,0 +1,39 @@
+package e2e
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	. "github.com/openservicemesh/osm/tests/framework"
+)
+
+const meshConfigName = "osm-mesh-config"
+
+var _ = OSMDescribe("Test init-osm-controller functionalities",
+	OSMDescribeInfo{
+		Tier:   2,
+		Bucket: 1,
+	},
+	func() {
+		Context("When osm-controller starts in fresh environment", func() {
+			It("creates default MeshConfig resource", func() {
+				instOpts := Td.GetOSMInstallOpts()
+				namespace := instOpts.ControlPlaneNS
+
+				// Install OSM
+				Expect(Td.InstallOSM(instOpts)).To(Succeed())
+				meshConfig, err := Td.GetMeshConfig(meshConfigName, namespace)
+				Expect(err).ShouldNot(HaveOccurred())
+
+				// validate osm MeshConfig
+				Expect(meshConfig.Spec.Traffic.EnablePermissiveTrafficPolicyMode).Should(BeFalse())
+				Expect(meshConfig.Spec.Traffic.EnableEgress).Should(BeFalse())
+				Expect(meshConfig.Spec.Sidecar.LogLevel).Should(Equal("error"))
+				Expect(meshConfig.Spec.Observability.PrometheusScraping).Should(BeTrue())
+				Expect(meshConfig.Spec.Observability.EnableDebugServer).Should(BeFalse())
+				Expect(meshConfig.Spec.Observability.Tracing.Enable).Should(BeFalse())
+				Expect(meshConfig.Spec.Traffic.UseHTTPSIngress).Should(BeFalse())
+				Expect(meshConfig.Spec.Certificate.ServiceCertValidityDuration).Should(Equal("24h"))
+			})
+		})
+	})
