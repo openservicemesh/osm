9cc42bbfa60f4278058201287f014dd177039d87
Sneha Chhabria <snchh@microsoft.com>
2021-05-13 18:17:58 -0700

    MesnConfig : update comments for types
    Updates the comments for the API types of MeshConfig
    
    Part of #3040
    
    Signed-off-by: Sneha Chhabria <snchh@microsoft.com>

--- pkg/apis/config/v1alpha1/mesh_config.go
+++ pkg/apis/config/v1alpha1/mesh_config.go
@@ -5,66 +5,114 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// MeshConfig is the configuration for the service mesh overall
+// MeshConfig is the type used to represent the mesh configuration.
 // +genclient
 // +genclient:noStatus
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 type MeshConfig struct {
-	metav1.TypeMeta   `json:",inline" yaml:",inline"`
+	// Object's type metadata.
+	metav1.TypeMeta `json:",inline" yaml:",inline"`
+
+	// Object's metadata.
+	// +optional
 	metav1.ObjectMeta `json:"metadata,omitempty" yaml:"metadata,omitempty"`
 
+	// Spec is the MeshConfig specification.
+	// +optional
 	Spec MeshConfigSpec `json:"spec,omitempty" yaml:"spec,omitempty"`
 }
 
-// MeshConfigSpec is the spec for OSM's configuration
+// MeshConfigSpec is the spec for OSM's configuration.
 type MeshConfigSpec struct {
-	Sidecar       SidecarSpec       `json:"sidecar,omitempty"`
-	Traffic       TrafficSpec       `json:"traffic,omitempty"`
+	// Sidecar defines the configurations of the proxy sidecar in a mesh.
+	Sidecar SidecarSpec `json:"sidecar,omitempty"`
+
+	// Traffic defines the traffic management configurations for a mesh instance.
+	Traffic TrafficSpec `json:"traffic,omitempty"`
+
+	// Observalility defines the observability configurations for a mesh instance.
 	Observability ObservabilitySpec `json:"observability,omitempty"`
-	Certificate   CertificateSpec   `json:"certificate,omitempty"`
+
+	// Certificate defines the certificate management configurations for a mesh instance.
+	Certificate CertificateSpec `json:"certificate,omitempty"`
 }
 
-// SidecarSpec is the spec for OSM's sidecar configuration
+// SidecarSpec is the type used to represent the specifications for the proxy sidecar.
 type SidecarSpec struct {
-	EnablePrivilegedInitContainer bool                        `json:"enablePrivilegedInitContainer,omitempty"`
-	LogLevel                      string                      `json:"logLevel,omitempty"`
-	EnvoyImage                    string                      `json:"envoyImage,omitempty"`
-	InitContainerImage            string                      `json:"initContainerImage,omitempty"`
-	MaxDataPlaneConnections       int                         `json:"maxDataPlaneConnections,omitempty"`
-	ConfigResyncInterval          string                      `json:"configResyncInterval,omitempty"`
-	Resources                     corev1.ResourceRequirements `json:"resources,omitempty"`
+	// EnablePrivilegedInitContainer defines a boolean indicating whether the init container for a meshed pod should run as privileged.
+	EnablePrivilegedInitContainer bool `json:"enablePrivilegedInitContainer,omitempty"`
+
+	// LogLevel defines the  logging level for the sidecar's logs.
+	LogLevel string `json:"logLevel,omitempty"`
+
+	// EnvoyImage defines the container image used for the Envoy proxy sidecar.
+	EnvoyImage string `json:"envoyImage,omitempty"`
+
+	// InitContainerImage defines the container image used for the init container injected to meshed pods.
+	InitContainerImage string `json:"initContainerImage,omitempty"`
+
+	// MaxDataPlaneConnections defines the maximum allowed data plane connections from a proxy sidecar to the OSM controller.
+	MaxDataPlaneConnections int `json:"maxDataPlaneConnections,omitempty"`
+
+	// ConfigResyncInterval defines the resync interval for regular proxy broadcast updates.
+	ConfigResyncInterval string `json:"configResyncInterval,omitempty"`
+
+	// Resources defines the compute resources for the sidecar.
+	Resources corev1.ResourceRequirements `json:"resources,omitempty"`
 }
 
-// TrafficSpec is the spec for OSM's traffic management configuration
+// TrafficSpec is the type used to represent OSM's traffic management configuration.
 type TrafficSpec struct {
-	EnableEgress                      bool     `json:"enableEgress,omitempty"`
-	OutboundIPRangeExclusionList      []string `json:"outboundIPRangeExclusionList,omitempty"`
-	OutboundPortExclusionList         []int    `json:"outboundPortExclusionList,omitempty"`
-	UseHTTPSIngress                   bool     `json:"useHTTPSIngress,omitempty"`
-	EnablePermissiveTrafficPolicyMode bool     `json:"enablePermissiveTrafficPolicyMode,omitempty"`
+	// EnableEgress defines a boolean indicating if mesh-wide Egress is enabled.
+	EnableEgress bool `json:"enableEgress,omitempty"`
+
+	// OutboundIPRangeExclusionList defines a global list of IP address ranges to exclude from outbound traffic interception by the sidecar proxy.
+	OutboundIPRangeExclusionList []string `json:"outboundIPRangeExclusionList,omitempty"`
+
+	// OutboundPortExclusionList defines a global list of ports to exclude from outbound traffic interception by the sidecar proxy.
+	OutboundPortExclusionList []int `json:"outboundPortExclusionList,omitempty"`
+
+	// UseHTTPSIngress defines a boolean indicating if HTTPS Ingress is enabled globally in the mesh.
+	UseHTTPSIngress bool `json:"useHTTPSIngress,omitempty"`
+
+	// EnablePermissiveTrafficPolicyMode defines a boolean indicating if permissive traffic policy mode is enabled mesh-wide.
+	EnablePermissiveTrafficPolicyMode bool `json:"enablePermissiveTrafficPolicyMode,omitempty"`
 }
 
-// ObservabilitySpec is the spec for OSM's observability related configuration
+// ObservabilitySpec is the type to represent OSM's observability configurations.
 type ObservabilitySpec struct {
-	EnableDebugServer  bool        `json:"enableDebugServer,omitempty"`
-	PrometheusScraping bool        `json:"prometheusScraping,omitempty"`
-	Tracing            TracingSpec `json:"tracing,omitempty"`
+	// EnableDebugServer defines if the debug endpoint on the OSM controller pod is enabled.
+	EnableDebugServer bool `json:"enableDebugServer,omitempty"`
+
+	// PrometheusScraping defines a boolean indicating if sidecars should be configured for Prometheus metrics scraping.
+	PrometheusScraping bool `json:"prometheusScraping,omitempty"`
+
+	// Tracing defines OSM's tracing configuration.
+	Tracing TracingSpec `json:"tracing,omitempty"`
 }
 
-// TracingSpec is the spec for OSM's tracing configuration
+// TracingSpec is the type to represent OSM's tracing configuration.
 type TracingSpec struct {
-	Enable   bool   `json:"enable,omitempty"`
-	Port     int16  `json:"port,omitempty"`
-	Address  string `json:"address,omitempty"`
+	// Enable defines a boolean indicating if the sidecars are enabled for tracing.
+	Enable bool `json:"enable,omitempty"`
+
+	// Port defines the tracing collector's port.
+	Port int16 `json:"port,omitempty"`
+
+	// Address defines the tracing collectio's hostname.
+	Address string `json:"address,omitempty"`
+
+	// Endpoint defines the API endpoint for tracing requests sent to the collector.
 	Endpoint string `json:"endpoint,omitempty"`
 }
 
-// CertificateSpec is the spec for OSM's certificate management configuration
+// CertificateSpec is type to reperesent OSM's certificate management configuration.
 type CertificateSpec struct {
+	// ServiceCertValidityDuration defines the service certificate validity duration.
 	ServiceCertValidityDuration string `json:"serviceCertValidityDuration,omitempty"`
 }
 
-// MeshConfigList lists the MeshConfig objects
+// MeshConfigList lists the MeshConfig objects.
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 type MeshConfigList struct {
 	metav1.TypeMeta `json:",inline"`
