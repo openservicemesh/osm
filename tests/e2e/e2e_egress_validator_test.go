9aa21a9b50b9720b12c343f0bee1446020fb5d8a
Sean Teeling <seanteeling@microsoft.com>
2021-05-18 14:13:20 -0700

    create initial entry point for policy interaction in e2e tests

---
+++ tests/e2e/e2e_egress_validator_test.go
@@ -0,0 +1,13 @@
+package e2e
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/openservicemesh/osm/tests/framework"
+)
+
+var _ = OSMDescribe("Submit Egress Policy",
+	OSMDescribeInfo{},
+	func() {
+
+		It("Tests a valid Egress Policy", func() {})
+	})
