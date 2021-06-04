df499c0de80f3e6015e8408cdf74e9b45e5113c2
timmyreilly <tireilly@microsoft.com>
2021-06-04 11:02:02 -0700

    autogen and types for multiclusterservice
    Signed-off-by: timmyreilly <tireilly@microsoft.com>
    
    adding comments for spec
    
    Signed-off-by: timmyreilly <tireilly@microsoft.com>
    
    adding comments to spec
    
    Signed-off-by: timmyreilly <tireilly@microsoft.com>
    
    updating comment
    
    Signed-off-by: timmyreilly <tireilly@microsoft.com>
    
    details for multicluster naming
    
    fixing comments and removing certificate
    
    Signed-off-by: timmyreilly <tireilly@microsoft.com>
    
    running codegen with no certificate field
    
    Signed-off-by: timmyreilly <tireilly@microsoft.com>
    
    another comment update

--- pkg/gen/client/config/clientset/versioned/typed/config/v1alpha1/fake/fake_config_client.go
+++ pkg/gen/client/config/clientset/versioned/typed/config/v1alpha1/fake/fake_config_client.go
@@ -29,6 +29,10 @@ func (c *FakeConfigV1alpha1) MeshConfigs(namespace string) v1alpha1.MeshConfigIn
 	return &FakeMeshConfigs{c, namespace}
 }
 
+func (c *FakeConfigV1alpha1) MultiClusterServices(namespace string) v1alpha1.MultiClusterServiceInterface {
+	return &FakeMultiClusterServices{c, namespace}
+}
+
 // RESTClient returns a RESTClient that is used to communicate
 // with API server by this client implementation.
 func (c *FakeConfigV1alpha1) RESTClient() rest.Interface {
