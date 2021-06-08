61704780400e1fdc3db25c778105e891f71c18a1
Sean Teeling <seanteeling@microsoft.com>
2021-06-08 15:11:03 -0700

    feat(pkg/catalog):  gateway is configured for SSL passthrough
    Prep multi cluster gateway, by adding logic to the MeshCatalog
    for the gateway
    
    Signed-off-by: Sean Teeling <seanteeling@microsoft.com>
    
    Co-authored-by: Sneha Chhabria <snchh@microsoft.com>

---
+++ pkg/catalog/gateway.go
@@ -0,0 +1,14 @@
+package catalog
+
+import (
+	"github.com/openservicemesh/osm/pkg/envoy"
+	"github.com/openservicemesh/osm/pkg/identity"
+)
+
+// isOSMGateway checks if the ServiceIdentity belongs to the MultiClusterGateway.
+// Only used if MultiClusterMode is enabled.
+func (mc *MeshCatalog) isOSMGateway(svcIdentity identity.ServiceIdentity) bool {
+	sa := svcIdentity.ToK8sServiceAccount()
+	return mc.configurator.GetFeatureFlags().EnableMulticlusterMode &&
+		envoy.ProxyKind(sa.Name) == envoy.KindGateway && sa.Namespace == mc.configurator.GetOSMNamespace()
+}
