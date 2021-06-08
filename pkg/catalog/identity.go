9d84626cdeb4fe55194baf4931480098f21b6085
Sean Teeling <seanteeling@microsoft.com>
2021-06-08 15:11:03 -0700

    feat(pkg/catalog):  gateway is configured for permissive mode.
    Signed-off-by: Sean Teeling <seanteeling@microsoft.com>

---
+++ pkg/catalog/identity.go
@@ -0,0 +1,15 @@
+package catalog
+
+import (
+	"github.com/openservicemesh/osm/pkg/envoy"
+	"github.com/openservicemesh/osm/pkg/featureflags"
+	"github.com/openservicemesh/osm/pkg/identity"
+)
+
+// IsMultiClusterGateway checks if the ServiceIdentity belongs to the MultiClusterGateway.
+// Only used if MultiClusterMode is enabled.
+func (mc *MeshCatalog) IsMultiClusterGateway(svcID identity.ServiceIdentity) bool {
+	sa := svcID.ToK8sServiceAccount()
+	return featureflags.IsMulticlusterModeEnabled() &&
+		envoy.ProxyKind(sa.Name) == envoy.KindGateway && sa.Namespace == mc.configurator.GetOSMNamespace()
+}
