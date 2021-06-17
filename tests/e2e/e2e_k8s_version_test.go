3e34b61bc467e0163777ae59c6974780c90fa783
Shashank Ram <shashr2204@gmail.com>
2021-06-17 16:32:44 -0700

    Deprecate support for k8s 1.18
    Deprecates support for k8s 1.18 which is no longer
    supported upstream.
    
    Signed-off-by: Shashank Ram <shashr2204@gmail.com>

--- tests/e2e/e2e_k8s_version_test.go
+++ tests/e2e/e2e_k8s_version_test.go
@@ -21,9 +21,6 @@ var _ = OSMDescribe("Test HTTP traffic for different k8s versions",
 		Context("Version v1.19.7", func() {
 			testK8sVersion("v1.19.7")
 		})
-		Context("Version v1.18.0", func() {
-			testK8sVersion("v1.18.0")
-		})
 	})
 
 func testK8sVersion(version string) {
