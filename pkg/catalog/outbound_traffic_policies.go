a4c20d8e0636c77855e31bcb98eec6b4341c9624
Shashank Ram <shashr2204@gmail.com>
2021-06-23 11:28:10 -0700

    catalog/route: fix missing hostnames for permissive mode
    In permissive mode, when client belongs to the same namespace
    as the upstream service, the shorthand FQDN <service>,
    <service>:<port> should be configured as domains in the
    virtual_host for the corresponding service. This change
    addresses this bug specific to permissive mode.
    
    Signed-off-by: Shashank Ram <shashr2204@gmail.com>

--- pkg/catalog/outbound_traffic_policies.go
+++ pkg/catalog/outbound_traffic_policies.go
@@ -20,7 +20,7 @@ func (mc *MeshCatalog) ListOutboundTrafficPolicies(downstreamIdentity identity.S
 	downstreamServiceAccount := downstreamIdentity.ToK8sServiceAccount()
 	if mc.configurator.IsPermissiveTrafficPolicyMode() {
 		var outboundPolicies []*trafficpolicy.OutboundTrafficPolicy
-		mergedPolicies := trafficpolicy.MergeOutboundPolicies(DisallowPartialHostnamesMatch, outboundPolicies, mc.buildOutboundPermissiveModePolicies()...)
+		mergedPolicies := trafficpolicy.MergeOutboundPolicies(DisallowPartialHostnamesMatch, outboundPolicies, mc.buildOutboundPermissiveModePolicies(downstreamServiceAccount.Namespace)...)
 		outboundPolicies = mergedPolicies
 		return outboundPolicies
 	}
@@ -145,7 +145,7 @@ func (mc *MeshCatalog) ListAllowedOutboundServicesForIdentity(serviceIdentity id
 	return allowedServices
 }
 
-func (mc *MeshCatalog) buildOutboundPermissiveModePolicies() []*trafficpolicy.OutboundTrafficPolicy {
+func (mc *MeshCatalog) buildOutboundPermissiveModePolicies(sourceNamespace string) []*trafficpolicy.OutboundTrafficPolicy {
 	var outPolicies []*trafficpolicy.OutboundTrafficPolicy
 
 	k8sServices := mc.kubeController.ListServices()
@@ -155,8 +155,11 @@ func (mc *MeshCatalog) buildOutboundPermissiveModePolicies() []*trafficpolicy.Ou
 	}
 
 	for _, destService := range destServices {
-		// TODO(steeling): shouldn't this check the source namespace.... not relevant to this PR though.
-		hostnames, err := mc.GetServiceHostnames(destService, service.LocalCluster)
+		locality := service.LocalCluster
+		if destService.Namespace == sourceNamespace {
+			locality = service.LocalNS
+		}
+		hostnames, err := mc.GetServiceHostnames(destService, locality)
 		if err != nil {
 			log.Error().Err(err).Msgf("Error getting service hostnames for service %s", destService)
 			continue
