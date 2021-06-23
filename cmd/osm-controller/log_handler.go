d6f0e67e11e29bc54ca1461e0054343a86a7513e
Eduard Serra <eduser25@gmail.com>
2021-06-23 12:05:14 -0700

    logger: making log level configurable through meshconfig
    (Backport release-v0.9)
    
    New `osmLogLevel` option has been added under observability meshconfig
    spec (seems the most related and relevant to the available categories)
    to allow changing the log level of OSM at runtime.
    
    The initial log level flag has been kept at it still mandates boot log
    level up to the point configurator and the log level handler are both
    up, which then meshconfig value takes over.
    
    The handler can't be easilly moved to the `logger` package as it
    introduces a hard dependency cycle between logger<->configurator, hence
    the handler has been left on osm-controller main.
    
    Adds a unit test, and see below a working example on a running deployment.
    
    Fixes #3646
    
    Signed-off-by: Eduard Serra <eduser25@gmail.com>

---
+++ cmd/osm-controller/log_handler.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+
+	"github.com/openservicemesh/osm/pkg/announcements"
+	"github.com/openservicemesh/osm/pkg/configurator"
+	"github.com/openservicemesh/osm/pkg/constants"
+	"github.com/openservicemesh/osm/pkg/kubernetes/events"
+	"github.com/openservicemesh/osm/pkg/logger"
+)
+
+// StartGlobalLogLevelHandler registers a listener to meshconfig events and log level changes,
+// and applies new log level at global scope
+func StartGlobalLogLevelHandler(cfg configurator.Configurator, stop <-chan struct{}) {
+	meshConfigChannel := events.GetPubSubInstance().Subscribe(
+		announcements.MeshConfigAdded,
+		announcements.MeshConfigDeleted,
+		announcements.MeshConfigUpdated)
+
+	// Run config listener
+	// Bootstrap after subscribing
+	currentLogLevel := constants.DefaultOSMLogLevel
+	logLevel := cfg.GetOSMLogLevel()
+	log.Info().Msgf("Setting initial log level from meshconfig: %s", logLevel)
+	err := logger.SetLogLevel(logLevel)
+	if err != nil {
+		log.Error().Msgf("Error setting initial log level from meshconfig: %v", err)
+	} else {
+		currentLogLevel = logLevel
+	}
+
+	go func() {
+		for {
+			select {
+			case <-meshConfigChannel:
+				logLevel := cfg.GetOSMLogLevel()
+				if logLevel != currentLogLevel {
+					err := logger.SetLogLevel(logLevel)
+					if err != nil {
+						log.Error().Msgf("Error setting log level from meshconfig: %v", err)
+					} else {
+						log.Info().Msgf("Global log level changed to: %s", logLevel)
+						currentLogLevel = logLevel
+					}
+				}
+			case <-stop:
+				return
+			}
+		}
+	}()
+}
