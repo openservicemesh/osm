7ef41172206cbb073c22ec7be30d6fa511e009ce
Shashank Ram <shashr2204@gmail.com>
2021-06-24 14:30:43 -0700

    envoy/identity: cleanup SDS refs in identity pkg
    Removes SDS implementation leakage in the identity
    pkg.
    
    Resolves #3182
    
    Signed-off-by: Shashank Ram <shashr2204@gmail.com>

--- pkg/identity/types_test.go
+++ pkg/identity/types_test.go
@@ -29,13 +29,6 @@ var _ = Describe("Test pkg/service functions", func() {
 			Expect(K8sServiceAccount{}.IsEmpty()).To(BeTrue())
 		})
 
-		It("implements ServiceIdentity{}.GetSDSSecretName() correctly", func() {
-			serviceIdentity := ServiceIdentity("one.two.three.four.five")
-			actual := serviceIdentity.GetSDSCSecretName()
-			expected := "two/one"
-			Expect(actual).To(Equal(expected))
-		})
-
 		It("implements K8sServiceAccount{}.ToServiceIdentity() correctly", func() {
 			actual := K8sServiceAccount{
 				Namespace: "ns",
