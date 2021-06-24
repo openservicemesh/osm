openservicemesh/osm

commit db538a1f85130c27505cae9c3a201947990766ac

feat(pkg/*) Add local cluster domain to mesh service.
This also requires adding it to the envoy cluster

Co-authored-by: brunoterkaly <bterkaly@microsoft.com>

Signed-off-by: Sean Teeling <seanteeling@microsoft.com>

pkg/envoy/eds/response.go
@@ -10,6 +10,7 @@ import (
 	"github.com/openservicemesh/osm/pkg/catalog"
 	"github.com/openservicemesh/osm/pkg/certificate"
 	"github.com/openservicemesh/osm/pkg/configurator"
+	"github.com/openservicemesh/osm/pkg/constants"
 	"github.com/openservicemesh/osm/pkg/endpoint"
 	"github.com/openservicemesh/osm/pkg/envoy"
 	"github.com/openservicemesh/osm/pkg/envoy/registry"
@@ -91,7 +92,11 @@ func clusterToMeshSvc(cluster string) (service.MeshService, error) {
 	if len(chunks) != 2 {
 		return service.MeshService{}, errors.Errorf("Invalid cluster name. Expected: <namespace>/<name>, Got: %s", cluster)
 	}
-	return service.MeshService{Namespace: chunks[0], Name: chunks[1]}, nil
+	return service.MeshService{
+		Namespace:     chunks[0],
+		Name:          chunks[1],
+		ClusterDomain: constants.LocalDomain,
+	}, nil
 }
 
 // getEndpointsForProxy returns only those service endpoints that belong to the allowed outbound service accounts for the proxy

pkg/envoy/eds/response_test.go
@@ -96,10 +96,14 @@ func TestClusterToMeshSvc(t *testing.T) {
 		expectError     bool
 	}{
 		{
-			name:            "valid cluster name",
-			cluster:         "foo/bar",
-			expectedMeshSvc: service.MeshService{Namespace: "foo", Name: "bar"},
-			expectError:     false,
+			name:    "valid cluster name",
+			cluster: "foo/bar",
+			expectedMeshSvc: service.MeshService{
+				Namespace:     "foo",
+				Name:          "bar",
+				ClusterDomain: constants.LocalDomain,
+			},
+			expectError: false,
 		},
 		{
 			name:            "invalid cluster name",

pkg/envoy/ads/response_test.go
@@ -53,7 +53,11 @@ var _ = Describe("Test ADS response functions", func() {
 
 	namespace := tests.Namespace
 	proxyUUID := tests.ProxyUUID
-	proxyService := service.MeshService{Name: tests.BookstoreV1ServiceName, Namespace: namespace}
+	proxyService := service.MeshService{
+		Name:          tests.BookstoreV1ServiceName,
+		Namespace:     namespace,
+		ClusterDomain: constants.LocalDomain,
+	}
 	proxySvcAccount := tests.BookstoreServiceAccount
 
 	labels := map[string]string{constants.EnvoyUniqueIDLabelName: tests.ProxyUUID}

pkg/envoy/cds/response_test.go
@@ -110,8 +110,8 @@ func TestNewResponse(t *testing.T) {
 
 	expectedLocalCluster := &xds_cluster.Cluster{
 		TransportSocketMatches:        nil,
-		Name:                          "default/bookbuyer-local",
-		AltStatName:                   "default/bookbuyer-local",
+		Name:                          "default/bookbuyer/local-local",
+		AltStatName:                   "default/bookbuyer/local-local",
 		ClusterDiscoveryType:          &xds_cluster.Cluster_Type{Type: xds_cluster.Cluster_STRICT_DNS},
 		EdsClusterConfig:              nil,
 		ConnectTimeout:                ptypes.DurationProto(1 * time.Second),
@@ -119,7 +119,7 @@ func TestNewResponse(t *testing.T) {
 		DnsLookupFamily:               xds_cluster.Cluster_V4_ONLY,
 		TypedExtensionProtocolOptions: HTTP2ProtocolOptions,
 		LoadAssignment: &xds_endpoint.ClusterLoadAssignment{
-			ClusterName: "default/bookbuyer-local",
+			ClusterName: "default/bookbuyer/local-local",
 			Endpoints: []*xds_endpoint.LocalityLbEndpoints{
 				{
 					Locality: &xds_core.Locality{
@@ -155,7 +155,7 @@ func TestNewResponse(t *testing.T) {
 
 	expectedBookstoreV1Cluster := &xds_cluster.Cluster{
 		TransportSocketMatches:        nil,
-		Name:                          "default/bookstore-v1",
+		Name:                          "default/bookstore-v1/local",
 		AltStatName:                   "",
 		ClusterDiscoveryType:          &xds_cluster.Cluster_Type{Type: xds_cluster.Cluster_EDS},
 		TypedExtensionProtocolOptions: HTTP2ProtocolOptions,
@@ -184,7 +184,7 @@ func TestNewResponse(t *testing.T) {
 	require.Nil(err)
 	expectedBookstoreV2Cluster := &xds_cluster.Cluster{
 		TransportSocketMatches:        nil,
-		Name:                          "default/bookstore-v2",
+		Name:                          "default/bookstore-v2/local",
 		AltStatName:                   "",
 		ClusterDiscoveryType:          &xds_cluster.Cluster_Type{Type: xds_cluster.Cluster_EDS},
 		TypedExtensionProtocolOptions: HTTP2ProtocolOptions,
@@ -217,7 +217,7 @@ func TestNewResponse(t *testing.T) {
 			},
 			TlsCertificates: nil,
 			TlsCertificateSdsSecretConfigs: []*xds_auth.SdsSecretConfig{{
-				Name: "service-cert:default/bookstore-v1",
+				Name: "service-cert:default/bookstore-v1/local",
 				SdsConfig: &xds_core.ConfigSource{
 					ConfigSourceSpecifier: &xds_core.ConfigSource_Ads{
 						Ads: &xds_core.AggregatedConfigSource{},
@@ -248,7 +248,7 @@ func TestNewResponse(t *testing.T) {
 			},
 			TlsCertificates: nil,
 			TlsCertificateSdsSecretConfigs: []*xds_auth.SdsSecretConfig{{
-				Name: "service-cert:default/bookstore-v2",
+				Name: "service-cert:default/bookstore-v2/local",
 				SdsConfig: &xds_core.ConfigSource{
 					ConfigSourceSpecifier: &xds_core.ConfigSource_Ads{
 						Ads: &xds_core.AggregatedConfigSource{},
@@ -310,8 +310,8 @@ func TestNewResponse(t *testing.T) {
 
 	expectedClusters := []string{
 		"default/bookstore-v1",
-		"default/bookstore-v2",
-		"default/bookbuyer-local",
+		"default/bookstore-v2/local",
+		"default/bookbuyer/local-local",
 		"passthrough-outbound",
 		"envoy-metrics-cluster",
 		"envoy-tracing-cluster",
@@ -321,12 +321,12 @@ func TestNewResponse(t *testing.T) {
 
 	for _, a := range actualClusters {
 		fmt.Println(a.Name)
-		if a.Name == "default/bookbuyer-local" {
+		if a.Name == "default/bookbuyer/local-local" {
 			assert.Truef(cmp.Equal(expectedLocalCluster, a, protocmp.Transform()), cmp.Diff(expectedLocalCluster, a, protocmp.Transform()))
-			foundClusters = append(foundClusters, "default/bookbuyer-local")
+			foundClusters = append(foundClusters, "default/bookbuyer/local-local")
 			continue
 		}
-		if a.Name == "default/bookstore-v1" {
+		if a.Name == "default/bookstore-v1/local" {
 			assert.Truef(cmp.Equal(expectedBookstoreV1Cluster, a, protocmp.Transform()), cmp.Diff(expectedBookstoreV1Cluster, a, protocmp.Transform()))
 
 			upstreamTLSContext := xds_auth.UpstreamTlsContext{}
@@ -341,7 +341,7 @@ func TestNewResponse(t *testing.T) {
 			continue
 		}
 
-		if a.Name == "default/bookstore-v2" {
+		if a.Name == "default/bookstore-v2/local" {
 			assert.Truef(cmp.Equal(expectedBookstoreV2Cluster, a, protocmp.Transform()), cmp.Diff(expectedBookstoreV1Cluster, a, protocmp.Transform()))
 
 			upstreamTLSContext := xds_auth.UpstreamTlsContext{}
@@ -352,7 +352,7 @@ func TestNewResponse(t *testing.T) {
 			assert.Equal("bookstore-v2.default.svc.cluster.local", upstreamTLSContext.Sni)
 			assert.Nil(a.LoadAssignment) //ClusterLoadAssignment setting for non EDS clusters
 
-			foundClusters = append(foundClusters, "default/bookstore-v2")
+			foundClusters = append(foundClusters, "default/bookstore-v2/local")
 			continue
 		}
 
@@ -373,7 +373,6 @@ func TestNewResponse(t *testing.T) {
 			continue
 		}
 	}
-
 	assert.ElementsMatch(expectedClusters, foundClusters)
 }
 
@@ -403,7 +402,12 @@ func TestNewResponseGetLocalServiceClusterError(t *testing.T) {
 	cfg := configurator.NewMockConfigurator(ctrl)
 	meshCatalog := catalog.NewMockMeshCataloger(ctrl)
 
-	svc := service.MeshService{Namespace: "ns", Name: "svc"}
+	svc := service.MeshService{
+		Namespace:     "ns",
+		Name:          "svc",
+		ClusterDomain: constants.LocalDomain,
+	}
+
 	proxyIdentity := identity.K8sServiceAccount{Name: "svcacc", Namespace: "ns"}.ToServiceIdentity()
 	proxyRegistry := registry.NewProxyRegistry(registry.ExplicitProxyServiceMapper(func(*envoy.Proxy) ([]service.MeshService, error) {
 		return []service.MeshService{svc}, nil
@@ -506,8 +510,8 @@ func TestNewResponseForGateway(t *testing.T) {
 	resp, err := NewResponse(meshCatalog, proxy, nil, cfg, nil, proxyRegistry)
 	tassert.NoError(t, err)
 	tassert.Len(t, resp, 2)
-	tassert.Equal(t, "default/bookbuyer", resp[0].(*xds_cluster.Cluster).Name)
-	tassert.Equal(t, "default/bookwarehouse", resp[1].(*xds_cluster.Cluster).Name)
+	tassert.Equal(t, "default/bookbuyer/local", resp[0].(*xds_cluster.Cluster).Name)
+	tassert.Equal(t, "default/bookwarehouse/local", resp[1].(*xds_cluster.Cluster).Name)
 }
 
 func TestRemoveDups(t *testing.T) {

pkg/envoy/rds/response_test.go
@@ -160,7 +160,7 @@ func TestNewResponse(t *testing.T) {
 							Route: trafficpolicy.RouteWeightedClusters{
 								HTTPRouteMatch: tests.BookstoreBuyHTTPRoute,
 								WeightedClusters: mapset.NewSet(service.WeightedCluster{
-									ClusterName: "default/bookstore-v1-local",
+									ClusterName: "default/bookstore-v1/local-local",
 									Weight:      100,
 								}),
 							},
@@ -173,7 +173,7 @@ func TestNewResponse(t *testing.T) {
 							Route: trafficpolicy.RouteWeightedClusters{
 								HTTPRouteMatch: tests.BookstoreSellHTTPRoute,
 								WeightedClusters: mapset.NewSet(service.WeightedCluster{
-									ClusterName: "default/bookstore-v1-local",
+									ClusterName: "default/bookstore-v1/local-local",
 									Weight:      100,
 								}),
 							},
@@ -203,7 +203,7 @@ func TestNewResponse(t *testing.T) {
 							Route: trafficpolicy.RouteWeightedClusters{
 								HTTPRouteMatch: tests.BookstoreBuyHTTPRoute,
 								WeightedClusters: mapset.NewSet(service.WeightedCluster{
-									ClusterName: "default/bookstore-v1-local",
+									ClusterName: "default/bookstore-v1/local-local",
 									Weight:      100,
 								}),
 							},
@@ -216,7 +216,7 @@ func TestNewResponse(t *testing.T) {
 							Route: trafficpolicy.RouteWeightedClusters{
 								HTTPRouteMatch: tests.BookstoreSellHTTPRoute,
 								WeightedClusters: mapset.NewSet(service.WeightedCluster{
-									ClusterName: "default/bookstore-v1-local",
+									ClusterName: "default/bookstore-v1/local-local",
 									Weight:      100,
 								}),
 							},

pkg/envoy/sds/response_test.go
@@ -12,6 +12,7 @@ import (
 	tassert "github.com/stretchr/testify/assert"
 	testclient "k8s.io/client-go/kubernetes/fake"
 
+	"github.com/openservicemesh/osm/pkg/constants"
 	"github.com/openservicemesh/osm/pkg/envoy/secrets"
 	configFake "github.com/openservicemesh/osm/pkg/gen/client/config/clientset/versioned/fake"
 
@@ -144,7 +145,11 @@ func TestGetRootCert(t *testing.T) {
 					identity.K8sServiceAccount{Name: "sa-2", Namespace: "ns-2"}.ToServiceIdentity(),
 					identity.K8sServiceAccount{Name: "sa-3", Namespace: "ns-2"}.ToServiceIdentity(),
 				}
-				d.mockCatalog.EXPECT().ListServiceIdentitiesForService(service.MeshService{Name: "service-2", Namespace: "ns-2"}).Return(associatedSvcAccounts, nil).Times(1)
+				d.mockCatalog.EXPECT().ListServiceIdentitiesForService(service.MeshService{
+					Name:          "service-2",
+					Namespace:     "ns-2",
+					ClusterDomain: constants.LocalDomain,
+				}).Return(associatedSvcAccounts, nil).Times(1)
 				d.mockCertificater.EXPECT().GetIssuingCA().Return([]byte("foo")).Times(1)
 			},
 
@@ -339,7 +344,11 @@ func TestGetSDSSecrets(t *testing.T) {
 					identity.K8sServiceAccount{Name: "sa-2", Namespace: "ns-2"}.ToServiceIdentity(),
 					identity.K8sServiceAccount{Name: "sa-3", Namespace: "ns-2"}.ToServiceIdentity(),
 				}
-				svc := service.MeshService{Name: "service-2", Namespace: "ns-2"}
+				svc := service.MeshService{
+					Name:          "service-2",
+					Namespace:     "ns-2",
+					ClusterDomain: constants.LocalDomain,
+				}
 				d.mockCatalog.EXPECT().ListServiceIdentitiesForService(svc).Return(associatedSvcAccounts, nil).Times(1)
 				d.mockCertificater.EXPECT().GetIssuingCA().Return([]byte("foo")).Times(1)
 			},

commit 38ad742d61fd9e297eb5315c71b06936e7e434ad

envoy/eds: fix cluster name to mesh service parsing
Change db538a1f8 introduced a regression where the cluster
name is not correctly parsed to retrieve the mesh service.

This causes EDS to error and endpoints to not be
programmed.

Signed-off-by: Shashank Ram <shashr2204@gmail.com>

pkg/envoy/eds/response.go
@@ -89,8 +89,8 @@ func generateEDSConfig(meshCatalog catalog.MeshCataloger, proxy *envoy.Proxy) ([
 
 func clusterToMeshSvc(cluster string) (service.MeshService, error) {
 	chunks := strings.Split(cluster, namespacedNameDelimiter)
-	if len(chunks) != 2 {
-		return service.MeshService{}, errors.Errorf("Invalid cluster name. Expected: <namespace>/<name>, Got: %s", cluster)
+	if len(chunks) != 3 {
+		return service.MeshService{}, errors.Errorf("Invalid cluster name. Expected: <namespace>/<name>/<cluster>, Got: %s", cluster)
 	}
 	return service.MeshService{
 		Namespace:     chunks[0],

pkg/envoy/eds/response_test.go
@@ -67,7 +67,7 @@ func TestEndpointConfiguration(t *testing.T) {
 	assert.NotNil(proxy)
 
 	request := &xds_discovery.DiscoveryRequest{
-		ResourceNames: []string{"default/bookstore-v1"},
+		ResourceNames: []string{"default/bookstore-v1/local"},
 	}
 	resources, err := NewResponse(meshCatalog, proxy, request, mockConfigurator, nil, nil)
 	assert.Nil(err)
@@ -97,7 +97,7 @@ func TestClusterToMeshSvc(t *testing.T) {
 	}{
 		{
 			name:    "valid cluster name",
-			cluster: "foo/bar",
+			cluster: "foo/bar/local",
 			expectedMeshSvc: service.MeshService{
 				Namespace:     "foo",
 				Name:          "bar",
@@ -113,7 +113,7 @@ func TestClusterToMeshSvc(t *testing.T) {
 		},
 		{
 			name:            "invalid cluster name",
-			cluster:         "foo/bar/baz",
+			cluster:         "foo/barbaz",
 			expectedMeshSvc: service.MeshService{},
 			expectError:     true,
 		},