f12b8f71952f8b72c41452a72fd497c2a55a0cb1
Sneha Chhabria <snchh@microsoft.com>
2021-06-24 17:52:07 -0700

    ref(preset-mesh-config): Change kind of preset-mesh-config to ConfigMap
    This commit changes the kind of preset-mesh-config from MeshConfig to
    ConfigMap
    
    Fixes #3654
    
    Signed-off-by: Sneha Chhabria <snchh@microsoft.com>
    (cherry picked from commit 2429d81ddc40539d834a7afa625656a1ed0d1f26)
    
    # Conflicts:
    #	charts/osm/templates/preset-mesh-config.yaml
    #	cmd/init-osm-controller/init-osm-controller_test.go

--- cmd/init-osm-controller/init-osm-controller_test.go
+++ cmd/init-osm-controller/init-osm-controller_test.go
@@ -4,22 +4,22 @@ import (
 	"testing"
 
 	tassert "github.com/stretchr/testify/assert"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
-	"github.com/openservicemesh/osm/pkg/apis/config/v1alpha1"
 )
 
 func TestCreateDefaultMeshConfig(t *testing.T) {
 	assert := tassert.New(t)
 
-	presetMeshConfig := &v1alpha1.MeshConfig{
+	presetMeshConfigMap := &corev1.ConfigMap{
 		TypeMeta: metav1.TypeMeta{
-			Kind:       "MeshConfig",
-			APIVersion: "config.openservicemesh.io/v1alpha1",
+			Kind:       "ConfigMap",
+			APIVersion: "v1",
 		},
 		ObjectMeta: metav1.ObjectMeta{
 			Name: presetMeshConfigName,
 		},
+<<<<<<< HEAD
 		Spec: v1alpha1.MeshConfigSpec{
 			Sidecar: v1alpha1.SidecarSpec{
 				LogLevel:                      "error",
@@ -43,10 +43,49 @@ func TestCreateDefaultMeshConfig(t *testing.T) {
 			Certificate: v1alpha1.CertificateSpec{
 				ServiceCertValidityDuration: "24h",
 			},
+=======
+		Data: map[string]string{
+			presetMeshConfigJSONKey: `{
+"sidecar": {
+  "enablePrivilegedInitContainer": false,
+  "logLevel": "error",
+  "maxDataPlaneConnections": 0,
+  "envoyImage": "envoyproxy/envoy-alpine:v1.18.3",
+  "initContainerImage": "openservicemesh/init:v0.9.0",
+  "configResyncInterval": "2s"
+},
+"traffic": {
+	"enableEgress": true,
+	"useHTTPSIngress": false,
+	"enablePermissiveTrafficPolicyMode": true,
+	"outboundPortExclusionList": [],
+	"inboundPortExclusionList": [],
+	"outboundIPRangeExclusionList": []
+  },
+  "observability": {
+	"enableDebugServer": false,
+	"osmLogLevel": "trace",
+	"tracing": {
+   "  enable": false
+	}
+  },
+  "certificate": {
+	"serviceCertValidityDuration": "24h"
+  },
+  "featureFlags": {
+	"enableWASMStats": true,
+	"enableEgressPolicy": true,
+	"enableMulticlusterMode": false,
+	"enableOSMGateway": false,
+	"enableAsyncProxyServiceMapping": false,
+	"enableValidatingWebhook": false
+	}
+}`,
+>>>>>>> 2429d81d (ref(preset-mesh-config): Change kind of preset-mesh-config to ConfigMap)
 		},
 	}
 
-	meshConfig := createDefaultMeshConfig(presetMeshConfig)
+	meshConfig := createDefaultMeshConfig(presetMeshConfigMap)
 	assert.Equal(meshConfig.Name, meshConfigName)
 	assert.Equal(meshConfig.Spec.Sidecar.LogLevel, "error")
 	assert.Equal(meshConfig.Spec.Sidecar.ConfigResyncInterval, "2s")
