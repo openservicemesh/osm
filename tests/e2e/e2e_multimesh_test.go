b83fbf8f93eb70c30608a90452e09917febab4de
Michael Tarng <20913254+mtarng@users.noreply.github.com>
2021-06-30 01:22:39 -0700

    First draft of e2e test with many TODOs to follow up on. Namely, figuring out what Multicluster CRD needs to be added and what SMI policies
    Signed-off-by: Michael Tarng <20913254+mtarng@users.noreply.github.com>

---
+++ tests/e2e/e2e_multimesh_test.go
@@ -0,0 +1,171 @@
+package e2e
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	. "github.com/openservicemesh/osm/tests/framework"
+)
+
+// This tests connectivity between two Service Meshes within one cluster.
+// In theory, this could be extended to two SMs in different clusters
+var _ = OSMDescribe("Testing HTTP Traffic from one mesh to another",
+	OSMDescribeInfo{
+		Tier:   1,
+		Bucket: 7,
+	},
+	func() {
+		Context("Test traffic flowing from client to server with a Kubernetes Service for the Source: HTTP", func() {
+			withSourceKubernetesService := true
+			testMultimesh(withSourceKubernetesService)
+		})
+
+	})
+
+func testMultimesh(withSourceKubernetesService bool) {
+	const destName = "server"   // osm
+	const sourceName = "client" //source
+    const sourceMeshName = "client-mesh"
+
+	It("Tests HTTP traffic for client pod -> server pod", func() {
+
+		// Install OSM
+		Expect(Td.InstallOSM(Td.GetOSMInstallOpts())).To(Succeed())
+
+		// Create namespace for "osm"
+		Expect(Td.CreateNs(destName, nil)).To(Succeed())
+		Expect(Td.AddNsToMesh(true, destName)).To(Succeed())
+
+		// Get simple pod definitions for the HTTP server
+		svcAccDef, podDef, svcDef := Td.SimplePodApp(
+			SimplePodAppDef{
+				Name:      destName,
+				Namespace: destName,
+				Image:     "kennethreitz/httpbin",
+				Ports:     []int{80},
+			})
+
+		_, err := Td.CreateServiceAccount(destName, &svcAccDef)
+		Expect(err).NotTo(HaveOccurred())
+		_, err = Td.CreatePod(destName, podDef)
+		Expect(err).NotTo(HaveOccurred())
+		dstSvc, err := Td.CreateService(destName, svcDef)
+		Expect(err).NotTo(HaveOccurred())
+
+		// Expect it to be up and running in it's receiver namespace
+		Expect(Td.WaitForPodsRunningReady(destName, 90*time.Second, 1)).To(Succeed())
+
+		// Install client mesh - slightly hacky way
+		clientMeshInstallOpts := Td.GetOSMInstallOpts()
+		clientMeshInstallOpts.ControlPlaneNS = sourceMeshName
+		Expect(Td.InstallOSM(clientMeshInstallOpts)).To(Succeed())
+
+		// Create namespace for source/client"
+		Expect(Td.CreateNs(sourceName, nil)).To(Succeed())
+		Expect(Td.AddNsToSpecificMesh(true, sourceMeshName, sourceName)).To(Succeed())
+
+		// Create source/client pod
+		srcPod := setupMultimeshSource(sourceName, withSourceKubernetesService)
+
+		// TODO - Add MultiClusterDefinition with both service accounts?
+
+		// TODO - Update to Multimesh?
+		By("Creating SMI policies")
+		// Deploy allow rule client->server
+		httpRG, trafficTarget := Td.CreateSimpleAllowPolicy(
+			SimpleAllowPolicy{
+				RouteGroupName:    "routes",
+				TrafficTargetName: "test-target",
+
+				SourceNamespace:      sourceName,
+				SourceSVCAccountName: sourceName,
+
+				DestinationNamespace:      destName,
+				DestinationSvcAccountName: destName,
+			})
+
+		// Configs have to be put into a monitored NS
+		// TODO - Also add to sourceMeshName?
+		_, err = Td.CreateHTTPRouteGroup(sourceName, httpRG)
+		Expect(err).NotTo(HaveOccurred())
+		_, err = Td.CreateTrafficTarget(sourceName, trafficTarget)
+		Expect(err).NotTo(HaveOccurred())
+
+		// All ready. Expect client to reach server
+		clientToServer := HTTPRequestDef{
+			SourceNs:        sourceName,
+			SourcePod:       srcPod.Name,
+			SourceContainer: sourceName,
+
+			Destination: fmt.Sprintf("%s.%s.svc.cluster.%s", dstSvc.Name, dstSvc.Namespace, "local"), // TODO - dstSvc.ClusterDomain?
+		}
+
+		srcToDestStr := fmt.Sprintf("%s -> %s",
+			fmt.Sprintf("%s.%s", sourceName, srcPod.Name),
+			clientToServer.Destination)
+
+		cond := Td.WaitForRepeatedSuccess(func() bool {
+			result := Td.HTTPRequest(clientToServer)
+
+			if result.Err != nil || result.StatusCode != 200 {
+				Td.T.Logf("> (%s) HTTP Req failed %d %v",
+					srcToDestStr, result.StatusCode, result.Err)
+				return false
+			}
+			Td.T.Logf("> (%s) HTTP Req succeeded: %d", srcToDestStr, result.StatusCode)
+			return true
+		}, 5, 90*time.Second)
+
+		sourceService := map[bool]string{true: "with", false: "without"}[withSourceKubernetesService]
+		Expect(cond).To(BeTrue(), "Failed testing HTTP traffic from source pod %s Kubernetes Service to a destination", sourceService)
+
+		// TODO test deleting in only default mesh and only in sourceMesh and deleting both for sanity check?
+		By("Deleting SMI policies")
+		Expect(Td.SmiClients.AccessClient.AccessV1alpha3().TrafficTargets(sourceName).Delete(context.TODO(), trafficTarget.Name, metav1.DeleteOptions{})).To(Succeed())
+		Expect(Td.SmiClients.SpecClient.SpecsV1alpha4().HTTPRouteGroups(sourceName).Delete(context.TODO(), httpRG.Name, metav1.DeleteOptions{})).To(Succeed())
+
+		// Expect client not to reach server
+		cond = Td.WaitForRepeatedSuccess(func() bool {
+			result := Td.HTTPRequest(clientToServer)
+
+			// Curl exit code 7 == Conn refused
+			if result.Err == nil || !strings.Contains(result.Err.Error(), "command terminated with exit code 7 ") {
+				Td.T.Logf("> (%s) HTTP Req failed, incorrect expected result: %d, %v", srcToDestStr, result.StatusCode, result.Err)
+				return false
+			}
+			Td.T.Logf("> (%s) HTTP Req failed correctly: %v", srcToDestStr, result.Err)
+			return true
+		}, 5, 150*time.Second)
+		Expect(cond).To(BeTrue())
+	})
+}
+
+func setupMultimeshSource(sourceName string, withKubernetesService bool) *v1.Pod {
+	// Get simple Pod definitions for the client
+	svcAccDef, podDef, _ := Td.SimplePodApp(SimplePodAppDef{
+		Name:      sourceName,
+		Namespace: sourceName,
+		Command:   []string{"sleep", "365d"},
+		Image:     "curlimages/curl",
+		Ports:     []int{80},
+	})
+
+	_, err := Td.CreateServiceAccount(sourceName, &svcAccDef)
+	Expect(err).NotTo(HaveOccurred())
+
+	srcPod, err := Td.CreatePod(sourceName, podDef)
+	Expect(err).NotTo(HaveOccurred())
+
+	// Expect it to be up and running in it's receiver namespace
+	Expect(Td.WaitForPodsRunningReady(sourceName, 90*time.Second, 1)).To(Succeed())
+
+	return srcPod
+}
--- tests/framework/namespace.go
+++ tests/framework/namespace.go
@@ -44,6 +44,38 @@ func (td *OsmTestData) AddNsToMesh(shouldInjectSidecar bool, ns ...string) error
 	return nil
 }
 
+// TODO - refactor original method to take service mesh name?
+// AddNsToSpecificMesh Adds monitored namespaces to the a specified mesh
+func (td *OsmTestData) AddNsToSpecificMesh(shouldInjectSidecar bool, meshName string, ns ...string) error {
+	td.T.Logf("Adding Namespaces [+%s] to the mesh", ns)
+	for _, namespace := range ns {
+		args := []string{"namespace", "add", namespace, fmt.Sprintf("--mesh-name=%s", meshName)}
+		if !shouldInjectSidecar {
+			args = append(args, "--disable-sidecar-injection")
+		}
+
+		stdout, stderr, err := td.RunLocal(filepath.FromSlash("../../bin/osm"), args...)
+		if err != nil {
+			td.T.Logf("error running osm namespace add")
+			td.T.Logf("stdout:\n%s", stdout)
+			td.T.Logf("stderr:\n%s", stderr)
+			return errors.Wrap(err, "failed to run osm namespace add")
+		}
+
+		if Td.EnableNsMetricTag {
+			args = []string{"metrics", "enable", "--namespace", namespace}
+			stdout, stderr, err = td.RunLocal(filepath.FromSlash("../../bin/osm"), args...)
+			if err != nil {
+				td.T.Logf("error running osm namespace add")
+				td.T.Logf("stdout:\n%s", stdout)
+				td.T.Logf("stderr:\n%s", stderr)
+				return errors.Wrap(err, "failed to run osm namespace add")
+			}
+		}
+	}
+	return nil
+}
+
 // WaitForNamespacesDeleted waits for the namespaces to be deleted.
 // Reference impl taken from https://github.com/kubernetes/kubernetes/blob/master/test/e2e/framework/util.go#L258
 func (td *OsmTestData) WaitForNamespacesDeleted(namespaces []string, timeout time.Duration) error {
