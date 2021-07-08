356cd2780c1476fb013d0f8b4c137fe29d924d9d
Kalya Subramanian <kasubra@microsoft.com>
2021-07-08 16:10:45 -0700

    create appPort constant
    Signed-off-by: Kalya Subramanian <kasubra@microsoft.com>

---
+++ tests/e2e/constants.go
@@ -0,0 +1,6 @@
+package e2e
+
+const (
+	// appPort is the port to be used for server and client applications
+	appPort = 14001
+)
--- tests/e2e/e2e_trafficsplit_same_sa_test.go
+++ tests/e2e/e2e_trafficsplit_same_sa_test.go
@@ -80,8 +80,8 @@ var _ = OSMDescribe("Test TrafficSplit where each backend shares the same Servic
 							Namespace:    serverNamespace,
 							ReplicaCount: int32(serverReplicaSet),
 							Image:        "simonkowallik/httpbin",
-							Ports:        []int{14001},
-							Command:      []string{"gunicorn", "-b", "0.0.0.0:14001", "httpbin:app", "-k", "gevent"},
+							Ports:        []int{appPort},
+							Command:      []string{"gunicorn", "-b", fmt.Sprintf("0.0.0.0:%d", appPort), "httpbin:app", "-k", "gevent"},
 						})
 
 					// Expose an env variable such as XHTTPBIN_X_POD_NAME:
@@ -122,7 +122,7 @@ var _ = OSMDescribe("Test TrafficSplit where each backend shares the same Servic
 							Command:      []string{"/bin/bash", "-c", "--"},
 							Args:         []string{"while true; do sleep 30; done;"},
 							Image:        "songrgg/alpine-debug",
-							Ports:        []int{14001},
+							Ports:        []int{appPort},
 						})
 
 					_, err := Td.CreateServiceAccount(clientApp, &svcAccDef)
@@ -165,7 +165,7 @@ var _ = OSMDescribe("Test TrafficSplit where each backend shares the same Servic
 				_, _, trafficSplitService := Td.SimplePodApp(SimplePodAppDef{
 					Name:      trafficSplitName,
 					Namespace: serverNamespace,
-					Ports:     []int{14001},
+					Ports:     []int{appPort},
 				})
 
 				// Creating trafficsplit service in K8s
@@ -212,7 +212,7 @@ var _ = OSMDescribe("Test TrafficSplit where each backend shares the same Servic
 							SourceContainer: ns, // container_name == NS for this test
 
 							// Targeting the trafficsplit FQDN
-							Destination: fmt.Sprintf("%s.%s:14001", trafficSplitName, serverNamespace),
+							Destination: fmt.Sprintf("%s.%s:%d", trafficSplitName, serverNamespace, appPort),
 						})
 					}
 				}
