e97bb33305e097b5e3b8fda5aaa83b07137a58af
Delyan Raychev <delyan.raychev@microsoft.com>
2021-07-15 17:53:47 -0700

    multicluster: Set default for MULTICLUSTER_CONTEXTS env var to 'alpha beta' (#3768)
    Signed-off-by: Delyan Raychev <delyan.raychev@microsoft.com>

--- demo/run-osm-multicluster-demo.sh
+++ demo/run-osm-multicluster-demo.sh
@@ -40,7 +40,7 @@ DEPLOY_PROMETHEUS="${DEPLOY_PROMETHEUS:-false}"
 DEPLOY_WITH_SAME_SA="${DEPLOY_WITH_SAME_SA:-false}"
 ENVOY_LOG_LEVEL="${ENVOY_LOG_LEVEL:-debug}"
 DEPLOY_ON_OPENSHIFT="${DEPLOY_ON_OPENSHIFT:-false}"
-MULTICLUSTER_CONTEXTS="${MULTICLUSTER_CONTEXTS:-()}"
+MULTICLUSTER_CONTEXTS="${MULTICLUSTER_CONTEXTS:-alpha beta}"
 
 # For any additional installation arguments. Used heavily in CI.
 optionalInstallArgs=$*
@@ -130,5 +130,4 @@ for CONTEXT in $MULTICLUSTER_CONTEXTS; do
 
     # Create the MultiClusterService object.
     ./demo/deploy-MultiClusterService.sh
-
 done
