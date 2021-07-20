eec39e2b446f8fd43029fb7ad3f36b24df635b66
Delyan Raychev <delyan.raychev@microsoft.com>
2021-07-20 09:18:37 -0700

    Adjust
    Signed-off-by: Delyan Raychev <delyan.raychev@microsoft.com>

--- demo/get-multicluster-envoy-configs.sh
+++ demo/get-multicluster-envoy-configs.sh
@@ -3,13 +3,10 @@
 # This script automates the collection of Envoy config during Multicluster demo/development.
 # The configs are collected from Envoys in the ALPHA kube context (cluster).
 
-TMP_PID_FILE=$(mktemp)
-
-PORT=15000
 
 kubectl config use-context alpha > /dev/null
 for NAMESPACE in bookbuyer bookstore; do
     for POD in $(kubectl get pod -n "${NAMESPACE}" --no-headers | awk '{print $1}'); do
-./osm proxy get "config_dump?include_eds" "${POD}" --namespace="${NAMESPACE}" > "envoy__config___${NAMESPACE}___${POD}.json"
+        ./osm proxy get "config_dump?include_eds" "${POD}" --namespace="${NAMESPACE}" > "envoy__config___${NAMESPACE}___${POD}.json"
     done
 done
