54dae7590f770a91d1c4b796d0e121ece130d42b
Delyan Raychev <delyan.raychev@microsoft.com>
2021-07-20 10:43:39 -0700

    multicluster: Adding ?include_eds to script getting Envoy configs (#3801)
    * multicluster: Adding ?include_eds to script getting Envoy configs
    
    Signed-off-by: Delyan Raychev <delyan.raychev@microsoft.com>

--- demo/get-multicluster-envoy-configs.sh
+++ demo/get-multicluster-envoy-configs.sh
@@ -3,9 +3,10 @@
 # This script automates the collection of Envoy config during Multicluster demo/development.
 # The configs are collected from Envoys in the ALPHA kube context (cluster).
 
+
 kubectl config use-context alpha > /dev/null
 for NAMESPACE in bookbuyer bookstore; do
     for POD in $(kubectl get pod -n "${NAMESPACE}" --no-headers | awk '{print $1}'); do
-      ./osm proxy get config_dump "${POD}" --namespace="${NAMESPACE}" > "envoy__config___${NAMESPACE}___${POD}.json"
+        ./osm proxy get "config_dump?include_eds" "${POD}" --namespace="${NAMESPACE}" > "envoy__config___${NAMESPACE}___${POD}.json"
     done
 done
