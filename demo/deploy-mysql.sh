e4273321e42736374b97f2d77afc94247cef2f6b
Allen Leigh <allenlsy@gmail.com>
2021-09-01 10:16:50 -0700

    chore: rearrange content in mysql deployment script
    Files in docs/example/manifests are to be used by following
    documentation for manual demo, rather than be exectued by automated
    demo. Thus the content in the manifest files need to be complete.
    
    This change completes the MySQL deployment manifest file, and updates
    the automated demo script accordingly.
    
    Signed-off-by: Allen Leigh <allenlsy@gmail.com>

--- demo/deploy-mysql.sh
+++ demo/deploy-mysql.sh
@@ -7,9 +7,78 @@ source .env
 DEPLOY_ON_OPENSHIFT="${DEPLOY_ON_OPENSHIFT:-false}"
 USE_PRIVATE_REGISTRY="${USE_PRIVATE_REGISTRY:-true}"
 
-kubectl apply -f docs/example/manifests/apps/mysql.yaml -n "$BOOKWAREHOUSE_NAMESPACE"
-
 kubectl apply -f - <<EOF
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: mysql
+  namespace: $BOOKWAREHOUSE_NAMESPACE
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: mysql
+  namespace: $BOOKWAREHOUSE_NAMESPACE
+spec:
+  ports:
+  - port: 3306
+    targetPort: 3306
+    name: client
+    appProtocol: tcp
+  selector:
+    app: mysql
+  clusterIP: None
+---
+apiVersion: apps/v1
+kind: StatefulSet
+metadata:
+  name: mysql
+  namespace: $BOOKWAREHOUSE_NAMESPACE
+spec:
+  serviceName: mysql
+  replicas: 1
+  selector:
+    matchLabels:
+      app: mysql
+  template:
+    metadata:
+      labels:
+        app: mysql
+    spec:
+      serviceAccountName: mysql
+      nodeSelector:
+        kubernetes.io/os: linux
+      containers:
+      - image: mysql:5.6
+        name: mysql
+        env:
+        - name: MYSQL_ROOT_PASSWORD
+          value: mypassword
+        - name: MYSQL_DATABASE
+          value: booksdemo
+        ports:
+        - containerPort: 3306
+          name: mysql
+        volumeMounts:
+        - mountPath: /mysql-data
+          name: data
+        readinessProbe:
+          tcpSocket:
+            port: 3306
+          initialDelaySeconds: 15
+          periodSeconds: 10
+      volumes:
+        - name: data
+          emptyDir: {}
+  volumeClaimTemplates:
+  - metadata:
+      name: data
+    spec:
+      accessModes: [ "ReadWriteOnce" ]
+      resources:
+        requests:
+          storage: 250M
+---
 kind: TrafficTarget
 apiVersion: access.smi-spec.io/v1alpha3
 metadata:
