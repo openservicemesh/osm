9048b024bfdf142194fe21d88fb903dae302d107
Thomas Stringer <thstring@microsoft.com>
2021-11-16 22:31:55 -0500

    Add bug-report ability to collect control plane logs (#4365)
    Adds bug-report ability to collect control plane logs
    
    Signed-off-by: Thomas Stringer <thstring@microsoft.com>

---
+++ pkg/bugreport/controlplane.go
@@ -0,0 +1,37 @@
+package bugreport
+
+import (
+	"context"
+	"fmt"
+	"path"
+	"strings"
+
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/openservicemesh/osm/pkg/constants"
+)
+
+func (c *Config) collectControlPlaneLogs() error {
+	pods, err := c.KubeClient.CoreV1().Pods(c.ControlPlaneNamepace).
+		List(context.Background(), v1.ListOptions{
+			LabelSelector: fmt.Sprintf(
+				"%s=%s",
+				constants.OSMAppNameLabelKey,
+				constants.OSMAppNameLabelValue,
+			),
+		})
+	if err != nil {
+		return fmt.Errorf("error getting control plane pods: %w", err)
+	}
+
+	for _, pod := range pods.Items {
+		cmd := []string{"kubectl", "logs", "-n", pod.Namespace, pod.Name}
+		outPath := path.Join(c.rootNamespaceDirPath(), pod.Namespace, rootPodDirName, pod.Name, commandsDirName, strings.Join(cmd, "_"))
+		if err := runCmdAndWriteToFile(cmd, outPath); err != nil {
+			return fmt.Errorf("error writing control pod logs: %w", err)
+		}
+		c.completionSuccess("Collected report for Pod %s/%s", pod.Namespace, pod.Name)
+	}
+
+	return nil
+}
