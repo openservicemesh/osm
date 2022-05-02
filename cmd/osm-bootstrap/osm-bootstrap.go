896fb7af871e7dbf74d4bd27863ef0336105913a
steeling <seanteeling@microsoft.com>
2022-05-02 19:03:25 -0400

    ref(certs): refactor k8s root ca secret access (#4657)
    Refactors the k8s secret access for root ca's. This is 
    done strictly to make subsequent PR's smaller,
    and easier to review.
    
    This is essentially just moving code around for the
    purpose of avoiding cyclic dependencies, with a
    couple renames, but almost 0 functionality change.
    
    The one change is abstracting NewFromPem, to
    work for both tresor and certmanager to accept a
    nil privatekey. It is now up to the caller to validate
    whether the private key is present if it is required.
    
    Signed-off-by: Sean Teeling <seanteeling@microsoft.com>

--- cmd/osm-bootstrap/osm-bootstrap.go
+++ cmd/osm-bootstrap/osm-bootstrap.go
@@ -177,10 +177,8 @@ func main() {
 	cfg := configurator.NewConfigurator(configClient, stop, osmNamespace, osmMeshConfigName, msgBroker)
 
 	// Intitialize certificate manager/provider
-	certProviderConfig := providers.NewCertificateProviderConfig(kubeClient, kubeConfig, cfg, providers.Kind(certProviderKind), osmNamespace,
+	certManager, _, _, err := providers.GenerateCertificateManager(kubeClient, kubeConfig, cfg, providers.Kind(certProviderKind), osmNamespace,
 		caBundleSecretName, tresorOptions, vaultOptions, certManagerOptions, msgBroker)
-
-	certManager, _, err := certProviderConfig.GetCertificateManager()
 	if err != nil {
 		events.GenericEventRecorder().FatalEvent(err, events.InvalidCertificateManager,
 			"Error initializing certificate manager of kind %s", certProviderKind)
