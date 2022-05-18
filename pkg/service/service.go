7bf723a2e397dcd3c3893f6796391807cfbb6fa2
Keith Mattix II <keithmattix2@gmail.com>
2022-05-18 17:44:22 -0500

    Remove unneded function
    Signed-off-by: Keith Mattix II <keithmattix2@gmail.com>

--- pkg/service/service.go
+++ pkg/service/service.go
@@ -4,22 +4,3 @@ package service
 func (ms MeshService) ServerName() string {
 	return ms.FQDN()
 }
-
-// FilterMeshServicesBySubdomain takes a slice of MeshServices and filters them down
-// to elements with matching subdomains (optionally including elements with no subdomain)
-func FilterMeshServicesBySubdomain(svcs []MeshService, subdomain string, includeEmptySubdomains bool) []MeshService {
-	var filteredSvcs []MeshService
-	for _, svc := range svcs {
-		if svc.Subdomain() == subdomain {
-			filteredSvcs = append(filteredSvcs, svc)
-			continue
-		}
-
-		if includeEmptySubdomains && svc.Subdomain() == "" {
-			filteredSvcs = append(filteredSvcs, svc)
-			continue
-		}
-	}
-
-	return filteredSvcs
-}
