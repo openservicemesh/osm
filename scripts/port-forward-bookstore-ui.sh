9749020d71c5ed301ceca89210f4f2c8bdcfc5f5
Shashank Ram <21697719+shashankram@users.noreply.github.com>
2022-07-18 11:17:35 -0700

    demo/scripts: fix bookstore app label and container name (#4910)
    Updates the scripts to be in sync with the manifests
    used in the manual demo documented on the website and
    to be compatible with changes made to the bookstore app
    labels in 5f54056b.
    
    Also fixes #4906 and updates the release notes for
    v1.2.
    
    Signed-off-by: Shashank Ram <shashr2204@gmail.com>

--- scripts/port-forward-bookstore-ui.sh
+++ scripts/port-forward-bookstore-ui.sh
@@ -7,15 +7,15 @@
 # shellcheck disable=SC1091
 source .env
 
-backend="${1:-bookstore}"
+selector="$1"
 thisScript="$(dirname "$0")/$(basename "$0")"
 
-if [ -z "$backend" ]; then
-    echo "Usage: $thisScript <backend-name>"
+if [ -z "$selector" ]; then
+    echo "Usage: $thisScript <selector>"
     exit 1
 fi
 
 BOOKSTORE_LOCAL_PORT="${BOOKSTORE_LOCAL_PORT:-8084}"
-POD="$(kubectl get pods --selector app="$backend" -n "$BOOKSTORE_NAMESPACE" --no-headers | grep 'Running' | awk 'NR==1{print $1}')"
+POD="$(kubectl get pods --selector "$selector" -n "$BOOKSTORE_NAMESPACE" --no-headers | grep 'Running' | awk 'NR==1{print $1}')"
 
 kubectl port-forward "$POD" -n "$BOOKSTORE_NAMESPACE" "$BOOKSTORE_LOCAL_PORT":14001
