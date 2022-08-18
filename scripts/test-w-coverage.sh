71d74047cd34471ce4546e32b2b6a9c470a4ae45
Sean Teeling <seanteeling@microsoft.com>
2022-08-18 10:15:04 -0400

    add cmd pkg and subdirs to coverage
    Signed-off-by: Sean Teeling <seanteeling@microsoft.com>

--- scripts/test-w-coverage.sh
+++ scripts/test-w-coverage.sh
@@ -16,7 +16,7 @@ go test -timeout 120s \
    -failfast \
    -race \
    -v \
-   -coverpkg=./pkg/... \
+   -coverpkg=./pkg/...,./cmd/... \
    -coverprofile=coverage.txt.with_generated_code \
    -covermode atomic $modules | tee testoutput.txt || { echo "go test returned non-zero"; exit 1; }
 
