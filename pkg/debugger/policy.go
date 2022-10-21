31efbe8aea85fca72548ea7cc3e32dcbc7a2d7b9
nshankar13 <nshankar@microsoft.com>
2022-10-21 06:18:05 -0400

    Get rid of policies struct
    Signed-off-by: nshankar13 <nshankar@microsoft.com>

--- pkg/debugger/policy.go
+++ pkg/debugger/policy.go
@@ -4,20 +4,8 @@ import (
 	"fmt"
 	"net/http"
 
-	access "github.com/servicemeshinterface/smi-sdk-go/pkg/apis/access/v1alpha3"
-	spec "github.com/servicemeshinterface/smi-sdk-go/pkg/apis/specs/v1alpha4"
-	split "github.com/servicemeshinterface/smi-sdk-go/pkg/apis/split/v1alpha2"
-
-	"github.com/openservicemesh/osm/pkg/identity"
 	"github.com/openservicemesh/osm/pkg/utils"
 )
-
-type policies struct {
-	TrafficSplits   []*split.TrafficSplit        `json:"traffic_splits"`
-	ServiceAccounts []identity.K8sServiceAccount `json:"service_accounts"`
-	RouteGroups     []*spec.HTTPRouteGroup       `json:"route_groups"`
-	TrafficTargets  []*access.TrafficTarget      `json:"traffic_targets"`
-}
 
 func (ds DebugConfig) getOSMConfigHandler() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
