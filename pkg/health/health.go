6e66bfb7400fd0adeb85b918f3f2006f9d309ac7
Whitney Griffith <whgriffi@microsoft.com>
2023-03-03 19:50:08 +0000

    fix(): reduce minimum tls version for osm controller, verifier, health
    Signed-off-by: Whitney Griffith <whgriffi@microsoft.com>

--- pkg/health/health.go
+++ pkg/health/health.go
@@ -50,7 +50,7 @@ func (httpProbe HTTPProbe) Probe() (int, error) {
 		transport := &http.Transport{
 			TLSClientConfig: &tls.Config{
 				InsecureSkipVerify: true,
-				MinVersion:         tls.VersionTLS13,
+				MinVersion:         tls.VersionTLS12,
 			},
 		}
 		client.Transport = transport
