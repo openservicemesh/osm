1f19bb530955e8c01990f7170e89f82eb699bcba
mudit singh <mudit.singh@india.nec.com>
2023-04-25 14:22:11 +0530

    Added contour installation tags
    Added set parameters for tls with contour
    
    Signed-off-by: mudit singh <mudit.singh@india.nec.com>

--- tests/framework/helm.go
+++ tests/framework/helm.go
@@ -23,7 +23,7 @@ func (td *OsmTestData) HelmInstallOSM(release, namespace string) error {
 	return nil
 }
 
-// HelmInstallOSM installs an osm control plane using the osm chart which lives in charts/osm with contour
+// HelmInstallOSMContour installs an osm control plane using the osm chart which lives in charts/osm with contour
 func (td *OsmTestData) HelmInstallOSMContour(release, namespace string) error {
 	if td.InstType == KindCluster {
 		if err := td.LoadOSMImagesIntoKind(); err != nil {
@@ -31,7 +31,7 @@ func (td *OsmTestData) HelmInstallOSMContour(release, namespace string) error {
 		}
 	}
 
-	values := fmt.Sprintf("osm.image.registry=%s,osm.image.tag=%s,osm.meshName=%s,contour.enabled=%s", td.CtrRegistryServer, td.OsmImageTag, release, "true")
+	values := fmt.Sprintf("osm.image.registry=%s,osm.image.tag=%s,osm.meshName=%s,contour.enabled=%s,contour.configInline.tls.envoy-client-certificate.name=%s,contour.configInline.tls.envoy-client-certificate.namespace=%s", td.CtrRegistryServer, td.OsmImageTag, release, "true", "osm-contour-envoy-client-cert", namespace)
 	args := []string{"install", release, "../../charts/osm", "--set", values, "--namespace", namespace, "--create-namespace", "--debug", "--wait"}
 	stdout, stderr, err := td.RunLocal("helm", args...)
 	if err != nil {
